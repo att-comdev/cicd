650be02158d317116de43f8a2749514007db4df9
Kaspars Skels <kaspars.skels@gmail.com>
2017-08-08 12:46:47 -0500

    WIP Jenkinsfile Chart template
    Change-Id: If14817250b9d43932372beb0de381d525e464835
    todo: Add more description here

---
+++ osh/openstack/openstack-helm/seed.groovy
@@ -0,0 +1,54 @@
+def comps = ['glance', 'nova', 'cinder', 'neutron']
+def gtdata = 'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data'
+
+
+for (comp in comps) {
+   pipelineJob("osh/openstack/openstack-helm/${comp}") {
+
+      // http://<gerrit>/plugin/job-dsl/api-viewer/index.html#path/javaposse.jobdsl.dsl.helpers.properties.PropertiesContext.pipelineTriggers-triggers-gerrit
+      triggers {
+         gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+               gerritProject {
+                  compareType('PLAIN')
+                  pattern('openstack/openstack-helm')
+                  branches {
+                     branch {
+                        compareType("ANT")
+                        pattern("**")
+                     }
+                  }
+                  filePaths {
+                     filePath {
+                        compareType("ANT")
+                        pattern("$comp/**")
+                     }
+                     filePath {
+                        compareType("ANT")
+                        pattern("helm-toolkit/**")
+                     }
+                  }
+                  disableStrictForbiddenFileVerification(false)
+               }
+            }
+            triggerOnEvents {
+               patchsetCreated {
+                  excludeDrafts(false)
+                  excludeTrivialRebase(false)
+                  excludeNoCodeChange(false)
+               }
+               changeMerged()
+            }
+         }
+
+         definition {
+            cps {
+               script(readFileFromWorkspace('osh/openstack/openstack-helm/Jenkinsfile'))
+               sandbox()
+            }
+         }
+
+      }
+   }
+
