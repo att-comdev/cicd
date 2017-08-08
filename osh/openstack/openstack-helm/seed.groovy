dc7435833c9bc1d2026de8d686ac91f2777b5188
Kaspars Skels <kaspars.skels@gmail.com>
2017-08-08 12:46:47 -0500

    Jenkinsfile template and seed for Charts
    Jenkins seed script to generate build job for each chart
    Generic Jenkinsfile template based on JOB_BASE_NAME
     - Individual Charts get published in the Artifactory
    
    Change-Id: If14817250b9d43932372beb0de381d525e464835

---
+++ osh/openstack/openstack-helm/seed.groovy
@@ -0,0 +1,54 @@
+
+def charts = ['cinder', 'glance', 'heat', 'horizon', 'keystone', 'magnum', 'mistral', 'neutron', 'nova']
+
+for (chart in charts) {
+   pipelineJob("osh/openstack/openstack-helm/${chart}") {
+
+      triggers {
+         // http://<gerrit>/plugin/job-dsl/api-viewer/index.html#path/javaposse.jobdsl.dsl.helpers.properties.PropertiesContext.pipelineTriggers-triggers-gerrit
+         gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+               gerritProject {
+                  compareType('PLAIN')
+                  pattern('openstack/openstack-helm')
+                  branches {
+                     branch {
+                        compareType("ANT")
+                        pattern("**")
+                     }
+                  }
+                  filePaths {
+                     filePath {
+                        compareType("ANT")
+                        pattern("$chart/**")
+                     }
+                     filePath {
+                        compareType("ANT")
+                        pattern("helm-toolkit/**")
+                     }
+                  }
+                  disableStrictForbiddenFileVerification(false)
+               }
+            }
+            triggerOnEvents {
+               patchsetCreated {
+                  excludeDrafts(false)
+                  excludeTrivialRebase(false)
+                  excludeNoCodeChange(false)
+               }
+               changeMerged()
+            }
+         }
+
+         definition {
+            cps {
+               script(readFileFromWorkspace('osh/openstack/openstack-helm/Jenkinsfile'))
+               sandbox()
+            }
+         }
+
+      }
+   }
+}
+
