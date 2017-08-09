5af2947430e796c44817ea7045515fb00872361e
zdeveloper <zedd.m.shmais@att.com>
2017-08-09 10:19:15 -0500

    [WIP] Added single node osh testing integrations
    Change-Id: Iabc9b9460f6685b30abb445ae25361a769c0018f

---
+++ osh/seedjobs/openstack-single-node/seed.groovy
@@ -0,0 +1,54 @@
+// If you want, you can define your seed job in the DSL and create it via the REST API.
+// See https://github.com/sheehan/job-dsl-gradle-example#rest-api-runner
+
+def branch = GIT_BRANCH
+def project = GIT_REPO + '-' + GIT_PROJECT
+
+def job_name = project + '/' + branch + '/' + 'seed'
+
+folder(project){
+    displayName(project)
+}
+
+folder(project+'/'+branch){
+    displayName(branch)
+}
+
+listView(project) {
+    description('OpenStack-Helm CI')
+    jobs {
+        name(project)
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
+
+job(job_name) {
+    parameters {
+        stringParam('GIT_PROJECT', GIT_PROJECT)
+        stringParam('GIT_REPO', GIT_REPO)
+        stringParam('GIT_BRANCH', GIT_BRANCH)
+        stringParam('SLACK_TEAM', SLACK_TEAM)
+        stringParam('SLACK_TOKEN', SLACK_TOKEN)
+        stringParam('SLACK_ROOM', SLACK_ROOM)
+    }
+    scm {
+        github("\$GIT_REPO/\$GIT_PROJECT", "\$GIT_BRANCH")
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        dsl {
+            external 'osh/openstack/openstack-single-node/*_job.groovy'
+            additionalClasspath 'osh/openstack/openstack-single-node/groovy'
+        }
+    }
+}
