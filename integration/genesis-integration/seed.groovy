3961138577668b8d7f4923b25df3463cb6df7c4a
Kaspars Skels <kaspars.skels@gmail.com>
2017-08-24 17:40:09 -0500

    Integration pipeline for Genesis
     - Pipeline executes UCP deployment script
     - Drydock used for bare-metal node deployment
     - Power off all bare-metal before genesis install
     - Add seed to create genesis-integration job
    
    Change-Id: I711b739fbb5b43bb28f822ff0b22ab6c7d0be9ca

---
+++ integration/genesis-integration/seed.groovy
@@ -0,0 +1,40 @@
+
+base_path = "integration/genesis-integration"
+
+pipelineJob("${base_path}/genesis-full") {
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/ucp-integration")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
+                }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("${base_path}/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
+
