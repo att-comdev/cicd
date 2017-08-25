20d7a5731314aabf02970689221d79257036ad62
Stacey Fletcher <staceylynnfletcher@gmail.com>
2017-08-25 12:42:51 -0500

    Remove patchset trigger from osh dev pipelines
    This patchset updates the openstack-helm pipelines to allow for only
    one VM to be provision per patchset to osh.  It will also build
    pipelines for openstack-helm-addons and all of openstack-helm charts
    
    Change-Id: Ibefa078dda1629b5ca07704c9c365306c9f914d7

--- osh/openstack/openstack-helm/seed.groovy
+++ osh/openstack/openstack-helm/seed.groovy
@@ -1,54 +1,79 @@
 
-def charts = ['cinder', 'glance', 'heat', 'horizon', 'keystone', 'magnum', 'mistral', 'neutron', 'nova']
+import groovy.json.JsonSlurper
 
-for (chart in charts) {
-   pipelineJob("osh/openstack/openstack-helm/${chart}") {
+def chartsJson = '''{ "osh":[{
+                        "repo":"openstack/openstack-helm",
+                        "charts":[  "cinder",
+                                    "heat",
+                                    "glance",
+                                    "horizon",
+                                    "keystone",
+                                    "magnum",
+                                    "mistral",
+                                    "neutron",
+                                    "nova",
+                                    "barbican",
+                                    "ceph",
+                                    "dns-helper",
+                                    "etcd",
+                                    "ingress",
+                                    "mariadb",
+                                    "memcached",
+                                    "rabbitmq",
+                                    "senlin"]
+                        },{
+                        "repo":"openstack/openstack-helm-addons",
+                        "charts":[  "elasticsearch",
+                                    "fluentd",
+                                    "kibana",
+                                    "postgresql"]}]}'''
 
-      triggers {
-         // http://<gerrit>/plugin/job-dsl/api-viewer/index.html#path/javaposse.jobdsl.dsl.helpers.properties.PropertiesContext.pipelineTriggers-triggers-gerrit
-         gerritTrigger {
-            serverName('OS-CommunityGerrit')
-            gerritProjects {
-               gerritProject {
-                  compareType('PLAIN')
-                  pattern('openstack/openstack-helm')
-                  branches {
-                     branch {
-                        compareType("ANT")
-                        pattern("**")
-                     }
-                  }
-                  filePaths {
-                     filePath {
-                        compareType("ANT")
-                        pattern("$chart/**")
-                     }
-                     filePath {
-                        compareType("ANT")
-                        pattern("helm-toolkit/**")
-                     }
-                  }
-                  disableStrictForbiddenFileVerification(false)
-               }
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(chartsJson)
+
+for (entry in object.osh) {
+    for (chart in entry.charts) {
+        pipelineJob("osh/${entry.repo}/${chart}") {
+
+            triggers {
+            // http://<gerrit>/plugin/job-dsl/api-viewer/index.html#path/javaposse.jobdsl.dsl.helpers.properties.PropertiesContext.pipelineTriggers-triggers-gerrit
+                gerritTrigger {
+                    serverName('OS-CommunityGerrit')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern('${entry.repo}')
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("**")
+                                }
+                            }
+                            filePaths {
+                                filePath {
+                                compareType("ANT")
+                                pattern("$chart/**")
+                                }
+                                filePath {
+                                compareType("ANT")
+                                pattern("helm-toolkit/**")
+                                }
+                            }
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        changeMerged()
+                    }
+                }
+
+                definition {
+                    cps {
+                        script(readFileFromWorkspace('osh/openstack/openstack-helm/Jenkinsfile'))
+                        sandbox()
+                    }
+                }
             }
-            triggerOnEvents {
-               patchsetCreated {
-                  excludeDrafts(false)
-                  excludeTrivialRebase(false)
-                  excludeNoCodeChange(false)
-               }
-               changeMerged()
-            }
-         }
-
-         definition {
-            cps {
-               script(readFileFromWorkspace('osh/openstack/openstack-helm/Jenkinsfile'))
-               sandbox()
-            }
-         }
-
-      }
-   }
+        }
+    }
 }
-
