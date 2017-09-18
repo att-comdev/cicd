18d65643dd76e83820c5b19c76137ae092396cd4
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-09-18 15:16:25 -0600

    Drydock Integration
    Change-Id: I9a36dc235683b3fa37eeed310988431a8380d1a6

---
+++ ucp/ucp-drydock/drydock-integration/seed.groovy
@@ -0,0 +1,45 @@
+import groovy.json.JsonSlurper
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(chartsJson)
+
+        pipelineJob('ucp/ucp-drydock/drydock-integration') {
+            triggers {
+            // http://<gerrit>/plugin/job-dsl/api-viewer/index.html#path/javaposse.jobdsl.dsl.helpers.properties.PropertiesContext.pipelineTriggers-triggers-gerrit
+                gerritTrigger {
+                    serverName('Gerrithub-jenkins')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern("att-comdev/cicd")
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("**")
+                                }
+                            }
+                            filePaths {
+                                filePath {
+                                compareType("ANT")
+                                pattern("ucp/ucp-drydock/drydock-integration/**")
+                                }
+                            }
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        patchsetCreated {
+                            excludeDrafts(false)
+                            excludeTrivialRebase(false)
+                            excludeNoCodeChange(false)
+                        }
+                    }
+                }
+
+                definition {
+                    cps {
+                        script(readFileFromWorkspace('ucp/ucp-drydock/drydock-integration/Jenkinsfile'))
+                        sandbox()
+                    }
+                }
+            }
+        }
