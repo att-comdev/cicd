6c369cf108c4521c8ba0939c7a86c36f1b5aa847
sb464f <sb464f@att.com>
2017-10-04 14:17:58 +0000

    updated both files
    Change-Id: Ide2de7cb67e8efd96acb10f3d18a795d20db3871

---
+++ kolla-newton/seed.groovy
@@ -0,0 +1,71 @@
+
+ import groovy.json.JsonSlurper 
+ 
+ 
+ def chartsJson = '''{ "kolla":[{ 
+                         "repo":"kolla_build/newton", 
+                         "charts":[  "cinder", 
+                                     "heat", 
+                                     "glance", 
+                                     "horizon", 
+                                     "keystone", 
+                                     "neutron", 
+                                     "nova", 
+                                     "barbican"] 
+                         }]}''' 
+ 
+ 
+ def jsonSlurper = new JsonSlurper() 
+ def object = jsonSlurper.parseText(chartsJson) 
+ 
+ 
+ for (entry in object.kolla) { 
+     for (chart in entry.charts) { 
+         pipelineJob("kolla/${entry.repo}/${chart}") { 
+ 
+ 
+             triggers { 
+             // http://<gerrit>/plugin/job-dsl/api-viewer/index.html#path/javaposse.jobdsl.dsl.helpers.properties.PropertiesContext.pipelineTriggers-triggers-gerrit 
+                 gerritTrigger { 
+                     serverName('Gerrithub-jenkins') 
+                     gerritProjects { 
+                         gerritProject { 
+                             compareType('PLAIN') 
+                             pattern("${entry.repo}") 
+                             branches { 
+                                 branch { 
+                                 compareType("ANT") 
+                                 pattern("**") 
+                                 } 
+                             } 
+                             filePaths { 
+                                 filePath { 
+                                 compareType("ANT") 
+                                 pattern("$chart/**") 
+                                 }  
+                             } 
+                             disableStrictForbiddenFileVerification(false) 
+                         } 
+                     } 
+                     triggerOnEvents { 
+                        patchsetCreated{
+                           excludeDrafts(false)
+                           excludeTrivialtRebase(false)
+                           excludeNoCodeChange(false)
+                          }
+                         changeMerged() 
+                     } 
+                 } 
+ 
+ 
+                 definition { 
+                     cps { 
+                         script(readFileFromWorkspace('kolla-newton/Jenkinsfile')) 
+                         sandbox() 
+                     } 
+                 } 
+             } 
+         } 
+     } 
+ } 
+
