7d927fa0c25c10de96e02ef13969afc7560c4119
Kaspars Skels <kaspars.skels@gmail.com>
2017-10-05 11:20:52 -0500

    DNM: Add armada parts to integration pipeline
    Armada used to deploy OSH
    Testing generic funcs file
    
    Change-Id: Ib4a9659fea2a306b7571f561ee7ce64b477fde0a

---
+++ integration/genesis-integration/funcs.groovy
@@ -0,0 +1,68 @@
+#!groovy
+
+def openstack_cmd(String cmd, String mount) {
+    keystone_image = "kolla/ubuntu-source-keystone:3.0.3"
+
+    docker_env = "-e 'S_AUTH_URL=http://keystone/v3" +
+                 " -e OS_PROJECT_DOMAIN_NAME=default" +
+                 " -e OS_USER_DOMAIN_NAME=default" +
+                 " -e OS_PROJECT_NAME=service" +
+                 " -e OS_REGION_NAME=RegionOne" +
+                 " -e OS_USERNAME=\$OS_USERNAME" +
+                 " -e OS_PASSWORD=\$OS_PASSWORD" +
+                 " -e OS_IDENTITY_API_VERSION=3"
+
+    docker_opts = "--rm --net=host -v ${mount}:/target"
+
+    return "sudo docker run ${docker_opts} ${docker_env} ${keystone_image} ${cmd}"
+}
+
+
+def jenkins_node_config(String name, String host, String creds) {
+    config = """<slave>
+        <name>${name}</name>
+        <description></description>
+        <remoteFS>/home/ubuntu/jenkins</remoteFS>
+        <numExecutors>1</numExecutors>
+        <mode>EXCLUSIVE</mode>
+        <retentionStrategy class=\"hudson.slaves.RetentionStrategy\$Always\"/>
+        <launcher class=\"hudson.plugins.sshslaves.SSHLauncher\" plugin=\"ssh-slaves@1.5\">
+        <host>${host}</host>
+        <port>22</port>
+        <credentialsId>${creds}</credentialsId>
+        </launcher>
+        <label>${name}</label>
+        <nodeProperties/>
+        <userId>ubuntu</userId>
+        </slave>"""
+    return config
+}
+
+
+def stack_create(String name, String tmpl) {
+
+    cmd = openstack_cmd("openstack stack create -t ${templ} ${name}")
+    for (i = 0; i <3; i++) {
+        code = sh (script: cmd, returnStatus: true)
+        if (code == 0) { break; }
+    }
+
+    sleep 30 // wait before ask for ip
+    cmd = openstack_cmd("openstack stack output show -f value -c output_value ${name} floating_ip")
+    return sh(returnStdout: true, script: cmd).trim()
+}
+
+
+def jenkins_node_create (String name, String config) {
+
+    withCredentials([usernamePassword(credentialsId: 'jenkins-token',
+        usernameVariable: 'JENKINS_USER',
+        passwordVariable: 'JENKINS_TOKEN')]) {
+
+        opts = "-s \$JENKINS_URL -auth \$JENKINS_USER:\$JENKINS_TOKEN"
+        sh "java -jar \$JENKINS_CLI $opts $name << $config"
+    }
+}
+
+return this;
+
