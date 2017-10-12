6a0e60a85e764e3f8b20a9b851d3ef8e7dfbf881
Kaspars Skels <kaspars.skels@gmail.com>
2017-10-12 16:41:12 -0500

    New pipeline for Promenade
     - Refactor pipeline to use common functions
     - Move files from ucp-promenade to promenade
     - Publish docker image and configs in Artifactory
    
    Change-Id: Ifb0c71cf1fedcc0712b8cb3c4efe6201a8571add

---
+++ ucp/promenade/seed.groovy
@@ -0,0 +1,46 @@
+
+pipelineJob("UCP/Promenade/promenade") {
+
+    parameters {
+        stringParam {
+            defaultValue(GERRIT_REFSPEC)
+            description('Pass att-comdev/cicd code refspec to the job')
+            name ('CICD_GERRIT_REFSPEC')
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/promenade")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
+                }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("ucp/promenade/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
+
