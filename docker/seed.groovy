604d4246f2cdc29b9b5fdc7e0e4957cec5119b85
vishnusharma964e <vs964e@att.com>
2017-10-13 15:26:06 +0000

    This PS creates pipeline jobs for the att-comdev/dockerfiles repo
    -Created seed.groovy file for generating the individual pipelines
    -Created stage for building docker images
    -Created stage for publishing docker images of rabbitmq,airflow,
     maas to Artifactory
    
    Change-Id: I932b2782e775de1cca8876cfa096d5dfdd99b335

--- docker/seed.groovy
+++ docker/seed.groovy
@@ -1,6 +1,6 @@
 import groovy.json.JsonSlurper
 
-def chartsJson = '''{ "dockerimages":[{
+def chartsJson = '''{ "images":[{
                         "repo":"att-comdev/dockerfiles",
                         "charts":[  "airflow",
                                     "mass",
@@ -10,9 +10,9 @@ def chartsJson = '''{ "dockerimages":[{
 def jsonSlurper = new JsonSlurper()
 def object = jsonSlurper.parseText(chartsJson)
 
-for (entry in object.dockerimages) {
+for (entry in object.images) {
     for (chart in entry.charts) {
-        pipelineJob("dockerimages/${entry.repo}/${chart}") {
+        pipelineJob("images/${entry.repo}/${chart}") {
 
             triggers {
                 gerritTrigger {
@@ -27,19 +27,23 @@ for (entry in object.dockerimages) {
                                 pattern("**")
                                 }
                             }
-                            
+                            filePaths {
+                                filePath {
+                                compareType("ANT")
+                                pattern("$chart/**")
+                                }                              
+                            }
                             disableStrictForbiddenFileVerification(false)
                         }
                     }
-                triggerOnEvents { 
-                    patchsetCreated { 
-                        excludeDrafts(false) 
-                        excludeTrivialRebase(false) 
-                        excludeNoCodeChange(false) 
-                     } 
-                    changeMerged() 
-                } 
-
+                    triggerOnEvents {
+                        patchsetCreated { 
+                           excludeDrafts(false) 
+                           excludeTrivialRebase(false) 
+                           excludeNoCodeChange(false) 
+                        } 
+                        changeMerged()
+                    }
                 }
 
                 definition {
