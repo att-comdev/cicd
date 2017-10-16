26c2a70a42df427ed209d13a3cc907bfecfacb0b
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-10-16 16:59:12 -0600

    Add SuperSeed job
    Change-Id: I184815a846712607bd5a90209f34e396cab7b619

---
+++ cicd/SuperSeed/seed.groovy
@@ -0,0 +1,66 @@
+base_path = "cicd"
+job_path = "${base_path}/SuperSeed"
+folder("${base_path}")
+
+freeStyleJob("${job_path}") {
+    label('master')
+    parameters {
+        stringParam {
+            name ('SEED_PATH')
+            defaultValue('')
+            description('Seed path. Example: cicd/SuperSeed/seed.groovy')
+        }
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('origin/master')
+            description('Gerrit refspec')
+        }
+        stringParam {
+            name ('GERRIT_PROJECT')
+            defaultValue('att-comdev/cicd')
+            description('Project on Gerrithub')
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-voting')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/cicd")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+/// PatchsetCreated trigger should be manually enabled on staging:
+//                patchsetCreated {
+//                   excludeDrafts(true)
+//                   excludeTrivialRebase(true)
+//                   excludeNoCodeChange(true)
+//                }
+
+/// changeMerged trigger for production:
+            changeMerged()
+            }
+        }
+    }
+    steps {
+    //Wipe the workspace:
+        wrappers {
+            preBuildCleanup()
+        }
+        shell(readFileFromWorkspace("${job_path}/superseed.sh"))
+        dsl {
+            external('${BUILD_NUMBER}/seed.groovy')
+            //ignoreExisting(true)
+            //removeAction('DISABLE')
+        }
+    }
+}
