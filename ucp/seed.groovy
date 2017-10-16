14a7e14785f340c910aa9d007cdf414130f32411
vishnusharma964e <vs964e@att.com>
2017-10-16 21:02:01 +0000

    This PS creates a master seedjob for all of the components in att-comdev/cicd/ucp repo
    Change-Id: I99c017533587043b3ee74c8d9d884d195011cf4c

---
+++ ucp/seed.groovy
@@ -0,0 +1,62 @@
+iimport groovy.json.JsonSlurper
+
+def componentsJson = '''{ "UCP":[{
+                        "repo":"att-comdev/cicd/ucp",
+                        "components":[  "deckhand",
+                                    "promenade",
+                                    "ucp-armada/armada",
+                                    "ucp-drydock/drydock"]
+                        }]}'''
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(componentsJson)
+
+for (entry in object.UCP) {
+    for (item in entry.components) {
+        pipelineJob("UCP/${entry.repo}/${item}") {
+
+            triggers {
+                gerritTrigger {
+                    serverName('Gerrithub-jenkins')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern("${entry.repo}")
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("**")
+                                }
+                            }
+                            filePaths {
+                                filePath {
+                                compareType("ANT")
+                                pattern("$item/**")
+                                }
+                            }
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        patchsetCreated {
+                           excludeDrafts(false)
+                           excludeTrivialRebase(false)
+                           excludeNoCodeChange(false)
+                        }
+                        changeMerged()
+                    }
+                }
+
+                definition {
+                    cps {
+
+                        script(readFileFromWorkspace('ucp/${entry.repo}/${item}/Jenkinsfile'))
+                        sandbox()
+
+                    }
+                }
+            }
+        }
+    }
+}
+
