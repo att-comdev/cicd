eba88f0e172f3e2d22b06350e29ae80c2aaabd0c
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-10-16 16:59:12 -0600

    Add SuperSeed job
    Change-Id: I184815a846712607bd5a90209f34e396cab7b619

---
+++ cicd/SuperSeed/seed.groovy
@@ -0,0 +1,75 @@
+base_path = "cicd"
+job_path = "${base_path}/SuperSeed"
+folder("${base_path}")
+
+freeStyleJob("${job_path}") {
+    label('master')
+    parameters {
+        stringParam {
+            name ('SEED_PATH')
+            defaultValue('')
+            description('Seed path. Example: cicd/SuperSeed/seed.groovy')
+        }
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('origin/master')
+            description('Gerrit refspec')
+        }
+        stringParam {
+            name ('GERRIT_PROJECT')
+            defaultValue('att-comdev/cicd')
+            description('Project on Gerrithub')
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-voting')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/cicd")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(true)
+                   excludeTrivialRebase(true)
+                   excludeNoCodeChange(true)
+                }
+//FIXME: change the trigger before the merge!!!!:
+//            changeMerged()
+            }
+        }
+    }
+    steps {
+    //Wipe the workspace:
+        wrappers {
+            preBuildCleanup()
+        }
+        shell(readFileFromWorkspace("${job_path}/superseed.sh"))
+//testing
+        File seed = new File("${WORKSPACE}/SEED_PATH.txt")
+        if (seed.exists()){
+            environmentVariables {
+//                overrideBuildParameters()
+                propertiesFile("SEED_PATH.txt")
+            }
+        }
+        println "SEED_PATH=${SEED_PATH}"
+        println "SystemSEED_PATH=" + System.getenv("SEED_PATH")
+
+        dsl {
+            external('${SEED_PATH}')
+            //ignoreExisting(true)
+            //removeAction('DISABLE')
+        }
+    }
+}
