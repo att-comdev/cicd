7a92ece6d442df7095d3c0f5579507471749ecc0
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-10-16 16:59:12 -0600

    Add SuperSeed job
    Change-Id: I184815a846712607bd5a90209f34e396cab7b619

---
+++ cicd/SuperSeed/seed.groovy
@@ -0,0 +1,62 @@
+base_path = "cicd/SuperSeed"
+
+freeStyleJob("${base_path}") {
+    label('master')
+    parameters {
+        stringParam {
+            name ('SEED_PATH')
+            defaultValue('')
+            description('Seed path')
+        }
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('origin/master')
+            description('Pass refspec to the job')
+        }
+        stringParam {
+            name ('GERRIT_PROJECT')
+            defaultValue('att-comdev/cicd')
+            description('Project name on Gerrithub')
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/cicd")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(true)
+                   excludeTrivialRebase(true)
+                   excludeNoCodeChange(true)
+                }
+//FIXME: change the trigger before the merge!!!!:
+//            changeMerged()
+            }
+        }
+    }
+    steps {
+    //WipeOut the workspace:
+        wrappers {
+            preBuildCleanup()
+        }
+        shell(readFileFromWorkspace("${base_path}/superseed.sh"))
+        dsl {
+            external('jobs/seed.groovy')
+            ignoreExisting(true)
+            removeAction('DELETE')
+        }
+    }
+}
