cab5cc2f52a691e6b743de850a82989d9e57809c
vishnusharma964e <vs964e@att.com>
2017-10-16 21:02:01 +0000

    Creating Master seed job for UCP Components
    This PS creates a master seedjob for all of the components in
    att-comdev/cicd/ucp repo
    -Created Jenkinsfile for Shipyard
    Change-Id: I99c017533587043b3ee74c8d9d884d195011cf4c

---
+++ ucp/seed.groovy
@@ -0,0 +1,67 @@
+import groovy.json.JsonSlurper
+
+def imagesJson = '''{ "UCP":[{
+                        "repo":"att-comdev",
+                        "images":[  "deckhand",
+                                    "promenade",
+                                    "shipyard",
+                                    "armada",
+                                    "drydock"]
+                        }]}'''
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.UCP) {
+    for (image in entry.images) {
+        pipelineJob("UCP/${image}/${image}") {
+            parameters {
+                stringParam {
+                    defaultValue(GERRIT_REFSPEC)
+                    description('Pass att-comdev/cicd code refspec to the job')
+                    name ('CICD_GERRIT_REFSPEC')
+                }
+            }
+            triggers {
+                gerritTrigger {
+                    serverName('Gerrithub-jenkins')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern("${entry.repo}/${image}")
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("**")
+                                }
+                            }
+                            //filePaths {
+                             //   filePath {
+                              //  compareType("ANT")
+                               // pattern("$image/**")
+                                //}
+                            //}
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        patchsetCreated {
+                           excludeDrafts(false)
+                           excludeTrivialRebase(false)
+                           excludeNoCodeChange(false)
+                        }
+                        changeMerged()
+                    }
+                }
+
+                definition {
+                    cps {
+                        script(readFileFromWorkspace("ucp/${image}/Jenkinsfile"))
+                        sandbox()
+                    }
+                }
+            }
+        }
+    }
+}
+
