e4172b6fc2430222fd66298d5e401db4d9675f27
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-10-16 16:59:12 -0600

    Add SuperSeed job
    Change-Id: I184815a846712607bd5a90209f34e396cab7b619

---
+++ cicd/SuperSeed/seed.groovy
@@ -0,0 +1,68 @@
+base_path = "cicd/SuperSeed"
+freeStyleJob("${base_path}") {
+    label('master')
+    parameters {
+        stringParam {
+            name ('SEED_PATH')
+            defaultValue('')
+            description('Seed path. Example: cicd/SuperSeed/seed.groovy')
+        }
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('origin/master')
+            description('Gerrit refspec, you can leave SEED_PATH empty if you have refspec')
+        }
+        stringParam {
+            name ('GERRIT_PROJECT')
+            defaultValue('att-comdev/cicd')
+            description('Project name on Gerrithub')
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-voting')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/cicd")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(true)
+                   excludeTrivialRebase(true)
+                   excludeNoCodeChange(true)
+                }
+//FIXME: change the trigger before the merge!!!!:
+//            changeMerged()
+            }
+        }
+    }
+    steps {
+    //Wipe the workspace:
+        wrappers {
+            preBuildCleanup()
+        }
+        shell(readFileFromWorkspace("${base_path}/superseed.sh"))
+        //testing:
+        sh '''
+            echo $0
+            pwd
+            ls
+        '''
+        dsl {
+            external("${env.SEED_PATH}")
+            //Keep disabled jobs. Does is changes the existing jobs?
+            ignoreExisting(true)
+            //removeAction('DISABLE')
+        }
+    }
+}
