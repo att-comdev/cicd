500de7c2967e2494443a3305798b9a36efa2fe1a
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-10-16 16:59:12 -0600

    Add SuperSeed job
    Change-Id: I184815a846712607bd5a90209f34e396cab7b619

---
+++ cicd/SuperSeed/seed.groovy
@@ -0,0 +1,52 @@
+base_path = "cicd/SuperSeed"
+
+pipelineJob("${base_path}") {
+
+//    parameters {
+//        stringParam {
+//            defaultValue(GERRIT_REFSPEC)
+//            description('Pass att-comdev/cicd code refspec to the job')
+//            name ('CICD_GERRIT_REFSPEC')
+//        }
+//    }
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/cicd")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+//FIXME: change before merging:
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(true)
+                   excludeTrivialRebase(true)
+                   excludeNoCodeChange(true)
+                }
+//            changeMerged()
+            }
+        }
+    }
+    steps {
+            shell(readFileFromWorkspace("${base_path}/superseed.sh"))
+        }
+
+//            definition {
+//                cps {
+//                    script(readFileFromWorkspace('jobs/seed.groovy'))
+//                    sandbox()
+//                }
+//            }
+//        }
+//    }
+}
