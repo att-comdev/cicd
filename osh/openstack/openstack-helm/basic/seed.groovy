3b354f4fe00aa18b43b48baab615d15d104b4658
vishnusharma964e <vs964e@att.com>
2017-10-20 18:53:48 +0000

    Updating OSH basic job Jenkinsfile to use 'funcs' for VM Launch
    This PS creates a seed and Jenkinsfile for the basic job in
    openstack/openstack-helm, also adds steps for publishnig logs to
    Nexus and tar balls to Artifactory.
    
    Change-Id: I040c4dbe577ca9f7447bc638e36f8722f95268a7

---
+++ osh/openstack/openstack-helm/basic/seed.groovy
@@ -0,0 +1,39 @@
+pipelineJob("charts/openstack/openstack-helm/basic") {
+
+    triggers {
+        gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/openstack-helm")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
+                }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("osh/openstack/openstack-helm/basic/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
+
+
+
