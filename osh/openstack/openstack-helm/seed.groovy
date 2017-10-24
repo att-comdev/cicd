13f7f3cf67f0485f2c5b288dd051c304b2031c64
Stacey Fletcher <staceylynnfletcher@gmail.com>
2017-10-24 12:20:33 -0500

    Update seed and basic pipeline
    This ps updates the seed to have throttle added and updates
    the basic openstack-helm pipeline.
    
    Change-Id: Iecbea92ce333c0f60a48542b4b624417d653a990

--- osh/openstack/openstack-helm/seed.groovy
+++ osh/openstack/openstack-helm/seed.groovy
@@ -1,78 +1,39 @@
-
-import groovy.json.JsonSlurper
-
-def chartsJson = '''{ "osh":[{
-                        "repo":"openstack/openstack-helm",
-                        "charts":[  "cinder",
-                                    "heat",
-                                    "glance",
-                                    "horizon",
-                                    "keystone",
-                                    "magnum",
-                                    "mistral",
-                                    "neutron",
-                                    "nova",
-                                    "barbican",
-                                    "ceph",
-                                    "dns-helper",
-                                    "etcd",
-                                    "ingress",
-                                    "mariadb",
-                                    "memcached",
-                                    "rabbitmq",
-                                    "senlin"]
-                        },{
-                        "repo":"openstack/openstack-helm-addons",
-                        "charts":[  "elasticsearch",
-                                    "fluentd",
-                                    "kibana",
-                                    "postgresql"]}]}'''
-
-def jsonSlurper = new JsonSlurper()
-def object = jsonSlurper.parseText(chartsJson)
-
-for (entry in object.osh) {
-    for (chart in entry.charts) {
-        pipelineJob("osh/${entry.repo}/${chart}") {
-
-            triggers {
-            // http://<gerrit>/plugin/job-dsl/api-viewer/index.html#path/javaposse.jobdsl.dsl.helpers.properties.PropertiesContext.pipelineTriggers-triggers-gerrit
-                gerritTrigger {
-                    serverName('OS-CommunityGerrit')
-                    gerritProjects {
-                        gerritProject {
-                            compareType('PLAIN')
-                            pattern("${entry.repo}")
-                            branches {
-                                branch {
-                                compareType("ANT")
-                                pattern("**")
-                                }
-                            }
-                            filePaths {
-                                filePath {
-                                compareType("ANT")
-                                pattern("$chart/**")
-                                }
-                                filePath {
-                                compareType("ANT")
-                                pattern("helm-toolkit/**")
-                                }
-                            }
-                            disableStrictForbiddenFileVerification(false)
+pipelineJob("charts/openstack/openstack-helm/basic") {
+    configure { 
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '10'
+        }
+    }
+    triggers {
+        gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/openstack-client")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
                         }
                     }
-                    triggerOnEvents {
-                        changeMerged()
-                    }
+                    disableStrictForbiddenFileVerification(false)
                 }
-
-                definition {
-                    cps {
-                        script(readFileFromWorkspace('osh/openstack/openstack-helm/Jenkinsfile'))
-                        sandbox()
-                    }
+            }
+            triggerOnEvents {
+                changeMerged()
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
                 }
+            }
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace('osh/openstack/openstack-helm/Jenkinsfile'))
+                sandbox()
             }
         }
     }
