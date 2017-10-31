29f951a5b93d8716b0b2bd51a7d069e0f08623c4
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-10-31 00:39:53 -0600

    Add UCP Charts
    Change-Id: I9f56bab9afaec5a3a16b79dba9cfaab540951899

---
+++ ucp/charts/seed.groovy
@@ -0,0 +1,40 @@
+pipelineJob("ucp/charts/all") {
+    triggers {
+        gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/openstack-helm")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    filePaths {
+                        filePath {
+                            compareType(REG_EXP)
+                            pattern(helm-toolkit/.*)
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                changeMerged()
+                patchsetCreated {
+                   excludeDrafts(true)
+                   excludeTrivialRebase(true)
+                   excludeNoCodeChange(true)
+                }
+            }
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace('ucp/charts/Jenkinsfile'))
+                sandbox()
+            }
+        }
+    }
+}
