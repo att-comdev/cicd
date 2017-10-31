729a1ca11a94c4e368c233eb5323387eb2204c85
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-10-31 00:39:53 -0600

    Add UCP Charts
    Change-Id: I9f56bab9afaec5a3a16b79dba9cfaab540951899

---
+++ ucp/charts/seed.groovy
@@ -0,0 +1,58 @@
+folder("UCP/charts")
+pipelineJob("UCP/charts/all") {
+    parameters {
+        stringParam {
+            name ('PROJECTS_LIST')
+            defaultValue('armada shipyard')
+            description('Project name')
+        }
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('master')
+            description('Gerrit refspec or branch')
+        }
+        stringParam {
+            name ('CICD_REFSPEC')
+            defaultValue('refs/changes/65/385065/27')
+            description('refspec that have script')
+        }
+    }
+    triggers {
+        gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/openstack-helm")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    filePaths {
+                        filePath {
+                            compareType('REG_EXP')
+                            pattern('helm-toolkit/.*')
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                changeMerged()
+                patchsetCreated {
+                   excludeDrafts(true)
+                   excludeTrivialRebase(true)
+                   excludeNoCodeChange(true)
+                }
+            }
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace('ucp/charts/Jenkinsfile'))
+                sandbox()
+            }
+        }
+    }
+}
