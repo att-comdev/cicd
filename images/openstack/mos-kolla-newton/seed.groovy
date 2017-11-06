0b51f1893d252ef2111696efb05979ac5c83f5b2
sb464f <sb464f@att.com>
2017-11-06 19:57:38 +0000

    Changed stable/newton to newton-eol Added pipelinejobs for internal mos repositories Change-Id: Iad534bf748e0e30c5784e467d5ffaaf9897f269b

---
+++ images/openstack/mos-kolla-newton/seed.groovy
@@ -0,0 +1,54 @@
+import groovy.json.JsonSlurper
+
+def imagesJson = '''{ "kolla":[{
+                        "repo":"openstack",
+                        "services":[ "cinder",
+                                     "ceilometer",
+                                     "heat",
+                                     "glance",
+                                     "horizon",
+                                     "mistral",
+                                     "keystone",
+                                     "neutron",
+                                     "nova"]
+             }]}'''
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.kolla) {
+    for (service in entry.services) {
+        pipelineJob("images/${entry.repo}/mos-kolla-newton/${service}") {
+
+            triggers {
+                gerritTrigger {
+                    serverName('internal-gerrit')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                          pattern("mos-${service}")
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("main/newton")
+                                }
+                            }
+
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        changeMerged()
+                    }
+                }
+
+                definition {
+                    cps {
+                        script(readFileFromWorkspace('images/openstack/mos-kolla-newton/Jenkinsfile'))
+                        sandbox()
+                    }
+                }
+            }
+        }
+    }
+}
