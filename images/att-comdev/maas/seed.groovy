1c3a0c0bf0482c10bc802868a9bf90bd181f9a7b
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2017-11-09 17:15:50 -0700

    Docker images job for maas
    Change-Id: I357773f7444e3f8952012d13183ed5245721ced3

---
+++ images/att-comdev/maas/seed.groovy
@@ -0,0 +1,78 @@
+import groovy.json.JsonSlurper
+def jsonSlurper = new JsonSlurper()
+
+//add new projects here:
+def MySuperJson = '''{"projects":[
+                        {"name":"maas-rack-controller",
+                         "repo":"att-comdev/maas",
+                         "branch":"**/master",
+                         "path":"images/maas-rack-controller"
+                         },
+                         {"name":"maas-region-controller",
+                         "repo":"att-comdev/maas",
+                         "branch":"**/master",
+                         "path":"images/maas-region-controller"
+                         },
+                         {"name":"sstream-cache",
+                         "repo":"att-comdev/maas",
+                         "branch":"**/master",
+                         "path":"images/sstream-cache"
+                         }
+                    ]}'''
+
+def Json = jsonSlurper.parseText(MySuperJson)
+
+JOB_FOLDER="images/att-comdev/maas"
+folder(JOB_FOLDER)
+for (project in Json.projects) {
+    pipelineJob("${JOB_FOLDER}/${project.name}") {
+//        environmentVariables {
+//            env('GERRIT_EVENT_TYPE',"manual")
+//            env('GERRIT_CHANGE_NUMBER','none')
+//            keepBuildVariables(true)
+//        }
+        triggers {
+            gerritTrigger {
+                silentMode(false)
+                serverName('Gerrithub-jenkins')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("${project.repo}")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        filePaths {
+                            filePath {
+                                compareType("ANT")
+                                pattern("${project.path}")
+                            }
+                            filePath {
+                                compareType("ANT")
+                                pattern("Makefile")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+                triggerOnEvents {
+                    patchsetCreated {
+                        excludeDrafts(true)
+                        excludeTrivialRebase(true)
+                        excludeNoCodeChange(true)
+                     }
+                    changeMerged()
+                }
+            }
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                    sandbox()
+                }
+            }
+        }
+    }
+}
