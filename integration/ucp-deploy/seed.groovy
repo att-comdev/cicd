e6dd3fef2474021ceff0ed70d2ec18d3cae3d245
Kaspars Skels <kaspars.skels@gmail.com>
2017-11-21 12:55:27 -0600

    Pipeline for ucp-deploy
    Change-Id: I86e558054d0f4c90a29bc97cfabd02515b860faa

---
+++ integration/ucp-deploy/seed.groovy
@@ -0,0 +1,48 @@
+
+folder("integration/ucp-deploy")
+
+pipelineJob("integration/ucp-deploy/ucp-deploy") {
+
+    parameters {
+        stringParam {
+            defaultValue(GERRIT_REFSPEC)
+            description('Pass att-comdev/cicd code refspec to the job')
+            name ('CICD_GERRIT_REFSPEC')
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/ucp-deploy")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
+                }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("integration/ucp-deploy/seed.groovy"))
+                sandbox()
+            }
+        }
+    }
+}
+
