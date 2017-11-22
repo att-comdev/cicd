a1475b77d5dd1d5c3ebb3cbfbbc82dee90080782
Kaspars Skels <kaspars.skels@gmail.com>
2017-11-22 15:25:23 -0600

    Pipeline for OpenStack LOCI image builds
    Change-Id: I1a294aa7eb164b950199de13645cf4b7d09c7e9b

---
+++ images/openstack/loci/seed.groovy
@@ -0,0 +1,46 @@
+
+JOB_BASE='images/openstack/loci'
+folder("${JOB_BASE}")
+
+// todo: think if we shall move reqs out from the loop
+// reqs potentially may have different triggers/lifecycle
+PROJECTS = ['requirements', 'keystone', 'heat']
+
+// todo: work on project list.. not all ready yet
+// PROJECTS = ['requirements', 'keystone', 'heat', 'cinder', 'glance', 'horizon', 'neutron', 'nova', 'ironic']
+
+
+PROJECTS.each {
+    pipelineJob("${JOB_BASE}/${project}") {
+        triggers {
+            gerritTrigger {
+                serverName('OS-CommunityGerrit')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("openstack/${project}")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+
+                // todo: look into triggers more
+                triggerOnEvents {
+                    changeMerged()
+                }
+            }
+
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_BASE}/Jenkinsfile"))
+                    sandbox()
+                }
+            }
+        }
+    }
+}
