b272a18e6942de11c7b5148f4a9b4495a6f0fc59
Kochetov Mikhail <mk899x@att.com>
2017-11-29 02:45:36 -0700

    Docker images job for airflow
    Change-Id: Id8d7a596d54c3e44694cbf867092a8a5265e7c9d

---
+++ images/att-comdev/airflow/seed.groovy
@@ -0,0 +1,66 @@
+import groovy.json.JsonSlurper
+def jsonSlurper = new JsonSlurper()
+def AirflowJson = '''{"projects":[
+                        {"name":"airflow",
+                         "repo":"att-comdev/shipyard",
+                         "path":"**"
+                         },
+                        {"name":"armada",
+                         "repo":"att-comdev/armada",
+                         "path":"**"
+                         },
+                        {"name":"drydock",
+                         "repo":"att-comdev/drydock",
+                         "path":"**"
+                         }
+                    ]}'''
+
+def Json = jsonSlurper.parseText(AirflowJson)
+
+JOB_FOLDER="images/att-comdev/airflow"
+folder(JOB_FOLDER)
+pipelineJob("${JOB_FOLDER}/airflow") {
+    triggers {
+        gerritTrigger {
+            silentMode(false)
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                for (project in Json.projects) {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern(project.repo)
+                        branches {
+                            branch {
+                                compareType('ANT')
+                                pattern("**/master")
+                            }
+                        }
+                        filePaths {
+                            filePath {
+                                compareType('ANT')
+                                pattern(project.path)
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(true)
+                    excludeTrivialRebase(true)
+                    excludeNoCodeChange(true)
+                }
+                changeMerged()
+            }
+        }
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+            sandbox()
+        }
+    }
+}
+
+
