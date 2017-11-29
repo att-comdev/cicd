493051053916e61f56797da37c10cb3dd77e842f
Kochetov Mikhail <mk899x@att.com>
2017-11-29 02:45:36 -0700

    Docker images job for airflow
    Change-Id: Id8d7a596d54c3e44694cbf867092a8a5265e7c9d

---
+++ images/att-comdev/airflow/seed.groovy
@@ -0,0 +1,62 @@
+import groovy.json.JsonSlurper
+def jsonSlurper = new JsonSlurper()
+
+PROJECT_FOLDER="images/att-comdev"
+folder("${PROJECT_FOLDER}")
+
+//2DO: Seed should be extended to merge events for armada and drydock
+
+//add new projects here:
+def MySuperJson = '''{"projects":[
+                        {"name":"airflow",
+                         "repo":"att-comdev/shipyard",
+                         "path":"**"
+                         }
+                    ]}'''
+//backup                         "path":"*/images/airflow"
+
+def Json = jsonSlurper.parseText(MySuperJson)
+
+PROJECT_NAME=Json.projects.name[0]
+pipelineJob("${PROJECT_FOLDER}/${PROJECT_NAME}/${PROJECT_NAME}") {
+    triggers {
+        gerritTrigger {
+            silentMode(false)
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern(Json.projects.repo[0])
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    filePaths {
+                        filePath {
+                            compareType('ANT')
+                            pattern(Json.projects.path[0])
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(true)
+                    excludeTrivialRebase(true)
+                    excludeNoCodeChange(true)
+                 }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("${PROJECT_FOLDER}/${PROJECT_NAME}/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
