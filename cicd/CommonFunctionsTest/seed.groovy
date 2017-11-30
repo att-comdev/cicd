d562a639fe5d3a679020778b74da8ce74999f2e6
Kochetov Mikhail <mk899x@att.com>
2017-11-30 02:58:23 -0700

    CommonFunctionsTest Job
    Change-Id: I0e9e19813b64c26e61188326cdff558372826bbb

---
+++ cicd/CommonFunctionsTest/seed.groovy
@@ -0,0 +1,35 @@
+PROJECT_FOLDER="CICD"
+PROJECT_NAME="CommonFunctionsTest"
+
+folder("${PROJECT_FOLDER}")
+pipelineJob("${PROJECT_FOLDER}/${PROJECT_NAME}") {
+    parameters {
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('master')
+            description('current refspec')
+        }
+    }
+    parameters {
+        stringParam {
+            name ('TEST_PARAM1')
+            defaultValue('Test1' )
+            description('Test job parameter')
+        }
+    }
+    parameters {
+        stringParam {
+            name ('TEST_PARAM2')
+            defaultValue('Test2')
+            description('Test job parameter')
+        }
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace("${PROJECT_FOLDER}/${PROJECT_NAME}/Jenkinsfile"))
+            sandbox()
+        }
+    }
+
+}
+
