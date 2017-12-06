d84ece3f5fe43665d5d42b77e7d0776b9b414116
Stacey Fletcher <staceylynnfletcher@gmail.com>
2017-12-06 15:40:54 -0600

    Add publish to Shipyard pipeline
    This ps will add publishing to shipyard
    for patchsets and merge to master images
    
    Change-Id: Icedae6760c15a66f87ff9b4da01494ec35455cd1

---
+++ ucp/shipyard/seed.groovy
@@ -0,0 +1,36 @@
+
+pipelineJob("UCP/shipyard/shipyard") {
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/shipyard")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
+                }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("ucp/shipyard/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
