0f193aa8778039847f8af02eae9ba0aa64c124cf
Kaspars Skels <kaspars.skels@gmail.com>
2017-12-07 14:24:29 -0600

    Virsh based resiliency pipeline for Promenade
    Use resiliency.json manifest to iterate over pipeline stages.
    
    Change-Id: I0fe33c89a14efa23526b64ba0a19d61adf224612

---
+++ ucp/promenade/resiliency/seed.groovy
@@ -0,0 +1,48 @@
+
+JOB_NAME = 'UCP/promenade/resiliency'
+folder(JOB_NAME)
+
+pipelineJob(JOB_NAME) {
+
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '3'
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("att-comdev/promenade")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
+                }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("ucp/promenade/resiliency/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
+
