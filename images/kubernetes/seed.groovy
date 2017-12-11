2a8842fb61a9b8a3359ee7f2f503f316367ad822
Kochetov Mikhail <mk899x@att.com>
2017-12-11 10:42:39 -0700

    Add Kubernetes pipeline
    Change-Id: I69ee7158ef4a42e2b1674b0380b3a26f61c5891d

---
+++ images/kubernetes/seed.groovy
@@ -0,0 +1,46 @@
+
+JOB_FOLDER="images/kubernetes"
+folder(JOB_FOLDER)
+
+import groovy.json.JsonSlurper
+def jsonSlurper = new JsonSlurper()
+
+//add new release branches here:
+def jobsJson = '''{"jobs":[
+                        {"name"  :"kubernetes-1.6",
+                         "branch":"release-1.6"},
+                        {"name"  :"kubernetes-1.8",
+                         "branch":"release-1.8"},
+                        {"name"  :"kubernetes-1.9",
+                         "branch":"release-1.9"},
+                        {"name"  :"kubernetes-master",
+                         "branch":"master"}
+                    ]}'''
+
+def Json = jsonSlurper.parseText(jobsJson)
+
+for (job in Json.jobs){
+    RELEASE_BRANCH=job.branch
+    JOB_NAME=job.name
+    pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+        parameters {
+            stringParam {
+                name ('RELEASE_BRANCH')
+                defaultValue(RELEASE_BRANCH)
+                description('Kube release branch here')
+            }
+        }
+        scm {
+            github('kubernetes/kubernetes', RELEASE_BRANCH)
+        }
+        triggers {
+            scm 'H * * * *'
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
