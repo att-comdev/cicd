bc27d7e9832cd4c68189b2378270be4f89048815
Kochetov Mikhail <mk899x@att.com>
2017-12-11 10:42:39 -0700

    Add Kubernetes pipeline
    Change-Id: I69ee7158ef4a42e2b1674b0380b3a26f61c5891d

---
+++ images/kubernetes/seed.groovy
@@ -0,0 +1,51 @@
+JOB_FOLDER='images'
+JOB_NAME='kubernetes'
+REPO_URL='https://github.com/nycmoma/4jenkins.git'
+RELEASE_BRANCH='release-1.8'
+
+folder(JOB_FOLDER)
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+    parameters {
+        stringParam {
+            name ('RELEASE_BRANCH')
+            defaultValue('release-1.8')
+            description('Kube release branch')
+        }
+    }
+    scm {
+        git(REPO_URL)
+//        github('nycmoma/4jenkins', RELEASE_BRANCH)
+    }
+    triggers {
+        scm('*/2 * * * *')
+//        gitHubPushTrigger()
+   }
+    definition {
+        cps {
+            script(readFileFromWorkspace("${JOB_FOLDER}/${JOB_NAME}/Jenkinsfile"))
+            sandbox()
+        }
+    }
+}
+
+//freeStyleJob("${JOB_FOLDER}/${JOB_NAME}") {
+//    parameters {
+//        stringParam {
+//            name ('RELEASE_BRANCH')
+//            defaultValue('release-1.8')
+//            description('Kube release branch')
+//        }
+//    }
+//    scm {
+//        github('nycmoma/4jenkins', 'release-1.8')
+//    }
+//    triggers {
+//        githubPush()
+//    }
+//    steps {
+//        slack.msg('It works!')
+//    }
+//    publishers {
+//        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+//    }
+//}
