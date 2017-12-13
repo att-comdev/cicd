3f2532f6ec6c9e9e6bd8ad514c75f1198a8ff1cd
Kaspars Skels <kaspars.skels@gmail.com>
2017-12-13 12:25:26 -0600

    Shared libary for pipelines
    Reuse existing funcs.groovy into structure required by
    Shared Groovy Libraries Plugin.
    
    Add template for single slave job that can be reused for pipelines
    that require single slave (sends Slack message on failure).
    
    Change-Id: I681f7bd25636010efb1b53e3789f36a091e01ce9

---
+++ vars/slack.groovy
@@ -0,0 +1,12 @@
+
+def slack_msg(String msg, String channel=SLACK_DEFAULT_CHANNEL){
+// Usage example:  funcs.slack_msg( "${env.GERRIT_CHANGE_URL} is OK!")
+// Custom channel: funcs.slack_msg( "${env.GERRIT_CHANGE_URL} is OK!",'#my_channel')
+    slackSend(
+        baseUrl: SLACK_URL,
+        tokenCredentialId: 'jenkins-slack',
+        channel: channel,
+        message: "Job <${env.JOB_URL}|${env.JOB_NAME}> said:\n" + msg
+    )
+}
+
