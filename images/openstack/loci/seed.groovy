a18ddb7ab53d73b34d9202979a58d6e8a677615e
Kaspars Skels <kaspars.skels@gmail.com>
2017-12-14 17:32:49 -0600

    Improvements to OpenStack LOCI pipeline
    Align LOCI build pipeline to utilize shared libs
    Merge community/mos seeds in a single file
    Add explicit base image as build argument
    
    Change-Id: I98d3c769b6be5a5e75734ba823f471f0c5a298b7

---
+++ images/openstack/loci/seed.groovy
@@ -0,0 +1,144 @@
+
+JOB_BASE='images/openstack/loci'
+
+folder("${JOB_BASE}/community")
+folder("${JOB_BASE}/mos")
+
+// { project: 'ref' }
+COMMUNITY_PROJECTS = ['requirements': 'stable/newton',
+            'keystone': 'newton-eol',
+            'heat': 'newton-eol']
+//            'cinder': 'newton-eol',
+//            'glance': 'newton-eol',
+//            'horizon': 'newton-eol',
+//            'nova': 'stable/newton',
+//            'ironic': 'stable/newton']
+
+MOS_PROJECTS = ['mos-keystone': 'main/newton',
+                'mos-heat': 'main/newton']
+//                'mos-cinder': 'main/newton',
+//                'mos-glance': 'main/newton',
+//                'mos-horizon': 'main/newton',
+//                'mos-nova': 'main/newton',
+//                'mos-ironic': 'main/newton']
+
+
+COMMUNITY_PROJECTS.each { project, ref ->
+
+    pipelineJob("${JOB_BASE}/community/${project}") {
+
+        // limit surge of patchsets
+        configure {
+            node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                durationName 'hour'
+                count '3'
+            }
+        }
+
+        parameters {
+            stringParam {
+                defaultValue(ref)
+                description('Default branch for manual build.\n\n' +
+                            'Currently master, stable/<branch>, and newton-eol are supported')
+                name ('PROJECT_REF')
+            }
+        }
+
+        triggers {
+            gerritTrigger {
+                serverName('OS-CommunityGerrit')
+                silentMode(true)
+
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("openstack/${project}")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("stable/*")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+
+                triggerOnEvents {
+                    patchsetCreated {
+                       excludeDrafts(false)
+                       excludeTrivialRebase(false)
+                       excludeNoCodeChange(false)
+                    }
+                    changeMerged()
+                }
+            }
+
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_BASE}/Jenkinsfile"))
+                    sandbox()
+                }
+            }
+        }
+    }
+}
+
+
+MOS_PROJECTS.each { project, ref ->
+
+    pipelineJob("${JOB_BASE}/mos/${project}") {
+
+        // limit surge of patchsets
+        configure {
+            node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                durationName 'hour'
+                count '3'
+            }
+        }
+
+        parameters {
+            stringParam {
+                defaultValue(ref)
+                description('Default branch for manual build.\n\n' +
+                            'Currently main/newton is supported.')
+                name ('PROJECT_REF')
+            }
+        }
+
+        triggers {
+            gerritTrigger {
+                serverName('internal-gerrit')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern(project)
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("main/newton")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+
+                triggerOnEvents {
+                    patchsetCreated {
+                       excludeDrafts(false)
+                       excludeTrivialRebase(false)
+                       excludeNoCodeChange(false)
+                    }
+                    changeMerged()
+                }
+            }
+
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_BASE}/Jenkinsfile"))
+                    sandbox()
+                }
+            }
+        }
+    }
+}
+
