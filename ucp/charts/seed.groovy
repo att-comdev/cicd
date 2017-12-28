c9a58abb45e1546a1d367cb6f6395796b564e55e
Kochetov Mikhail <mk899x@att.com>
2017-12-28 10:16:01 -0700

    Add Charts jobs for armada deckhand drydock promenade shipyard
    Change-Id: I2aa4b43ddd602dedc09879ace11f64d84d7d3d96

--- ucp/charts/seed.groovy
+++ ucp/charts/seed.groovy
@@ -1,63 +1,69 @@
-folder("UCP/charts")
-pipelineJob("UCP/charts/helm-toolkit-compatibility") {
-    parameters {
-        stringParam {
-            name ('PROJECT_LIST')
-            defaultValue('armada deckhand drydock promenade shipyard')
-            description('UCP Projects')
-        }
-        stringParam {
-            name ('GERRIT_REFSPEC')
-            defaultValue('master')
-            description('Gerrit refspec or branch')
-        }
-        stringParam {
-            name ('GERRIT_CHANGE_NUMBER')
-            defaultValue('0')
-            description('patchset number')
-        }
-        stringParam {
-            name ('GERRIT_EVENT_TYPE')
-            defaultValue('patchset-created')
-            description('patchset-created or change-merged')
-        }
-    }
-    triggers {
-        gerritTrigger {
-            serverName('OS-CommunityGerrit')
-            silentMode(true)
-            gerritProjects {
-                gerritProject {
-                    compareType('PLAIN')
-                    pattern("openstack/openstack-helm")
-                    branches {
-                        branch {
-                            compareType("ANT")
-                            pattern("**")
-                        }
-                    }
-                    filePaths {
-                        filePath {
-                            compareType('REG_EXP')
-                            pattern('helm-toolkit/.*')
-                        }
-                    }
-                    disableStrictForbiddenFileVerification(false)
-                }
+JOB_FOLDER="UCP/charts"
+folder(JOB_FOLDER)
+
+def projects = ['armada':'charts/.*',
+                'deckhand':'charts/.*',
+                'drydock':'charts/.*',
+                'promenade':'charts/.*',
+                'shipyard':'charts/.*']
+
+projects.each { project_name, file_path ->
+    JOB_NAME=project_name
+    pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+        parameters {
+            stringParam {
+                name ('GERRIT_REFSPEC')
+                defaultValue('master')
+                description('Gerrit refspec or branch')
             }
-            triggerOnEvents {
-                changeMerged()
-                patchsetCreated {
-                   excludeDrafts(true)
-                   excludeTrivialRebase(true)
-                   excludeNoCodeChange(true)
-                }
+            stringParam {
+                name ('GERRIT_CHANGE_NUMBER')
+                defaultValue('0')
+                description('patchset number')
+            }
+            stringParam {
+                name ('GERRIT_EVENT_TYPE')
+                defaultValue('patchset-created')
+                description('patchset-created or change-merged')
             }
         }
-        definition {
-            cps {
-                script(readFileFromWorkspace('ucp/charts/Jenkinsfile'))
-                sandbox()
+        triggers {
+            gerritTrigger {
+                serverName('Gerrithub-jenkins')
+//                silentMode(true)
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("att-comdev/${project_name}")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        filePaths {
+                            filePath {
+                                compareType('REG_EXP')
+                                pattern("${file_path}")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+                triggerOnEvents {
+                    changeMerged()
+                    patchsetCreated {
+                       excludeDrafts(true)
+                       excludeTrivialRebase(true)
+                       excludeNoCodeChange(true)
+                    }
+                }
+            }
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                    sandbox()
+                }
             }
         }
     }
