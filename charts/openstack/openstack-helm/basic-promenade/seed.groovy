415a4d18c30407fa6017dfd87d07f03c5022c208
Kaspars Skels <kaspars.skels@gmail.com>
2018-01-02 12:58:19 -0600

    OSH gate using Promenade k8s
    Change-Id: I1638fff339f0b21be94d8ef0d8bd1fee791acb92

---
+++ charts/openstack/openstack-helm/basic-promenade/seed.groovy
@@ -0,0 +1,48 @@
+
+JOB_BASE = 'charts/openstack/openstack-helm/basic-promenade'
+folder(JOB_BASE)
+
+pipelineJob(JOB_BASE) {
+
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '3'
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/openstack-helm")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
+                }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("${JOB_BASE}/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
+
