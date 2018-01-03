81092e2dd845fafac957f00d4b756710d1470135
Kochetov Mikhail <mk899x@att.com>
2018-01-02 18:05:10 -0700

    Rework of helm-toolkit-compatibility job.
    Change-Id: I8917964af6f6b88594d85b9eed36d7938cba4a7b

---
+++ charts/openstack/helm-toolkit-compatibility/seed.groovy
@@ -0,0 +1,56 @@
+JOB_FOLDER="charts/openstack"
+JOB_NAME="${JOB_FOLDER}/helm-toolkit-compatibility"
+folder(JOB_FOLDER)
+pipelineJob(JOB_NAME) {
+    parameters {
+        stringParam {
+            name ('PROJECT_LIST')
+            defaultValue('armada deckhand drydock promenade shipyard')
+            description('UCP Projects list')
+        }
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('master')
+            description('Gerrit refspec or branch in openstack-helm repo')
+        }
+    }
+    triggers {
+        gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            silentMode(true)
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/openstack-helm")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    filePaths {
+                        filePath {
+                            compareType('REG_EXP')
+                            pattern('helm-toolkit/.*')
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                changeMerged()
+                patchsetCreated {
+                   excludeDrafts(true)
+                   excludeTrivialRebase(true)
+                   excludeNoCodeChange(true)
+                }
+            }
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace("${JOB_NAME}/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
