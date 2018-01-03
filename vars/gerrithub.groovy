3a896bcb47568e36e2aed9eb049462809504c8fa
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-01-03 12:26:14 -0600

    Checkout to specific directory method
    This method will allow a cloned gerrithub repo
    to be checked out to a specific directory.
    
    Change-Id: Ibd15de00df7c2453594867847c8df45fc8cb9daf

--- vars/gerrithub.groovy
+++ vars/gerrithub.groovy
@@ -12,3 +12,16 @@ def clone(String project, String refspec){
          url: "https://review.gerrithub.io/" + project ]]]
 }
 
+def clone(String project, String refspec, String relativeTargetDirectory){
+// Usage example: gerrithub.clone("att-comdev/cicd", "origin/master", "armada")
+    checkout poll: false,
+    scm: [$class: 'GitSCM',
+         branches: [[name: refspec]],
+         doGenerateSubmoduleConfigurations: false,
+         extensions: [[$class: 'RelativeTargetDirectory',
+                       relativeTargetDir: relativeTargetDirectory]],
+         submoduleCfg: [],
+         userRemoteConfigs: [[refspec: 'refs/changes/*:refs/changes/*',
+                              url: "https://review.gerrithub.io/" + project ]]]
+}
+
