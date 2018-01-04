f41e8370dca8f984510bc56e0382fbe5ca0734ae
Kaspars Skels <kaspars.skels@gmail.com>
2018-01-03 18:57:58 -0600

    Single node OSH dev pipeline
    Change-Id: I0f9cd66f4393d7f0bdd1d357f334820f760c2af9

---
+++ charts/openstack/openstack-helm/single-dev/seed.groovy
@@ -0,0 +1,49 @@
+
+JOB_BASE = 'charts/openstack/openstack-helm/single-dev'
+
+pipelineJob(JOB_BASE) {
+
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '3'
+        }
+    }
+
+    triggers {
+        gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            silentMode(true)
+
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/openstack-helm")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                   excludeDrafts(false)
+                   excludeTrivialRebase(false)
+                   excludeNoCodeChange(false)
+                }
+                changeMerged()
+            }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("${JOB_BASE}/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
+
