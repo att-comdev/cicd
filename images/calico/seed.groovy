9f871f66f199892314fcd21d9076abaf2955871c
Kaspars Skels <kaspars.skels@gmail.com>
2018-01-09 20:03:16 -0600

    Image pipeline for Calico
    Change-Id: Ie985ff49b51f35c011a54a74bf9100e8baaa4343

---
+++ images/calico/seed.groovy
@@ -0,0 +1,48 @@
+
+JOB_FOLDER = 'images/calico'
+folder(JOB_FOLDER)
+
+pipelineJob("${JOB_FOLDER}/calico") {
+
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '3'
+        }
+    }
+
+    # triggers {
+    #     gerritTrigger {
+    #         serverName('Gerrithub-jenkins')
+    #         gerritProjects {
+    #             gerritProject {
+    #                 compareType('PLAIN')
+    #                 pattern("att-comdev/deckhand")
+    #                 branches {
+    #                     branch {
+    #                         compareType("ANT")
+    #                         pattern("**")
+    #                     }
+    #                 }
+    #                 disableStrictForbiddenFileVerification(false)
+    #             }
+    #         }
+    #         triggerOnEvents {
+    #             patchsetCreated {
+    #                excludeDrafts(false)
+    #                excludeTrivialRebase(false)
+    #                excludeNoCodeChange(false)
+    #             }
+    #             changeMerged()
+    #         }
+    #     }
+    # }
+
+    definition {
+        cps {
+            script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+            sandbox()
+        }
+    }
+}
+
