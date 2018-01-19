977da65034a9170a5d4ca79bc4a20411695fb08a
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-01-19 10:58:48 -0600

    Add promenade jobs to Jenkins staging environment
    This patchset is getting the Stage environment in sync
    with Production Jenkins.
    
    Change-Id: Ib5f4cb787d4a24bf0cfa1bd6ab5e5a33bb35e848

---
+++ images/att-comdev/promenade/seed.groovy
@@ -0,0 +1,61 @@
+import groovy.json.JsonSlurper
+
+def imagesJson = '''{ "images":[{
+                        "repo":"att-comdev/promenade",
+                        "pipelineNames":[
+                                  "promenade",
+                                  "resiliency"]
+                        }]}'''
+                        
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.images) {
+    for ( pipelineName entry.pipelineNames) {
+        pipelineJob("images/${entry.repo}/${pipelineName}") {
+            configure {
+                node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                    durationName 'hour'
+                    count '3'
+                }
+            }
+
+            triggers {
+                gerritTrigger {
+                    serverName('Gerrithub-jenkins')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern("${entry.repo}")
+                            branches {
+                                branch {
+                                    compareType("ANT")
+                                    pattern("**")
+                                }
+                            }
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        patchsetCreated {
+                        excludeDrafts(false)
+                        excludeTrivialRebase(false)
+                        excludeNoCodeChange(false)
+                        }
+                        changeMerged()
+                        commentAddedContains {
+                        commentAddedCommentContains('recheck')
+                        }
+                    }
+                }
+
+                definition {
+                    cps {
+                        script(readFileFromWorkspace("images/${entry.repo}/${pipelineName}/Jenkinsfile"))
+                        sandbox()
+                    }
+                }
+            }
+        }
+    }
+}
