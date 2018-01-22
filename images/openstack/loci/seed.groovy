9df95b9539c565b689ad47faffb88554439fc71f
Kaspars Skels <kaspars.skels@gmail.com>
2018-01-22 15:36:41 -0600

    Enable additional OS LOCI projects
    Change-Id: I0c43ed6bbbf798d7af71895426c7da52dd325675

--- images/openstack/loci/seed.groovy
+++ images/openstack/loci/seed.groovy
@@ -7,12 +7,11 @@ folder("${JOB_BASE}/mos")
 // { project: 'ref' }
 COMMUNITY_PROJECTS = ['requirements': 'stable/newton',
             'keystone': 'newton-eol',
-            'heat': 'newton-eol']
-//            'cinder': 'newton-eol',
-//            'glance': 'newton-eol',
-//            'horizon': 'newton-eol',
-//            'nova': 'stable/newton',
-//            'ironic': 'stable/newton']
+            'heat': 'newton-eol',
+            'glance': 'newton-eol',
+            'cinder': 'newton-eol',
+            'neutron': 'newton-eol',
+            'nova': 'stable/newton']
 
 MOS_PROJECTS = ['mos-keystone': 'main/newton',
                 'mos-heat': 'main/newton']
@@ -31,7 +30,7 @@ COMMUNITY_PROJECTS.each { project, ref ->
         configure {
             node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
                 durationName 'hour'
-                count '3'
+                count '300'
             }
         }
 
