e4bd36e9c9874793ac32d1491a97d3b844cfe74e
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-01-30 09:47:25 -0600

    This adds a ranger and ranger-agent pipeline
    Gives the ranger-agent team the ability to publish images
    to Artifactory
    
    Change-Id: If5a289018dd5fdf7264e4e9c3e83a52382a55f5f

---
+++ images/x/ranger/seed.groovy
@@ -0,0 +1,59 @@
+import groovy.json.JsonSlurper
+
+def imagesJson = '''{ "os":[{
+                        "repo":"x/ranger",
+                        "images":[
+                                  "ranger",
+                                  "rangercli"
+                                  ]
+                        },{
+                        "repo":"x/ranger-agent",
+                        "images":["ranger-agent"]}]}'''
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(imagesJson)
+folder("images")
+folder("images/x")
+
+for (entry in object.os) {
+    for (image in entry.images) {
+      pipelineJob("images/x/${image}") {
+            logRotator{
+              daysToKeep(90)
+            }
+            triggers {
+                gerritTrigger {
+                    serverName('OS-CommunityGerrit')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern("${entry.repo}")
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("**")
+                                }
+                            }
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        patchsetCreated {
+                           excludeDrafts(false)
+                           excludeTrivialRebase(false)
+                           excludeNoCodeChange(false)
+                        }
+                        changeMerged()
+                    }
+                }
+
+                definition {
+                    cps {
+                      script(readFileFromWorkspace("images/x/ranger/Jenkinsfile"))
+                      sandbox(false)
+                    }
+                }
+            }
+        }
+    }
+}
