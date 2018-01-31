812c58d24e7496d9c9aa7e924339870cc41d75ea
Kochetov, Mikhail (mk899x) <mk899x@us.att.com>
2018-01-31 14:33:41 -0700

    Add more tags into CoreDNS Img pipeline
    Change-Id: I030df1f234dd23d5228006caef43f1ce8ba14380

--- images/coredns/seed.groovy
+++ images/coredns/seed.groovy
@@ -3,13 +3,6 @@ JOB_NAME='coredns'
 
 folder(JOB_FOLDER)
 pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
-    parameters {
-        stringParam {
-            name ('GO_VERSION')
-            defaultValue('1.9.3')
-            description('GoLang version')
-        }
-    }
     scm {
         github('coredns/coredns', 'master')
     }
