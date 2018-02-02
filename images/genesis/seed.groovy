b157ae4756d032f0f59516e8ecd3c98160cf7905
Roman Gorshunov <rg445d@intl.att.com>
2018-02-02 11:34:24 -0500

    [WIP] [CCICD-346] Genesis node autodeploy
    Change-Id: I769a630e441a4861e1c7481bc6e4b155e96428ce

---
+++ images/genesis/seed.groovy
@@ -0,0 +1,57 @@
+JOB_FOLDER='images'
+JOB_NAME='genesis'
+
+folder(JOB_FOLDER)
+
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+
+    displayName('Genesis builder')
+    description('This job is supposed to build ISO and boot/deploy Genesis node')
+
+    logRotator {
+        numToKeep(5)
+        artifactNumToKeep(5)
+    }
+
+    parameters {
+        stringParam {
+            name ('iDracIpAddress')
+            defaultValue('')
+            description('iDRAC IP address')
+        }
+        stringParam {
+            name ('iDracUser')
+            defaultValue('')
+            description('iDRAC user')
+        }
+        password {
+            name ('iDracPassword')
+            defaultValue('')
+            description('iDRAC user password')
+        }
+        stringParam {
+            name ('preseedUrl')
+            defaultValue('')
+            description('Location of preseed.cfg, http(s) URL')
+        }
+        stringParam {
+            name ('bootIso')
+            defaultValue('')
+            description('Location of ISO to boot server from')
+        }
+        nodeParam('runOnHost') {
+            description('Select node for the Job to run on')
+            eligibility('IgnoreOfflineNodeEligibility')
+            trigger('multiSelectionDisallowed')
+        }
+    }
+
+    definition {
+        cps {
+            sandbox()
+            script(readFileFromWorkspace("${JOB_FOLDER}/${JOB_NAME}/Jenkinsfile"))
+        }
+    }
+
+}
+
