3b457da92448419c06d0b2415af3704a5d5ad2fe
Roman Gorshunov <rg445d@intl.att.com>
2018-02-02 11:34:24 -0500

    [WIP] [CCICD-346] Genesis node autodeploy
    Change-Id: I769a630e441a4861e1c7481bc6e4b155e96428ce

---
+++ images/genesis/seed.groovy
@@ -0,0 +1,15 @@
+JOB_FOLDER='images'
+JOB_NAME='genesis'
+
+folder(JOB_FOLDER)
+
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+    parameters {
+        stringParam {
+            name ('ipAddress')
+            defaultValue('8.8.8.8')
+            description('iDRAC IP address')
+        }
+    }
+}
+
