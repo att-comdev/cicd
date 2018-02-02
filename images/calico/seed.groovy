a40207931a4840044991d5ae51bbea20f99476ac
sb464f <sb464f@att.com>
2018-02-02 17:08:01 +0000

    Docker images pipeline for Calico
    Change-Id: Iefa66a8c8620604e4393756eaed45e0a6c8cc8e1

---
+++ images/calico/seed.groovy
@@ -0,0 +1,32 @@
+JOB_BASE="images/calico"
+
+folder(JOB_BASE)
+
+COMMUNITY_PROJECTS = ['cni': 'cni-plugin',
+                      'kube-controllers': 'kube-controllers',
+                      'node': 'calico']
+
+COMMUNITY_PROJECTS.each { project, repo ->
+
+    pipelineJob("${JOB_FOLDER}/${project}") {
+        parameters {
+            stringParam {
+                defaultValue("${repo}")
+                description('Paas job repo')
+                name ('JOB_REPO')
+            }
+        }
+        scm {
+            github("projectcalico/${repo}", "master")
+        }
+        triggers {
+            scm 'H * * * *'
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
