92be2eabb18f01c2a8c49f31ba68a8c2f1ca3e86
Milind Belhe <milindvb@gmail.com>
2018-02-08 12:50:49 -0600

    Updating PROJECT build-arg for mos builds
    Change-Id: Iac77eaf91cc6904d7f83c13da978c475252bda1e

--- images/openstack/loci/seed.groovy
+++ images/openstack/loci/seed.groovy
@@ -5,20 +5,21 @@ folder("${JOB_BASE}/community")
 folder("${JOB_BASE}/mos")
 
 // { project: 'ref' }
-COMMUNITY_PROJECTS = ['requirements': 'stable/newton',
-                      'keystone': 'newton-eol',
-                      'heat': 'newton-eol',
-                      'glance': 'newton-eol',
-                      'cinder': 'newton-eol',
-                      'neutron': 'newton-eol',
-                      'nova': 'stable/newton']
+COMMUNITY_PROJECTS = ['requirements': 'stable/ocata',
+                      'keystone': 'stable/ocata',
+                      'heat': 'stable/ocata',
+                      'glance': 'stable/ocata',
+                      'cinder': 'stable/ocata',
+                      'neutron': 'stable/ocata',
+                      'nova': 'stable/ocata']
 
-MOS_PROJECTS = ['mos-keystone': 'main/newton',
-                'mos-heat': 'main/newton',
-                'mos-glance': 'main/newton',
-                'mos-cinder': 'main/newton',
-                'mos-neutron': 'main/newton',
-                'mos-nova': 'main/newton']
+// master is ocata branch for mos
+MOS_PROJECTS = ['mos-keystone': 'master',
+                'mos-heat': 'master',
+                'mos-glance': 'master',
+                'mos-cinder': 'master',
+                'mos-neutron': 'master',
+                'mos-nova': 'master']
 
 
 COMMUNITY_PROJECTS.each { project, ref ->
@@ -113,7 +114,7 @@ MOS_PROJECTS.each { project, ref ->
                         branches {
                             branch {
                                 compareType("ANT")
-                                pattern("main/newton")
+                                pattern("master")
                             }
                         }
                         disableStrictForbiddenFileVerification(false)
