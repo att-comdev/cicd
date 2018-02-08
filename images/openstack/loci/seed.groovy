d06f0d0c24d42a4e4bbe2642b304da746a41d037
Milind Belhe <milindvb@gmail.com>
2018-02-08 12:50:49 -0600

    Updating PROJECT build-arg for mos builds
    Change-Id: Iac77eaf91cc6904d7f83c13da978c475252bda1e

--- images/openstack/loci/seed.groovy
+++ images/openstack/loci/seed.groovy
@@ -4,21 +4,34 @@ JOB_BASE='images/openstack/loci'
 folder("${JOB_BASE}/community")
 folder("${JOB_BASE}/mos")
 
+LOCI_BASE_IMAGE="${ARTF_DOCKER_URL}/ubuntu:xenial"
+LOCI_SRIOV_BASE_IMAGE="${ARTF_DOCKER_URL}/ubuntu:18.04"
 // { project: 'ref' }
-COMMUNITY_PROJECTS = ['requirements': 'stable/newton',
-                      'keystone': 'newton-eol',
-                      'heat': 'newton-eol',
-                      'glance': 'newton-eol',
-                      'cinder': 'newton-eol',
-                      'neutron': 'newton-eol',
-                      'nova': 'stable/newton']
+COMMUNITY_PROJECTS = ['requirements': 'stable/ocata',
+                      'keystone': 'stable/ocata',
+                      'heat': 'stable/ocata',
+                      'glance': 'stable/ocata',
+                      'cinder': 'stable/ocata',
+                      'neutron': 'stable/ocata',
+                      'nova': 'stable/ocata']
 
-MOS_PROJECTS = ['mos-keystone': 'main/newton',
-                'mos-heat': 'main/newton',
-                'mos-glance': 'main/newton',
-                'mos-cinder': 'main/newton',
-                'mos-neutron': 'main/newton',
-                'mos-nova': 'main/newton']
+// master is ocata branch for mos
+MOS_PROJECTS = ['mos-keystone': 'master',
+                'mos-heat': 'master',
+                'mos-glance': 'master',
+                'mos-cinder': 'master',
+                'mos-neutron': 'master',
+                'mos-nova': 'master',
+                'mos-horizon': 'master']
+
+MOS_BASELINE_PROJECTS = ['mos-keystone': 'master/ocata',
+                'mos-heat': 'master/ocata',
+                'mos-glance': 'master/ocata',
+                'mos-cinder': 'master/ocata',
+                'mos-neutron': 'master/ocata',
+                'mos-nova': 'master/ocata',
+                'mos-horizon': 'master/ocata',
+                'mos-neutron-sriov':'master/ocata']
 
 
 COMMUNITY_PROJECTS.each { project, ref ->
@@ -113,7 +126,78 @@ MOS_PROJECTS.each { project, ref ->
                         branches {
                             branch {
                                 compareType("ANT")
-                                pattern("main/newton")
+                                pattern("master")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+
+                triggerOnEvents {
+                    patchsetCreated {
+                       excludeDrafts(false)
+                       excludeTrivialRebase(false)
+                       excludeNoCodeChange(false)
+                    }
+                    changeMerged()
+                }
+            }
+
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_BASE}/Jenkinsfile"))
+                    sandbox()
+                }
+            }
+        }
+    }
+}
+//temporary
+MOS_BASELINE_PROJECTS.each { project, ref ->
+
+    pipelineJob("${JOB_BASE}/mos-baseline/${project}") {
+
+        // limit surge of patchsets
+        configure {
+            node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                durationName 'hour'
+                count '3'
+            }
+        }
+
+        parameters {
+            stringParam {
+                defaultValue(ref)
+                description('Default branch for manual build.\n\n' +
+                            'Currently main/newton is supported.')
+                name ('PROJECT_REF')
+            }
+          if("${project}".equals("mos-neutron-sriov")){
+            stringParam {
+                defaultValue("${LOCI_SRIOV_BASE_IMAGE}")
+                description('Image needed for SR-IOV')
+                name ('LOCI_BASE_IMAGE')
+            } 
+          } else {
+            stringParam {
+                defaultValue("${LOCI_BASE_IMAGE}")
+                description('Image needed for 16.04')
+                name ('LOCI_BASE_IMAGE')
+            }                              
+          }
+        }
+
+        triggers {
+            gerritTrigger {
+                serverName('internal-gerrit')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern(project)
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("master")
                             }
                         }
                         disableStrictForbiddenFileVerification(false)
