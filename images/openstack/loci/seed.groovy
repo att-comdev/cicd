1f2886d2bb7031d7c55a303a9b3ba2e890eafe67
Milind Belhe <milindvb@gmail.com>
2018-02-08 12:50:49 -0600

    Updating PROJECT build-arg for mos builds
    Change-Id: Iac77eaf91cc6904d7f83c13da978c475252bda1e

--- images/openstack/loci/seed.groovy
+++ images/openstack/loci/seed.groovy
@@ -1,29 +1,35 @@
 
 JOB_BASE='images/openstack/loci'
 
-folder("${JOB_BASE}/community")
-folder("${JOB_BASE}/mos")
-
+folder("${JOB_BASE}/community/ocata")
+folder("${JOB_BASE}/ocata/mos")
+SEMANTIC_RELEASE_VERSION="0.8"
+LOCI_BASE_IMAGE="${ARTF_DOCKER_URL}/ubuntu:xenial"
+LOCI_SRIOV_BASE_IMAGE="${ARTF_DOCKER_URL}/ubuntu:18.04"
 // { project: 'ref' }
-COMMUNITY_PROJECTS = ['requirements': 'stable/newton',
-                      'keystone': 'newton-eol',
-                      'heat': 'newton-eol',
-                      'glance': 'newton-eol',
-                      'cinder': 'newton-eol',
-                      'neutron': 'newton-eol',
-                      'nova': 'stable/newton']
+COMMUNITY_PROJECTS = ['requirements': 'stable/ocata',
+                      'keystone': 'stable/ocata',
+                      'heat': 'stable/ocata',
+                      'glance': 'stable/ocata',
+                      'cinder': 'stable/ocata',
+                      'neutron': 'stable/ocata',
+                      'nova': 'stable/ocata']
 
-MOS_PROJECTS = ['mos-keystone': 'main/newton',
-                'mos-heat': 'main/newton',
-                'mos-glance': 'main/newton',
-                'mos-cinder': 'main/newton',
-                'mos-neutron': 'main/newton',
-                'mos-nova': 'main/newton']
+// master is ocata branch for mos
+MOS_PROJECTS = ['mos-keystone': 'master',
+                'mos-heat': 'master',
+                'mos-glance': 'master',
+                'mos-cinder': 'master',
+                'mos-neutron': 'master',
+                'mos-nova': 'master',
+                'mos-horizon': 'master',
+                'mos-neutron-sriov':'master',
+                'mos-nova-1804':'master']
 
 
 COMMUNITY_PROJECTS.each { project, ref ->
 
-    pipelineJob("${JOB_BASE}/community/${project}") {
+    pipelineJob("${JOB_BASE}//community/ocata/${project}") {
 
         // limit surge of patchsets
         configure {
@@ -39,6 +45,11 @@ COMMUNITY_PROJECTS.each { project, ref ->
                 description('Default branch for manual build.\n\n' +
                             'Currently master, stable/<branch>, and newton-eol are supported')
                 name ('PROJECT_REF')
+            }
+            stringParam {
+                defaultValue("${LOCI_BASE_IMAGE}")
+                description('Image needed for 16.04')
+                name ('LOCI_BASE_IMAGE')
             }
         }
 
@@ -82,10 +93,11 @@ COMMUNITY_PROJECTS.each { project, ref ->
 }
 
 
+//temporary
 MOS_PROJECTS.each { project, ref ->
-
-    pipelineJob("${JOB_BASE}/mos/${project}") {
-
+    pipelineJob("${JOB_BASE}/mos/ocata/${project}") {
+        def propsPrefix = "${project}".split('-')[0]
+        def propsSuffix = "${project}".split('-')[1]
         // limit surge of patchsets
         configure {
             node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
@@ -101,11 +113,27 @@ MOS_PROJECTS.each { project, ref ->
                             'Currently main/newton is supported.')
                 name ('PROJECT_REF')
             }
+          if(("${project}"==("mos-neutron-sriov")) || ("${project}"==("mos-nova-1804"))){
+            stringParam {
+                defaultValue("${LOCI_SRIOV_BASE_IMAGE}")
+                description('Image needed for SR-IOV')
+                name ('LOCI_BASE_IMAGE')
+            }
+          } else {
+            stringParam {
+                defaultValue("${LOCI_BASE_IMAGE}")
+                description('Image needed for 16.04')
+                name ('LOCI_BASE_IMAGE')
+            }
+          }
+          stringParam('RELEASE_CURRENT_KEY',"RDM9.images.${propsPrefix}.${propsSuffix}.dev.current")
+          stringParam('RELEASE_STATUS_KEY',"RDM9.images.${propsPrefix}.${propsSuffix}.dev.status")
         }
 
         triggers {
             gerritTrigger {
-                serverName('internal-gerrit')
+                silentMode(true)
+                serverName('mtn5-gerrit')
                 gerritProjects {
                     gerritProject {
                         compareType('PLAIN')
@@ -113,7 +141,7 @@ MOS_PROJECTS.each { project, ref ->
                         branches {
                             branch {
                                 compareType("ANT")
-                                pattern("main/newton")
+                                pattern("master")
                             }
                         }
                         disableStrictForbiddenFileVerification(false)
