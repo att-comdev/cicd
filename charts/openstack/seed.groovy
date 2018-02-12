352966afa2c363acf82faaefc5f9e783fbe732e5
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-02-12 15:29:18 -0600

    Cluster VM to deploy OSH charts
    This ps brings up a k8s cluster with 5 VMs that will be used
    to deploy OSH charts to.
    
    Change-Id: Idabe0554247e212bc0463ef393b0ade7754a97f0

---
+++ charts/openstack/seed.groovy
@@ -0,0 +1,102 @@
+
+import groovy.json.JsonSlurper
+
+def chartsJson = '''{ "osh":[{
+                        "repo":"openstack/openstack-helm",
+                        "charts":[  "cinder",
+                                    "heat",
+                                    "glance",
+                                    "horizon",
+                                    "keystone",
+                                    "magnum",
+                                    "mistral",
+                                    "neutron",
+                                    "nova",
+                                    "barbican",
+                                    "ceph",
+                                    "dns-helper",
+                                    "etcd",
+                                    "ingress",
+                                    "mariadb",
+                                    "memcached",
+                                    "rabbitmq",
+                                    "senlin"]
+                        },{
+                        "repo":"openstack/openstack-helm-addons",
+                        "charts":[
+                                    "artifactory",
+                                    "jenkins"
+                                 ]},{
+                        "repo":"openstack/openstack-helm-infra",
+                        "charts":[  "elasticsearch",
+                                    "fluentd",
+                                    "kibana",
+                                    "postgresql",
+                                    "calico",
+                                    "flannel",
+                                    "grafana",
+                                    "kube-dns",
+                                    "nfs-provisioner",
+                                    "prometheus-alertmanager",
+                                    "prometheus-kube-state-metrics",
+                                    "prometheus-openstack-exporter",
+                                    "prometheus",
+                                    "redis",
+                                    "registry",
+                                    "tiller"]}
+
+]}'''
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(chartsJson)
+
+for (entry in object.osh) {
+    for (chart in entry.charts) {
+        pipelineJob("charts/${entry.repo}/${chart}") {
+            disabled(SILENT_MODE.toBoolean())
+            parameters {
+                 stringParam('REPO',"${entry.repo}")
+            }
+            triggers {
+            // http://<gerrit>/plugin/job-dsl/api-viewer/index.html#path/javaposse.jobdsl.dsl.helpers.properties.PropertiesContext.pipelineTriggers-triggers-gerrit
+                gerritTrigger {
+                    silentMode(SILENT_MODE.toBoolean())
+                    serverName('OS-CommunityGerrit')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern("${entry.repo}")
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("**")
+                                }
+                            }
+                            filePaths {
+                                filePath {
+                                compareType("ANT")
+                                pattern("$chart/**")
+                                }
+                                filePath {
+                                compareType("ANT")
+                                pattern("helm-toolkit/**")
+                                }
+                            }
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        changeMerged()
+                    }
+                }
+
+                definition {
+                    cps {
+                        script(readFileFromWorkspace('charts/openstack/Jenkinsfile'))
+                        sandbox()
+                    }
+                }
+            }
+        }
+    }
+}
