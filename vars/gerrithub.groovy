f3f51d2987605bd34f3bda9753c5b9e9757f152c
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-02-17 15:55:57 -0600

    Add checkoutAndMerge method
    This ps adds the ability for Jenkins to checkout a patchset to a
    branch and merge it with the current master.
    
    Change-Id: Ie39d99d756add7178f515a8423f45c111d41ef1f

--- vars/gerrithub.groovy
+++ vars/gerrithub.groovy
@@ -12,3 +12,18 @@ def clone(String project, String refspec){
          url: "https://review.gerrithub.io/" + project ]]]
 }
 
+//This method is used so that we can checkout the patchset to a local
+//branch and then merge it with the current master before we build and test
+def checkoutAndMerge(String project, String refspec){
+    checkout poll: false,
+    scm: [$class: 'GitSCM',
+              branches: [[name: refspec]],
+              doGenerateSubmoduleConfigurations: false,
+              extensions: [[$class: 'LocalBranch',
+                            localBranch: 'jenkins']],
+                            submoduleCfg: [],
+                            userRemoteConfigs: [[refspec: 'refs/changes/*:refs/changes/*',
+                                                 url: "https://review.gerrithub.io/" + project]]]
+    sh 'git checkout master'
+    sh 'git merge jenkins'
+}
