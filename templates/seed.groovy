1eb26fc1747f5286fc3225c83072355c9ed783ef
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-02-19 11:07:45 -0600

    Add template for pipelines
    This ps starts adding templates for adding a Jenkins pipeline
    using Jenkinsfile and seeds
    
    Change-Id: Ic5a5329e97c351aa22d48829b4c23efbfd260baa

---
+++ templates/seed.groovy
@@ -0,0 +1,55 @@
+import groovy.json.JsonSlurper
+
+def imagesJson = '''{ "UCP":[{
+                        "repo":"att-comdev",
+                        "images":[
+                                  "my-component1",
+                                  "my-component2",
+                                  "my-component3",
+                                  "my-component4"
+                                  ]
+                        }]}'''
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.UCP) {
+    for (image in entry.images) {
+      pipelineJob("images/${entry.repo}/${image}/${image}") {
+            triggers {
+                gerritTrigger {
+                    serverName('Gerrithub-jenkins')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern("${entry.repo}/${image}")
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("**")
+                                }
+                            }
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        patchsetCreated {
+                           excludeDrafts(false)
+                           excludeTrivialRebase(false)
+                           excludeNoCodeChange(false)
+                        }
+                        changeMerged()
+                    }
+                }
+
+                definition {
+                    cps {
+                      script(readFileFromWorkspace("images/${entry.repo}/${image}/Jenkinsfile"))
+                        sandbox()
+                    }
+                }
+            }
+        }
+    }
+}
+
