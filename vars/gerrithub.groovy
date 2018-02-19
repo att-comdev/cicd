e4ac86b1eb4848d9d9f9ce801d06a0f7e48f550e
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-02-18 22:43:54 -0600

    Removing directories
    This patchset removes the ability to checkout to a directory
    
    Change-Id: I78ed3e037f63e50f60aa7863e171951a79c70919

--- vars/gerrithub.groovy
+++ vars/gerrithub.groovy
@@ -14,15 +14,13 @@ def clone(String project, String refspec){
 
 //This method is used so that we can checkout the patchset to a local
 //branch and then merge it with the current master before we build and test
-def checkoutAndMerge(String project, String refspec, String targetDirectory){
+def checkoutAndMerge(String project, String refspec){
     checkout poll: false,
     scm: [$class: 'GitSCM',
               branches: [[name: refspec]],
               doGenerateSubmoduleConfigurations: false,
               extensions: [[$class: 'LocalBranch',
                             localBranch: 'jenkins']],
-                          [[$class: 'RelativeTargetDirectory',
-                            relativeTargetDir: targetDirectory]],
                             submoduleCfg: [],
                             userRemoteConfigs: [[refspec: 'refs/changes/*:refs/changes/*',
                                                  url: "https://review.gerrithub.io/" + project]]]
