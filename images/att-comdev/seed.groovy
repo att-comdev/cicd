ec9d65d9e1b496a1278a4794a93d9a8e49067b70
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-02-21 22:48:11 -0600

    Build off of github master
    This ps will iterate over drydock, armada, shipyard, deckhand,
    maas to build a pipeline that will build and publish images off
    of github master
    
    Change-Id: Ia3e36702ecda9906aa302ab5d2aedeb23b96ac94

--- images/att-comdev/seed.groovy
+++ images/att-comdev/seed.groovy
@@ -56,3 +56,58 @@ for (entry in object.UCP) {
         }
     }
 }
+imagesJson = '''{ "github":[{
+                               "repo":"att-comdev/shipyard",
+                               "image":"shipyard"
+                            },{
+                               "repo":"att-comdev/shipyard",
+                               "image":"airflow"
+                            },{          
+                               "repo":"att-comdev/drydock",
+                               "image":"drydock"
+                            },{
+                               "repo":"att-comdev/maas",
+                               "image":"sstream-cache"
+                            },{
+                               "repo":"att-comdev/maas",
+                               "image":"maas-region-controller"
+                            },{
+                               "repo":"att-comdev/maas",
+                               "image":"maas-rack-controller"
+                            },{
+                               "repo":"att-comdev/deckhand",
+                               "image":"deckhand"
+                            },{
+                               "repo":"att-comdev/armada",
+                               "image":"armada"
+                            }]}'''
+           
+jsonSlurper = new JsonSlurper()
+object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.github) {
+    pipelineJob("images/${entry.repo}/${entry.image}/${entry.image}-master") {
+        parameters {
+            stringParam {
+                defaultValue("${entry.image}")
+                description('Name of repo in att-comdev to build')
+                name ('PROJECT_NAME')
+            }
+            stringParam {
+                defaultValue("0.1.0")
+                description('Put RC version here')
+                name('VERSION')
+            }
+        }
+        scm {
+            github("${entry.repo}")
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("images/att-comdev/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
