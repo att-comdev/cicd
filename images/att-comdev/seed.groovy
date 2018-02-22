9090588bbca1d123837e3e9df988ecdff86ea90e
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-02-21 22:48:11 -0600

    Build off of github master
    This ps will iterate over drydock, armada, shipyard, deckhand,
    maas to build a pipeline that will build and publish images off
    of github master
    
    Change-Id: Ia3e36702ecda9906aa302ab5d2aedeb23b96ac94

--- images/att-comdev/seed.groovy
+++ images/att-comdev/seed.groovy
@@ -58,3 +58,77 @@ for (entry in object.UCP) {
         }
     }
 }
+
+imagesJson = '''{ "github":[{
+                               "repo":"att-comdev/shipyard",
+                               "directory":"images/att-comdev/shipyard",
+                               "image":"shipyard"
+                            },{
+                               "repo":"att-comdev/shipyard",
+                                "directory":"images/att-comdev/shipyard/airflow",
+                                "image":"airflow"
+                            },{          
+                               "repo":"att-comdev/drydock",
+                                "directory":"images/att-comdev/drydock",
+                                "image":"drydock"
+                            },{
+                                "repo":"att-comdev/maas",
+                                "directory":"images/att-comdev/maas/sstream-cache",
+                                "image":"sstream-cache"
+                            },{
+                                "repo":"att-comdev/maas",
+                                "directory":"images/att-comdev/maas/maas-region-controller",
+                                "image":"maas-region-controller"
+                            },{
+                                "repo":"att-comdev/maas",
+                                "directory":"images/att-comdev/maas/maas-rack-controller",
+                                "image":"maas-rack-controller"
+                            },{
+                                "repo":"att-comdev/deckhand",
+                                "directory":"images/att-comdev/deckhand",
+                                "image":"deckhand"
+                            },{
+                                "repo":"att-comdev/armada",
+                                "directory":"images/att-comdev/armada",
+                                "image":"armada"
+                            },{
+                                "repo":"att-comdev/promenade",
+                                "directory":"images/att-comdev/promenade",
+                                "image":"promenade"
+                            }]}'''
+           
+jsonSlurper = new JsonSlurper()
+object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.github) {
+    pipelineJob("${entry.directory}/${entry.image}-master") {
+        parameters {
+            stringParam {
+                defaultValue("${entry.image}")
+                description('Name of repo in att-comdev to build')
+                name ('PROJECT_NAME')
+            }
+            stringParam {
+                defaultValue("0.1.0")
+                description('Put RC version here')
+                name('VERSION')
+            }
+            stringParam {
+                defaultValue("*/master")
+                description('Put Branch or commit id here')
+                name('COMMIT_ID')
+            }
+        }
+        scm {
+            github("${entry.repo}")
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("images/att-comdev/Jenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+}
+
