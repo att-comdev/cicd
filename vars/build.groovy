8b9109a8e363986ff4eeee6729b92f6a30cc2989
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-03-04 14:01:00 -0600

    Update makeImages
    Update method to makeImages variables
    
    Change-Id: I7338a66be30b530accf5956420efc3e60795b14c

--- vars/build.groovy
+++ vars/build.groovy
@@ -3,12 +3,12 @@ def makeImages(){
    sh '''
       sudo make images \
       IMAGE_PREFIX=${IMAGE_PREFIX} \
-      IMAGE_NAME=\${JOB_BASE_NAME} \
-      DOCKER_REGISTRY=\${ARTF_DOCKER_URL} \
-      LABEL='org.label-schema.vcs-ref=\${IMAGE_TAG} \
-      --label org.label-schema.vcs-url=\${GERRIT_CHANGE_URL} \
-      --label org.label-schema.version=0.1.0-\${BUILD_NUMBER}' \
-      IMAGE_TAG=\${IMAGE_TAG}
+      IMAGE_NAME=${JOB_BASE_NAME} \
+      DOCKER_REGISTRY=${ARTF_DOCKER_URL} \
+      LABEL=org.label-schema.vcs-ref=${IMAGE_TAG} \
+      --label org.label-schema.vcs-url=${GERRIT_CHANGE_URL} \
+      --label org.label-schema.version=0.1.0-${BUILD_NUMBER} \
+      IMAGE_TAG=${IMAGE_TAG}
    '''
 }
 
