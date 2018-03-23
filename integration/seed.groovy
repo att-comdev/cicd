2c6ab759c9193b85f8e64f1073cb94b9872f32a8
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-03-23 12:19:25 -0500

    Integration pipeline update
    Adding most recent Integration pipeline code to gerrit.  This also
    duplicates the pipeline to a new site-update which will be a WIP
    on another patchset.
    
    Change-Id: Ife380161c7cac111f6511d544cb7b31743d3aba1

---
+++ integration/seed.groovy
@@ -0,0 +1,46 @@
+base_path = "integration"
+
+import groovy.json.JsonSlurper
+
+def imagesJson = '''{ "genesis":[
+                          "genesis-full",
+                          "update-site"
+                        ]}'''
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.genesis) {
+    pipelineJob("${base_path}/${entry}") {
+        parameters {
+            stringParam {
+                defaultValue('refs/changes/67/403067/1')
+                description('Pass att-comdev/cicd code refspec to the job')
+                name ('CLCP_INTEGRATION_REFSPEC')
+            }
+            booleanParam {
+                defaultValue(false)
+                description('Enable Shipyard for Drydock and Armada operator')
+                name ('SHIPYARD_ENABLED')
+            }
+            booleanParam {
+                defaultValue(false)
+                description('Enable Sonobuoy conformance tests')
+                name ('SONOBUOY_ENABLED')
+            }
+        }
+    
+        concurrentBuild(false)
+
+        triggers {
+            cron('H */4 * * *')
+
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${base_path}/${entry}/Jenkinsfile"))
+                    sandbox()
+                }
+            }
+        }
+    }
+}
