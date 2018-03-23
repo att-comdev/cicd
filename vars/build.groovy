e108800615c6414847328823cee5dbe37abf49b8
Roman Gorshunov <roman.gorshunov@att.com>
2018-03-23 18:15:30 +0100

    [CCICD-279] Adding artifactory properties
    Code review from Kaspars
    
    Change-Id: I10ba4ce5ebaf0b013d2dd3bc8b25101b9ae8b2ce

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,15 @@
 //Use this method to build images in a repeatable fashion
 def makeImages(){
-   sh "sudo make images IMAGE_PREFIX=${IMAGE_PREFIX} IMAGE_NAME=\${JOB_BASE_NAME} DOCKER_REGISTRY=\${ARTF_DOCKER_URL} LABEL='org.label-schema.vcs-ref=\${IMAGE_TAG} --label org.label-schema.vcs-url=\${GERRIT_CHANGE_URL} --label org.label-schema.version=0.1.0-\${BUILD_NUMBER}' IMAGE_TAG=\${IMAGE_TAG}"
+   sh '''
+      sudo make images \
+      IMAGE_PREFIX=${IMAGE_PREFIX} \
+      IMAGE_NAME=\${JOB_BASE_NAME} \
+      DOCKER_REGISTRY=\${ARTF_DOCKER_URL} \
+      LABEL='org.label-schema.vcs-ref=\${IMAGE_TAG} \
+      --label org.label-schema.vcs-url=\${GERRIT_CHANGE_URL} \
+      --label org.label-schema.version=0.1.0-\${BUILD_NUMBER}' \
+      IMAGE_TAG=\${IMAGE_TAG}
+   '''
 }
 
 /**
@@ -13,6 +22,8 @@ def makeImages(){
 String getImageDigest (String image) {
 // Usage example:
 //    def imageDigest=build.getImageDigest(IMAGE)
-//    publish.setProperty ('jenkins-artifactory', 'https://artifacts-aic.atlantafoundry.com/artifactory/api/storage/clcp-manifests', "${RELEASE_CURRENT_KEY}=${imageDigest}")
+//    publish.setProperty ('jenkins-artifactory', \
+//        'https://artifacts-aic.atlantafoundry.com/artifactory/api/storage/clcp-manifests', \
+//        "${RELEASE_CURRENT_KEY}=${imageDigest}")
   sh "sudo docker inspect --format='{{index .RepoDigests 0}}' ${image}"
 }
