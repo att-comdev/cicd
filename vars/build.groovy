ecf6bf70ce50a0d22d66a72643e276883c692eac
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-03-23 13:57:02 -0500

    Revert change
    Determine if this is why publish is failing
    
    Change-Id: Ie2cb6ecff0fe563c8867a78cc18186c7daff93f1

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,15 +1,6 @@
 //Use this method to build images in a repeatable fashion
 def makeImages(){
-   sh '''
-      sudo make images \
-      IMAGE_PREFIX=${IMAGE_PREFIX} \
-      IMAGE_NAME=\${JOB_BASE_NAME} \
-      DOCKER_REGISTRY=\${ARTF_DOCKER_URL} \
-      LABEL='org.label-schema.vcs-ref=\${IMAGE_TAG} \
-      --label org.label-schema.vcs-url=\${GERRIT_CHANGE_URL} \
-      --label org.label-schema.version=0.1.0-\${BUILD_NUMBER}' \
-      IMAGE_TAG=\${IMAGE_TAG}
-   '''
+   sh "sudo make images IMAGE_PREFIX=${IMAGE_PREFIX} IMAGE_NAME=\${JOB_BASE_NAME} DOCKER_REGISTRY=\${ARTF_DOCKER_URL} LABEL='org.label-schema.vcs-ref=\${IMAGE_TAG} --label org.label-schema.vcs-url=\${GERRIT_CHANGE_URL} --label org.label-schema.version=0.1.0-\${BUILD_NUMBER}' IMAGE_TAG=\${IMAGE_TAG}"
 }
 
 /**
