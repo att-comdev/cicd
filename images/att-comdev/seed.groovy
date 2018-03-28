67b64c95c3cf81f4edbd7403e339669101512b2a
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-03-28 13:46:12 -0500

    Adding pipelines for dockerfiles
    This patchset will create any pipelines for anything needed in
    dockerfiles in att-comdev
    
    Change-Id: I1eb992b554d44f5b9b1e1f19a93b7a8b44841072

--- images/att-comdev/seed.groovy
+++ images/att-comdev/seed.groovy
@@ -57,3 +57,61 @@ for (entry in object.UCP) {
         }
     }
 }
+
+imagesJson = '''{ "attcomdev":[{
+                        "repo":"att-comdev/dockerfiles",
+                        "images":[
+                                  "nagios"
+                                  ]
+                        }]}'''
+jsonSlurper = new JsonSlurper()
+object = jsonSlurper.parseText(imagesJson)
+for (entry in object.UCP) {
+    folder("images/${entry.repo}")
+    for (image in entry.images) {
+        pipelineJob("images/${entry.repo}/${image}") {
+            configure {
+                node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                    durationName 'hour'
+                    count '5'
+                }
+            }
+            triggers {
+                gerritTrigger {
+                    serverName('Gerrithub-jenkins')
+                    gerritProjects {
+                        gerritProject {
+                            compareType('PLAIN')
+                            pattern("${entry.repo}/${image}")
+                            branches {
+                                branch {
+                                compareType("ANT")
+                                pattern("**")
+                                }
+                            }
+                            disableStrictForbiddenFileVerification(false)
+                        }
+                    }
+                    triggerOnEvents {
+                        patchsetCreated {
+                           excludeDrafts(false)
+                           excludeTrivialRebase(false)
+                           excludeNoCodeChange(false)
+                        }
+                        changeMerged()
+                        commentAddedContains {
+                           commentAddedCommentContains('recheck')
+                        }
+                    }
+                }
+
+                definition {
+                    cps {
+                      script(readFileFromWorkspace("images/att-comdev/${image}/Jenkinsfile"))
+                      sandbox()
+                    }
+                }
+            }
+        }
+    }
+}
