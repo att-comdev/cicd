141cdda41daaf0f7ef8d98e926f4d0ef236c76c1
Kaspars Skels <kaspars.skels@gmail.com>
2018-04-17 12:13:00 -0500

    Job for launching dev VMs in CI/CD env
    Change-Id: I14c6090af1664da061c0629450d9eb0a9d6e5d38

---
+++ dev/seed.groovy
@@ -0,0 +1,49 @@
+
+JOB_FOLDER='dev'
+JOB_NAME='vms'
+
+folder(JOB_FOLDER)
+
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+
+    description("This job enables to create a VM on demand, and keep it running")
+
+    logRotator {
+        daysToKeep(30)
+    }
+
+    parameters {
+
+        stringParam {
+            name ('NODE_POSTFIX')
+            defaultValue('user-vm1')
+            description('Unique name to identify heat stack to be created')
+            trim(true)
+        }
+
+        stringParam {
+            name ('SSH_KEY')
+            defaultValue('')
+            description('Public SSH key to access the VM')
+            trim(true)
+        }
+
+        choiceParam('NODE_DATA', ['bootstrap.sh'],
+                    'Select user data based on resources/heat/stack')
+
+        choiceParam('NODE_IMAGE', ['cicd-ubuntu-16.04-server-cloudimg-amd64',
+                                   'cicd-ubuntu-18.04-server-cloudimg-amd64'],
+                    'Select image for the VM')
+
+        choiceParam('NODE_FLAVOR', ['m1.medium', 'm1.large', 'm1.xlarge'],
+                    'Select flavor for the VM')
+    }
+
+    definition {
+        cps {
+            sandbox()
+            script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+        }
+    }
+}
+
