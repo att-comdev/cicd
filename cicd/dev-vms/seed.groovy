ec855a8ff58678e58a1713df97fe0b8904d8dafe
Kaspars Skels <kaspars.skels@gmail.com>
2018-04-17 12:13:00 -0500

    Prepare heat templates and libs for dev VMs
    Change-Id: I14c6090af1664da061c0629450d9eb0a9d6e5d38

---
+++ cicd/dev-vms/seed.groovy
@@ -0,0 +1,40 @@
+
+JOB_FOLDER='cicd/dev-vms'
+JOB_NAME='dev-vms'
+
+folder(JOB_FOLDER)
+
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+
+    logRotator {
+        numToKeep(20)
+        artifactNumToKeep(20)
+    }
+
+    parameters {
+        stringParam {
+            name ('NODE_NAME')
+            defaultValue('user-vm1')
+            description('Unique name to identify heat stack to be created')
+        }
+
+        choiceParam('NODE_TMPL', ['ubuntu.m1.small.yaml',
+                               'ubuntu.m1.medium.yaml',
+                               'ubuntu.m1.large.yaml',
+                               'ubuntu.m1.xlarge.yaml',
+                               'ubuntu.m1.xxlarge.yaml'],
+                    'Select template based on resources/heat')
+
+        choiceParam('NODE_DATA', ['bootstrap.sh'],
+                    'Select user data based on resources/heat')
+ 
+    }
+
+    definition {
+        cps {
+            sandbox()
+            script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+        }
+    }
+}
+
