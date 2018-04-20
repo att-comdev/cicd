c8117609522f9d98135b5e2436eabd968f5cd6e7
Roman Gorshunov <roman.gorshunov@att.com>
2018-04-20 16:41:28 +0200

    [WIP] [406101] Ubuntu cloud images build localization pipeline
    Adding pipeline for cloud images rebuild with our localization
    
    Change-Id: I84655a2c9136f0ceb45a518a028b32335ca12432

---
+++ images/att-comdev/ubuntu/cloud/seed.groovy
@@ -0,0 +1,29 @@
+JOB_FOLDER="images/att-comdev/ubuntu/cloud"
+JOB_NAME="BuildCloudImages"
+
+folder(JOB_FOLDER)
+
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+
+    displayName('Build localized Ubuntu cloud images')
+    description('\nThis job is supposed to build (re-build) custom Ubuntu '+
+                'cloud images and upload them to Artifactory.\nList of '+
+                'cloud images is in Jenkinsfile, submit a change to amend it.')
+
+    logRotator {
+        numToKeep(5)
+        artifactNumToKeep(5)
+    }
+
+    parameters {
+        choiceParam('UPLOAD_IMAGES', ['false', 'true'], 'Upload cloud images to repository')
+    }
+
+    definition {
+        cps {
+            sandbox()
+            script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+        }
+    }
+
+}
