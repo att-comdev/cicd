d772c57dfde332792e2ef452f24050d6decddb47
sb464f <sb464f@att.com>
2018-04-25 16:53:36 +0000

    Add debug-genesis pipeline
    Remove the seed.groovy file at integration level
    
    Change-Id: I0708849f983e56997baa1e07a935198f98d10d4b

---
+++ integration/debug-genesis/seed.groovy
@@ -0,0 +1,24 @@
+JOB_FOLDER='integration'
+JOB_NAME='debug-genesis'
+
+folder(JOB_FOLDER)
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+    concurrentBuild(false)
+
+    blockOn(['genesis-full', 'site-update']) {
+        blockLevel('GLOBAL')
+        scanQueueFor('ALL')
+    }
+    triggers {
+        buildResult('H */4 * * *') {
+            combinedJobs()
+            triggerInfo('genesis-full, site-update', BuildResult.SUCCESS, BuildResult.UNSTABLE, BuildResult.FAILURE)
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace("${JOB_FOLDER}/${JOB_NAME}/Jenkinsfile"))
+                sandbox(false)
+            }
+        }
+    }
+}
