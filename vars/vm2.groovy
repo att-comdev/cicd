714571b9ec665abd5d5483a77624059b6c770296
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-05-01 10:31:50 -0500

    Adding loci bootstrap
    Putting loci bootstrap script into version control.
    
    Change-Id: I26e177de4a0a542204ef69e57a748b82bcf92175

--- vars/vm2.groovy
+++ vars/vm2.groovy
@@ -100,3 +100,95 @@ def call(udata = 'bootstrap.sh',
     }
   return ip
 }
+
+def call(udata = 'bootstrap.sh',
+         image = 'cicd-ubuntu-16.04-server-cloudimg-amd64',
+         flavor = 'm1.medium',
+         postfix = '',
+         buildtype = 'basic',
+         leak = false,
+         Closure body) {
+
+    // resolve args to heat parameters
+    def parameters = " --parameter image=${image}" +
+                     " --parameter flavor=${flavor}"
+
+    // node used for launching VMs
+    def launch_node = 'jenkins-node-launch'
+
+    def name = "${JOB_BASE_NAME}-${BUILD_NUMBER}"
+  
+    def stack_template="heat/stack/ubuntu.${buildtype}.stack.template.yaml"
+
+    // optionally uer may supply additional identified for the VM
+    // this makes it easier to find it in OpenStack
+    if (postfix) {
+      name += "-${postfix}"
+    }
+
+    def ip = ""
+
+    try {
+        stage ('Node Launch') {
+            node(launch_node) {
+              tmpl = libraryResource "${stack_template}"
+                writeFile file: 'template.yaml', text: tmpl
+
+                data = libraryResource "heat/stack/${udata}"
+                writeFile file: udata, text: data
+
+                heat.stack_create(name, "${WORKSPACE}/template.yaml", parameters)
+                ip = heat.stack_output(name, 'floating_ip')
+            }
+
+            node('master') {
+                jenkins.node_create (name, ip)
+            }
+
+            node(launch_node) {
+                 timeout (14) {
+                    node(name) {
+                        sh 'hostname'
+                    }
+                }
+            }
+        }
+
+        // body executed under specified vm node
+        node (name) {
+            body()
+        }
+
+    } catch (error) {
+        // notify.msg("Pipeline failed: ${error}")
+        error(error)
+
+    } finally {
+        try {
+            // node(name){
+            //     stage("Publish Jenkins Logs"){
+            //         try{
+            //             publish.putArtifacts(logs.getJenkinsConsoleOutput(), "logs/${JOB_NAME}/")
+            //         } catch (error){
+            //             notify.msg("Logs published failed: ${error}")
+            //         }
+            //     }
+            // }
+        } finally {
+            stage ('Node Destroy') {
+                node(launch_node) {
+                  if (!leak) {
+                     node('master') {
+                       jenkins.node_delete(name)
+                     }
+                    node(launch_node) {
+                      heat.stack_delete(name)
+                    }
+                  }
+                }
+            }
+        }
+    }
+  return ip
+}
+
