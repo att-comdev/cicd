20eb012f0b4b525a17743cc7dd22c0d4c43a9c22
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-05-01 10:31:50 -0500

    Adding loci bootstrap
    Putting loci bootstrap script into version control.
    
    Change-Id: I26e177de4a0a542204ef69e57a748b82bcf92175

---
+++ resources/heat/stack/loci-bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+echo "127.0.1.1   $(hostname)" >> /etc/hosts
+
+# required for docker within docker
+mkdir -p /etc/docker
+cat << EOF > /etc/docker/daemon.json
+{
+  "storage-driver": "overlay2"
+}
+EOF
+
+# git-ssh wrapper
+cat << EOF > /usr/bin/git-ssh-wrapper
+#!/bin/bash
+ssh -i "\$SSH_KEY" "\$@"
+EOF
+
+chmod a+x /usr/bin/git-ssh-wrapper
+
+
+apt-get update
+apt-get install -y docker.io
