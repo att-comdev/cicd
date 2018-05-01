8c190670168defc59510a9a96ddd74c3a67b09ef
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-05-01 10:31:50 -0500

    Adding loci bootstrap
    Putting loci bootstrap script into version control.
    
    Change-Id: I26e177de4a0a542204ef69e57a748b82bcf92175

---
+++ resources/heat/stack/loci-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo "127.0.1.1   $(hostname)" >> /etc/hosts
+
+# git-ssh wrapper
+cat << EOF > /usr/bin/git-ssh-wrapper
+#!/bin/bash
+ssh -i "\$SSH_KEY" "\$@"
+EOF
+
+chmod a+x /usr/bin/git-ssh-wrapper
