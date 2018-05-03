c12a3383ad0d18c35a4f62047473882b6f4c9687
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-05-03 13:15:12 -0500

    Code review job pipeline
    This patchset is putting the code review job into version control.
    
    This patchset also updates it to call the same entrypoint: make tests
    
    Change-Id: I750766656473e76925cce5ce545e94491d0e4002

---
+++ code-review/seed.groovy
@@ -0,0 +1,37 @@
+pipelineJob("code-review") {
+    triggers {
+        gerritTrigger {
+            serverName('Gerrithub-jenkins')
+            gerritProjects {
+                gerritProject {
+                    compareType('REG_EXP')
+                    pattern("^att-comdev/(?!(cicd|docker)).*")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(false)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(false)
+                }
+                changeMerged()
+                commentAddedContains {
+                    commentAddedCommentContains('recheck')
+                }
+             }
+         }
+         definition {
+             cps {
+                 script(readFileFromWorkspace("code-review/Jenkinsfile"))
+                 sandbox()
+             }
+         }
+     }
+}
