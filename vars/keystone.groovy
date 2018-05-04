dd66f7230b7af94cecb35e579ac2759ae6db7bc0
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-05-03 23:03:26 -0400

    [WIP] Add libs to support SY & IAM actions
    This PS adds shared Jenkins libs to support  actions against
    the Shipyard and Keystone APIs. In addition, establishes a global
    Configuration file where static, global variables can be introduced
    to use across pipelines, effectively eliminating the need to create
    global vars within Jenkins itself, as well as allows these global vars
    to be kept in source control.
    
    Change-Id: Ia7b5d845dc2007ec63e42d586a710e5705e61760

---
+++ vars/keystone.groovy
@@ -0,0 +1,66 @@
+def retrieveToken(iamCredId, iamFqdn) {
+    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${iamCredId}",
+            usernameVariable: 'user', passwordVariable: 'pw']]) {
+
+        def conn = new URL("${iamFqdn}/v3/auth/tokens").openConnection()
+        //def conn = url.openConnection()
+
+        conn.setRequestMethod("GET")
+        conn.setRequestProperty("Content-Type", "application/json")
+        conn.doOutput = true
+
+        def requestJson = """ {
+            "auth": {
+                "identity": {
+                    "methods": ["password"],
+                    "password": {
+                        "user": {
+                            "name": "${user}",
+                            "domain": { "id": "default" },
+                            "password": "${pw}"
+                        }
+                    }
+                }
+            }
+        } """
+
+        //def writer = new OutputStreamWriter(conn.outputStream)
+        //writer.write(requestJson)
+        //writer.flush()
+        //writer.close()
+        //conn.connect()
+
+        conn.getOutputStream().write(requestJson.getBytes("UTF-8"))
+
+        def result = conn.getHeaderField("X-Subject-Token")
+        return result
+    }
+}
+
+def retrieve_token(iamCredId, iamFqdn) {
+    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${iamCredId}",
+            usernameVariable: 'user', passwordVariable: 'pw']]) {
+
+        def requestJson = """ {
+            "auth": {
+                "identity": {
+                    "methods": ["password"],
+                    "password": {
+                        "user": {
+                            "name": "$user",
+                            "domain": { "id": "default" },
+                            "password": "$pw"
+                        }
+                    }
+                }
+            }
+        } """
+
+        println('RequestJson: '+requestJson)
+        def response = httpRequest contentType: 'APPLICATION_JSON', httpMode: 'GET', requestBody: JsonOutput.toJson(requestJson), url: "${iamFqdn}/v3/auth/tokens"
+        println('Status: '+response.status)
+        println('Response: '+response.content)
+        return response.status
+
+    }
+}
