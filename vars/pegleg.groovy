a215b1936a2152bbe7c7628cabfbe0644ef84932
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-05-03 23:03:26 -0400

    [WIP] Add libs to support SY & IAM actions
    This PS adds shared Jenkins libs to support  actions against
    the Shipyard and Keystone APIs. In addition, establishes a global
    Configuration file where static, global variables can be introduced
    to use across pipelines, effectively eliminating the need to create
    global vars within Jenkins itself, as well as allows these global vars
    to be kept in source control.
    
    Change-Id: Ia7b5d845dc2007ec63e42d586a710e5705e61760

---
+++ vars/pegleg.groovy
@@ -0,0 +1,14 @@
+def lint(peglegImage, siteRepo, globalRepo, secretsRepo) {
+    sh "sudo docker run --rm -i --net=none --workdir='/workspace' -v \$(pwd):/workspace \
+        ${peglegImage} pegleg lint -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo}"
+}
+
+def render(peglegImage, siteRepo, globalRepo, secretsRepo, siteName) {
+    sh "sudo docker run --rm -i --net=none --workdir='/workspace' -v \$(pwd):/workspace \
+        ${peglegImage} pegleg site -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo} render ${siteName} > ${siteName}.yaml"
+}
+
+def collect(peglegImage, siteRepo, globalRepo, secretsRepo, siteName, outDir) {
+    sh "docker run --rm -i --net=none --workdir='/workspace' -v \$(pwd):/workspace \
+        ${peglegImage} pegleg site -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo} collect ${siteName} -s ${outDir}"
+}
