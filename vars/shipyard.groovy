a463660657b6f21fc2ca10b05b145f09057210bc
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-05-03 23:03:26 -0400

    [WIP] Add libs to support SY & IAM actions
    This PS adds shared Jenkins libs to support  actions against
    the Shipyard and Keystone APIs. In addition, establishes a global
    Configuration file where static, global variables can be introduced
    to use across pipelines, effectively eliminating the need to create
    global vars within Jenkins itself, as well as allows these global vars
    to be kept in source control.
    
    Change-Id: Ia7b5d845dc2007ec63e42d586a710e5705e61760

---
+++ vars/shipyard.groovy
@@ -0,0 +1,44 @@
+def createAction(uuid, authToken, shipyardFqdn, action) {
+
+    def conn = new URL("${shipyardFqdn}/api/v1.0/actions").openConnection()
+    conn.setRequestMethod("POST")
+    conn.setRequestProperty("Content-Type", "application/json")
+    conn.setRequestProperty("X-Context-Marker", "${uuid}")
+    conn.setRequestProperty("X-Auth-Token", "${authToken}")
+    conn.doOutput = true
+
+    def requestJson = """ {
+        "name": "${action}"
+    } """
+
+    conn.getOutputStream().write(requestJson.getBytes("UTF-8"))
+    return conn.responseCode
+}
+
+def create_configdocs(String uuid, String authToken, String filePath, String shipyardFqdn, String bucketName, String bufferMode) {
+    sh "curl -iv -H 'X-Context-Marker: ${uuid}' -H 'X-Auth-Token: ${authToken}' -H 'Content-Type: application/x-yaml' \
+        --data-binary \"@${filePath}\" $shipyardFqdn/api/v1.0/configdocs/${bucketName}?buffermode=${bufferMode} > response"
+    return get_response_code()
+}
+
+def createConfigdocs(String uuid, String authToken, String filePath, String shipyardFqdn, String bucketName, String bufferMode) {
+        def conn = new URL("$shipyardFqdn/api/v1.0/configdocs/${bucketName}?buffermode=${bufferMode}").openConnection()
+        conn.setRequestMethod("GET")
+        conn.setRequestProperty("Content-Type", "application/x-yaml")
+        conn.setRequestProperty("X-Context-Marker", "${uuid}")
+        conn.setRequestProperty("X-Auth-Token", "${authToken}")
+        conn.doOutput = true
+
+        conn.getOutputStream().write(new File("${filePath}").bytes)
+        return conn.responseCode
+}
+
+def commit_configdocs(String uuid, String authToken, String shipyardFqdn) {
+    sh "curl -iv -X POST -H 'X-Context-Marker: ${uuid}' -H 'X-Auth-Token: ${authToken}' -H 'Content-Type: application/x-yaml' \
+        $shipyardFqdn/api/v1.0/commitconfigdocs > response"
+    return get_response_code()
+}
+
+private def get_response_code() {
+    return sh(script: "cat response | grep 'HTTP/1.1' | tail -1 | grep -o '[0-9]\\{3\\}'", returnStdout: true).trim()
+}
