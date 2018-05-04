9614efd66be9d8d8ace3aa8c245bb557155036b3
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-05-03 23:03:26 -0400

    [US413535] Add libs to support SY & IAM actions
    This PS adds shared Jenkins libs to support  actions against
    the Shipyard and Keystone APIs. In addition, establishes a global
    Configuration file where static, global variables can be introduced
    to use across pipelines, effectively eliminating the need to create
    global vars within Jenkins itself, as well as allows these global vars
    to be kept in source control.
    
    Change-Id: Ia7b5d845dc2007ec63e42d586a710e5705e61760

---
+++ vars/pegleg.groovy
@@ -0,0 +1,30 @@
+import static att.comdev.cicd.config.Conf.*
+
+def lint(siteRepo, globalRepo, secretsRepo) {
+    sh "sudo docker run --rm -i --net=none --workdir='/workspace' -v \$(pwd):/workspace \
+        $PEGLEG_IMAGE pegleg -v lint -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo} -x P001 -x P003"
+}
+
+def lintWithinContainer(siteRepo, globalRepo, secretsRepo) {
+    sh "pegleg -v lint -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo} -x P001 -x P003"
+}
+
+def render(siteRepo, globalRepo, secretsRepo, siteName) {
+    sh "sudo docker run --rm -i --net=none --workdir='/workspace' -v \$(pwd):/workspace \
+        $PEGLEG_IMAGE pegleg site -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo} render ${siteName} > ${siteName}.yaml"
+}
+
+def renderWithinContainer(siteRepo, globalRepo, secretsRepo, siteName) {
+    return sh(script: "pegleg site -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo} render ${siteName} > ${siteName}.yaml", returnStatus: true).trim()
+}
+
+// For use when calling Docker to run/send commands to the Pegleg container
+def collect(siteRepo, globalRepo, secretsRepo, siteName, outDir) {
+    sh "docker run --rm -i --net=none --workdir=/workspace -v \$(pwd):/workspace \
+        $PEGLEG_IMAGE pegleg site -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo} collect ${siteName} -s ${outDir}"
+}
+
+// For use when calling Pegleg from directly within the container
+def collectWithinContainer(siteRepo, globalRepo, secretsRepo, siteName, outDir) {
+    sh "pegleg site -p ${siteRepo} -a ${globalRepo} -a ${secretsRepo} collect ${siteName} -s ${outDir}"
+}
