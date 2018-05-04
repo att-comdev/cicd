e731c16de45d938cad611a44bf2c8ec5cf1aee45
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-05-03 23:03:26 -0400

    [WIP] Add libs to support SY & IAM actions
    This PS adds shared Jenkins libs to support  actions against
    the Shipyard and Keystone APIs. In addition, establishes a global
    Configuration file where static, global variables can be introduced
    to use across pipelines, effectively eliminating the need to create
    global vars within Jenkins itself, as well as allows these global vars
    to be kept in source control.
    
    Change-Id: Ia7b5d845dc2007ec63e42d586a710e5705e61760

---
+++ src/att/comdev/cicd/config/Conf.groovy
@@ -0,0 +1,8 @@
+#! /usr/bin/groovy
+package att.comdev.cicd.config
+
+class Configuration {
+
+    public static final String PEGLEG_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/pegleg:cc6476507535b58c57a3aee38a465b34d557f86f"
+
+}
