a59e18b09a69ddc6d675a0342efaf070a77e8f6b
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-05-03 23:03:26 -0400

    [US413535] Add libs to support SY & IAM actions
    This PS adds shared Jenkins libs to support  actions against
    the Shipyard and Keystone APIs. In addition, establishes a global
    Configuration file where static, global variables can be introduced
    to use across pipelines, effectively eliminating the need to create
    global vars within Jenkins itself, as well as allows these global vars
    to be kept in source control.
    
    Change-Id: Ia7b5d845dc2007ec63e42d586a710e5705e61760

---
+++ src/att/comdev/cicd/gerrit/Setup.groovy
@@ -0,0 +1,15 @@
+package att.comdev.cicd.gerrit
+
+class Setup {
+
+    public static String repoUser = ""
+    public static String gerritUrl = ""
+    public static String credId = ""
+
+    public Setup(repoUser, gerritUrl, credId) {
+        this.repoUser = repoUser
+        this.gerritUrl = gerritUtl
+        this.credId = credId
+    }
+
+}
---
+++ vars/gerrit.groovy
@@ -0,0 +1,12 @@
+def checkoutToRelativeDir(Setup setup, repo, targetDir, branch = "master", refspec = "master") {
+    checkout poll: false,
+    scm: [$class: "GitSCM",
+        branches: [[name: "${branch}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [[$class: "RelativeTargetDirectory",
+            relativeTargetDir: "${targetDir}"]],
+        submoduleCfg: [],
+        userRemoteConfigs: [[refspec: "${refspec}",
+                             url: "ssh://" + setup.repoUser + "@" + setup.gerritUrl + "/${repo}",
+                             credentialsId: setup.credId]]]
+}
