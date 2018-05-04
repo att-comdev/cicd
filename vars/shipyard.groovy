32b62763cb32a685a803014856b18c4d3f45c714
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-05-03 23:03:26 -0400

    [WIP] Add libs to support SY & IAM actions
    This PS adds shared Jenkins libs to support  actions against
    the Shipyard and Keystone APIs. In addition, establishes a global
    Configuration file where static, global variables can be introduced
    to use across pipelines, effectively eliminating the need to create
    global vars within Jenkins itself, as well as allows these global vars
    to be kept in source control.
    
    Change-Id: Ia7b5d845dc2007ec63e42d586a710e5705e61760

---
+++ vars/shipyard.groovy
@@ -0,0 +1,87 @@
+def createConfigdocsWithinContainer(uuid, bucketName, outDir) {
+    sh """
+        shipyard --context-marker=${uuid} create configdocs ${bucketName} --replace --directory=${outDir} > response
+        cat response
+    """
+
+    def errors = getShipyardErrorCount() as Integer
+    println "Errors: " + errors
+    if(errors.compareTo(0) == 1) {
+        error("Build failed. Error(s) while processing.")
+    }
+}
+
+def createConfigdocs(uuid, authToken, filePath, shipyardFqdn, bucketName, bufferMode) {
+    sh """
+        curl -iv -H 'X-Context-Marker: ${uuid}' -H 'X-Auth-Token: ${authToken}' -H 'Content-Type: application/x-yaml' \
+            --data-binary \"@${filePath}\" $shipyardFqdn/api/v1.0/configdocs/${bucketName}?buffermode=${bufferMode} > response
+        cat response
+    """
+
+    def code = getResponseCode() as Integer
+    println "Code: " + code
+    if(code.compareTo(201) == 1) {
+        error("Build failed. Bad response code.")
+    }
+}
+
+def commitConfigDocsWithinContainer(uuid) {
+    sh """
+        shipyard --context-marker=${uuid} commit configdocs > response
+        cat response
+    """
+    def errors = getShipyardErrorCount() as Integer
+    println "Errors: " + errors
+    if(errors.compareTo(0) == 1) {
+        error("Build failed. Error(s) while processing.")
+    }
+}
+
+def commitConfigdocs(uuid, authToken, shipyardFqdn) {
+    def conn = new URL("${shipyardFqdn}/api/v1.0/commitconfigdocs").openConnection()
+    conn.setRequestMethod("POST")
+    conn.setRequestProperty("X-Context-Marker", "${uuid}")
+    conn.setRequestProperty("X-Auth-Token", "${authToken}")
+    conn.doOutput = true
+
+    println "Response: " + conn.responseMessage
+    def code = conn.responseCode as Integer
+    println "Code: " + code
+    if(code.compareTo(201) == 1) {
+        error("Build failed. Bad response code.")
+    }
+}
+
+def createActionWithinContainer(uuid, action) {
+    return sh(script: "shipyard --context-marker=${uuid} create action ${action}", returnStdout: true).trim()
+}
+
+def createAction(uuid, authToken, shipyardFqdn, action) {
+    def conn = new URL("${shipyardFqdn}/api/v1.0/actions").openConnection()
+    conn.setRequestMethod("POST")
+    conn.setRequestProperty("Content-Type", "application/json")
+    conn.setRequestProperty("X-Context-Marker", "${uuid}")
+    conn.setRequestProperty("X-Auth-Token", "${authToken}")
+    conn.doOutput = true
+
+    def requestJson = """ {
+        "name": "${action}"
+    } """
+
+    conn.getOutputStream().write(requestJson.getBytes("UTF-8"))
+
+    println "Response: " + conn.responseMessage
+    def code = conn.responseCode as Integer
+    println "Code: " + code
+    if(code.compareTo(201) == 1) {
+        error("Build failed. Bad response code.")
+    }
+}
+
+private def getShipyardErrorCount() {
+    return sh(script: "tail -1 response | cut -d ':' -f2 | xargs | cut -d ',' -f1", returnStdout: true)
+}
+
+private def getResponseCode() {
+    return sh(script: "cat response | grep 'HTTP/1.1' | tail -1 | grep -o '[0-9]\\{3\\}'", returnStdout: true)
+}
