4c63ca2c13067015b498640d138adc318934d317
Roman Gorshunov <roman.gorshunov@att.com>
2018-05-18 20:04:12 +0200

    Adding possibility to build generic packages
    Requires repo with package files, and ./build.sh script.
    
    Change-Id: Id666736bc1c84fc8a39cb2753d2dc57aaac7a01e

---
+++ packages/other/seed.groovy
@@ -0,0 +1,73 @@
+JOB_FOLDER="packages/other"
+JOB_NAME="BuildPackages"
+
+folder(JOB_FOLDER)
+
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+
+    displayName('Build Generic Package')
+    description('\nThis job is supposed to build custom generic package for '+
+                'Ubuntu from Gerrit repo and upload it to Artifactory.\n\n'+
+                'Supports anonymous Gerrit clone/fetch access via HTTPS or '+
+                'SSH access with user name and SSH key stored as Jenkins '+
+                'credential.')
+
+    logRotator {
+        numToKeep(50)
+        artifactNumToKeep(50)
+    }
+
+    parameters {
+        stringParam {
+            name ('GERRIT_HOST')
+            defaultValue('review.gerrithub.io')
+            description('Gerrit server HOSTNAME for anonymous access via '+
+                        'HTTPS, or via SSH. Provide port for SSH access '+
+                        '(most probably :29418).')
+        }
+        stringParam {
+            name ('GERRIT_PROJECT')
+            defaultValue('att-comdev/cicd')
+            description('Gerrit project')
+        }
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('refs/changes/48/412048/1')
+            description('Gerrit refspec (refs/changes/xx/xxxxx/x)')
+        }
+        //stringParam {
+        //    name ('GERRIT_SSH_USER')
+        //    defaultValue('')
+        //    description('Gerrit SSH user; leave empty if you are using '+
+        //                'anonymous HTTPS access')
+        //}
+        //stringParam {
+        //    name ('GERRIT_SSH_CREDS')
+        //    defaultValue('')
+        //    description('Gerrit SSH credentials stored on Jenkins; leave '+
+        //                'empty if you are using anonymous HTTPS access')
+        //}
+        credentialsParam('DEPLOY_KEY') {
+            type('com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey')
+            //required()
+            defaultValue('')
+            description('SSH Key for deploying build artifacts')
+        }
+        choiceParam('UPLOAD_PACKAGES', ['false', 'true'],
+                        'Upload packages to repository')
+        stringParam {
+            name ('ARTF_REPOSITORY_PATH')
+            defaultValue('genesis/')
+            description('Path on artifactory repository where packages '+
+                        'would be uploaded')
+        }
+    }
+
+    definition {
+        cps {
+            sandbox()
+            script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+        }
+    }
+
+}
