ad7823381bc451c5c80f2b9c5560df5209e9d7b2
Roman Gorshunov <roman.gorshunov@att.com>
2018-05-18 20:04:12 +0200

    Adding possibility to build generic packages
    Requires repo with package files, and ./build.sh script.
    
    Change-Id: Id666736bc1c84fc8a39cb2753d2dc57aaac7a01e

---
+++ packages/other/seed.groovy
@@ -0,0 +1,58 @@
+JOB_FOLDER="packages/other"
+JOB_NAME="BuildPackages"
+
+folder(JOB_FOLDER)
+
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+
+    displayName('Build Generic Package')
+    description('\nThis job is supposed to build custom generic '+
+                'package for Ubuntu and upload it to Artifactory.')
+
+    logRotator {
+        numToKeep(50)
+        artifactNumToKeep(50)
+    }
+
+    parameters {
+        stringParam {
+            name ('GERRIT_URL')
+            defaultValue('https://review.gerrithub.io')
+            description('Gerrit URL')
+        }
+        stringParam {
+            name ('GERRIT_PROJECT')
+            defaultValue('att-comdev/cicd')
+            description('Gerrit project')
+        }
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('refs/changes/48/412048/1')
+            description('Gerrit refspec (refs/changes/xx/xxxxx/x)')
+        }
+        stringParam {
+            name ('GERRIT_SSH_USER')
+            defaultValue('')
+            description('Gerrit SSH user; leave empty if you are using anonymous HTTPS access')
+        }
+        stringParam {
+            name ('GERRIT_SSH_CREDS')
+            defaultValue('')
+            description('Gerrit SSH credentials stored on Jenkins; leave empty if you are using anonymous HTTPS access')
+        }
+        choiceParam('UPLOAD_PACKAGES', ['false', 'true'], 'Upload packages to repository')
+        stringParam {
+            name ('ARTF_REPOSITORY_PATH')
+            defaultValue('genesis/')
+            description('Path on artifactory repository where packages would be uploaded')
+        }
+    }
+
+    definition {
+        cps {
+            sandbox()
+            script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+        }
+    }
+
+}
