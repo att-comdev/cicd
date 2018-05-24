14aa79b3fcc81aa993b85b34de3f448207d484bf
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-05-24 12:35:42 -0500

    Generic Gerrit
    This adds a generic gerrit to be used by any Gerrit (hub, openstack)
    
    Change-Id: I0b24ba8f766eedc08291545bf0fadcebadd50783

---
+++ vars/gerrit.groovy
@@ -0,0 +1,64 @@
+def clone(String url, String refspec){
+// Usage example: gerrit.clone("gerrit url", "origin/master")
+// clone refspec: gerrit.clone("gerrit url", "${env.GERRIT_REFSPEC}")
+    checkout poll: false,
+    scm: [$class: 'GitSCM',
+         branches: [[name: refspec]],
+         doGenerateSubmoduleConfigurations: false,
+         extensions: [[$class: 'CleanBeforeCheckout']],
+         submoduleCfg: [],
+         userRemoteConfigs: [[refspec: '${GERRIT_REFSPEC}',
+         url: "url" ]]]
+}
+
+def cloneToBranch(String url, String refspec, String targetDirectory){
+//This method is used so that we can checkout the patchset to a local
+//branch and then rebase it locally with the current master before we build and test
+    checkout poll: false,
+    scm: [$class: 'GitSCM',
+              branches: [[name: refspec]],
+              doGenerateSubmoduleConfigurations: false,
+              extensions: [[$class: 'LocalBranch',
+                            localBranch: 'jenkins'],
+                           [$class: 'RelativeTargetDirectory',
+                            relativeTargetDir: targetDirectory]],
+                            submoduleCfg: [],
+                            userRemoteConfigs: [[refspec: '${GERRIT_REFSPEC}',
+                                                 url: "url"]]]
+}
+
+def rebase(){
+//This method will rebase the local checkout with master and then continue build, tests, etc
+    sh '''git config user.email "airship.jenkins@gmail.com"
+          git config user.name "Jenkins"
+          git pull --rebase origin master'''
+}
+
+//Replace clone and rebase methods
+def checkout(String revision, String branchToClone, String refspec, String targetDirectory){
+   if(revision){
+       IMAGE_TAG=revision
+   }
+   cloneToBranch(branchToClone, refspec, targetDirectory)
+   if(!revision) {
+       dir(env.WORKSPACE+"/"+targetDirectory){
+           rebase()
+       }
+   }
+}
+
+def cloneProject(String url, String branch, String refspec, String targetDirectory){
+//This method is used so that we can checkout different project
+//from any patchset in different pipelines
+    checkout poll: false,
+    scm: [$class: 'GitSCM',
+              branches: [[name: "${branch}"]],
+              doGenerateSubmoduleConfigurations: false,
+              extensions: [[$class: 'LocalBranch',
+                            localBranch: 'jenkins'],
+                           [$class: 'RelativeTargetDirectory',
+                            relativeTargetDir: targetDirectory]],
+                            submoduleCfg: [],
+                            userRemoteConfigs: [[refspec: "${refspec}",
+                                                 url: "url"]]]
+}
