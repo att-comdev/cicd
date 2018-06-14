afeb3a834b466ba446de8090bccfa7f33076f9ae
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2018-06-14 06:38:11 -0400

    [426678] - create NodeCleanup job
    Cleanup a stale or no longer required Jenkins Node
    Delete the node and the associated heat stack
    Allow for list of nodes excluded for deletion
    
    Change-Id: Id3a1c1e18418c0cda811182a1d29dd476f936a87

--- src/att/comdev/cicd/config/conf.groovy
+++ src/att/comdev/cicd/config/conf.groovy
@@ -4,5 +4,6 @@ class Conf {
 
     public static final String PEGLEG_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/pegleg:ef47933903047339bd63fcfa265dfe4296e8a322"
     public static final String PROMENADE_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/promenade@sha256:e8a6d6e671fa330e63a91b978757c3cde241aad81f2166aebc9a0880702c0f7c"
+    public static final String EXCLUDE_NODES = "master jenkins-node-launch clcp-seaworthy-genesis 5ec-seaworthy-genesis"
 
 }
