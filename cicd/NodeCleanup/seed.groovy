4ca0594cc0f6adeb660e37c010f38bc0e1c6a2f6
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2018-06-14 06:38:11 -0400

    [426678] - create NodeCleanup job
    Cleanup a stale or no longer required Jenkins Node
    Delete the node and the associated heat stack
    Allow for list of nodes excluded for deletion
    
    Change-Id: Id3a1c1e18418c0cda811182a1d29dd476f936a87

---
+++ cicd/NodeCleanup/seed.groovy
@@ -0,0 +1,22 @@
+base_path = "cicd"
+job_path = "${base_path}/NodeCleanup"
+folder("${base_path}")
+
+pipelineJob(job_path) {
+    description("This job deletes the jenkins node and its underlying stack")
+    label('master')
+    parameters {
+        stringParam {
+            name ('DELETE_NODENAME')
+            defaultValue('')
+            description('Node to be deleted')
+         //   trim(true)
+        }
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace("${job_path}/Jenkinsfile"))
+            sandbox()
+        }
+    }
+}
