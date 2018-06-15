dae36387c3b9914471fff2f1022885a65588a3c8
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-06-15 09:16:31 -0500

    Add Image Build for Tempest
    Generic Jenkinsfile and seed to build image.
    
    Change-Id: I10829ac169f64215cee5b067632323ccb775d31c

---
+++ images/nc/seed.groovy
@@ -0,0 +1,52 @@
+JOB_FOLDER="images/nc"
+folder(JOB_FOLDER)
+pipelineJob("${JOB_FOLDER}/aqua") {
+    configure {
+                node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                    durationName 'hour'
+                    count '3'
+                }
+    }
+    triggers {
+        gerritTrigger {
+            silentMode(false)
+            serverName('mtn5-gerrit')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("PUT PROJECT HERE")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    filePaths {
+                        filePath {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(true)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(true)
+                }
+                changeMerged()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile-AQUA"))
+            sandbox()
+        }
+    }
+}
