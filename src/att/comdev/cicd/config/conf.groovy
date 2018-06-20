ed48dd3e435e42d86e601186ec2918b28d6964ca
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-06-20 10:13:19 -0400

    Iterate on shared libs, global vars, pipes
    This PS looks to iterate on our current shared libs, global vars,
    as well as pipes.
    
    Change-Id: Ice6b737c9b83234ac31f7f98ef2953ca96f55c5d

--- src/att/comdev/cicd/config/conf.groovy
+++ src/att/comdev/cicd/config/conf.groovy
@@ -2,6 +2,11 @@ package att.comdev.cicd.config
 
 class conf {
 
+    // URLs
+    public static final String ARTF_WEB_URL = "artifacts-aic.atlantafoundry.com/artifactory"
+
+    // Images
+
     public static final String PEGLEG_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/pegleg:ef47933903047339bd63fcfa265dfe4296e8a322"
     public static final String PROMENADE_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/promenade@sha256:e8a6d6e671fa330e63a91b978757c3cde241aad81f2166aebc9a0880702c0f7c"
 
