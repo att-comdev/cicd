fcb7a1c6e34fcce85c0c65934a6bded3e35d7ac9
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-06-24 02:43:00 -0400

    Enhance shared libs for genesis VM deployer
    This PS looks to enhance our shared libs to aid in the creation
    of the Genesis VM deployer.
    
    Change-Id: I930872f9fe1103f51fa7f60de53191db2e70b457

--- src/att/comdev/cicd/config/conf.groovy
+++ src/att/comdev/cicd/config/conf.groovy
@@ -6,4 +6,7 @@ class conf {
     public static final String PROMENADE_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/promenade@sha256:e8a6d6e671fa330e63a91b978757c3cde241aad81f2166aebc9a0880702c0f7c"
     public static final String EXCLUDE_NODES = "master jenkins-node-launch clcp-seaworthy-genesis 5ec-seaworthy-genesis"
 
+    // Artifactory
+    public static final String ARTF_SERVER_ID = "artifactory"
+
 }
---
+++ vars/artifactory.groovy
@@ -0,0 +1,22 @@
+import att.comdev.cicd.config.conf
+
+import groovy.json.JsonOutput
+
+//def artf = Artifactory.server conf.ARTF_SERVER_ID
+def artf = Artifactory.server "artifactory"
+
+def spec(pattern, target) {
+    spec = ["files": [["pattern": pattern,
+                       "target": target,
+                       "flat": true]]]
+    return new JsonOutput().toJson(spec)
+}
+
+def publish(pattern, target) {
+    info = artf.upload(spec(pattern, target))
+    artf.publishBuildInfo(info)
+}
+
+def download(pattern, target) {
+    artf.download(spec(pattern, target))
+}
