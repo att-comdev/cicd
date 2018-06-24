890bdcc5bc58b8d5d0ba4f8c2251e33432223070
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-06-24 02:43:00 -0400

    Enhance shared libs for genesis VM deployer
    This PS looks to enhance our shared libs to aid in the creation
    of the Genesis VM deployer.
    
    Change-Id: I930872f9fe1103f51fa7f60de53191db2e70b457

---
+++ vars/artifactory.groovy
@@ -0,0 +1,22 @@
+import att.comdev.cicd.config.conf
+
+import groovy.json.JsonOutput
+
+//def artf = Artifactory.server conf.ARTF_SERVER_ID
+//def artf = Artifactory.server "artifactory"
+
+def spec(pattern, target) {
+    spec = ["files": [["pattern": pattern,
+                       "target": target,
+                       "flat": true]]]
+    return new JsonOutput().toJson(spec)
+}
+
+def publish(pattern, target) {
+    info = Artifactory.server("artifactory").upload(spec(pattern, target))
+    Artifactory.server("artifactory").publishBuildInfo(info)
+}
+
+def download(pattern, target) {
+    Artifactory.server("artifactory").download(spec(pattern, target))
+}
