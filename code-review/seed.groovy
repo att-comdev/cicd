7946b4ade6e19f335238ec1a94fc515f04caa2d3
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-06-25 23:35:09 -0500

    Code-Review SSH update
    This patchset allows for ssh urls with credentials to trigger
    the code-review job.
    
    Just set the INTERNAL_GERRIT_SSH and INTERNAL_GERRIT_KEY to
    your gerrit and key.
    
    Change-Id: I8453c2ac8e1bde9f52db84d1a9c6505be088ef93

--- code-review/seed.groovy
+++ code-review/seed.groovy
@@ -2,11 +2,21 @@ pipelineJob("code-review") {
     description("This job is for python applications to run pep8, bandit, unit tests, and code coverage")
     triggers {
         gerritTrigger {
-            serverName('Gerrithub-jenkins')
             gerritProjects {
                 gerritProject {
                     compareType('REG_EXP')
                     pattern("^att-comdev/(?!(cicd|docker|maas|treasuremap|ucp-integration)).*")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+                gerritProject {
+                    compareType('REG_EXP')
+                    pattern("^aic-aqa-(?!(resource)).*")
                     branches {
                         branch {
                             compareType("ANT")
