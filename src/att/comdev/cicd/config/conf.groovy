5a6a8db0c366ee2f91797549717b99bff627641c
Roman Gorshunov <roman.gorshunov@att.com>
2018-07-02 14:31:28 +0200

    NodeCleanup job fixes
    Added AF nodes to be excluded from cleanup, added configuration for AF
    Jenkins, split job to two stages for better visibility of failures.
    
    Change-Id: Ia0194e5cd85edfa6cfed211731c88861ad9ec501

--- src/att/comdev/cicd/config/conf.groovy
+++ src/att/comdev/cicd/config/conf.groovy
@@ -4,7 +4,7 @@ class conf {
 
     public static final String PEGLEG_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/pegleg:ef47933903047339bd63fcfa265dfe4296e8a322"
     public static final String PROMENADE_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/promenade@sha256:e8a6d6e671fa330e63a91b978757c3cde241aad81f2166aebc9a0880702c0f7c"
-    public static final String EXCLUDE_NODES = "master jenkins-node-launch clcp-seaworthy-genesis 5ec-seaworthy-genesis"
+    public static final String EXCLUDE_NODES = "master jenkins-node-launch 10.24.20.18-slave 10.24.20.19-slave MyCodeReviewDev att-comdev-charts-dev cab24-r820-18 cab24-r820-19 openstack-helm-charts genesis clcp-seaworthy-genesis 5ec-seaworthy-genesis"
 
     // Artifactory
     public static final String ARTF_SERVER_ID = "artifactory"
