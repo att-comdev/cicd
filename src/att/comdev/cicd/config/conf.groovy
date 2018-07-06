ab421cab5e21d018aee5a13d2cd4bb9ba4741b71
Dejaeger, Darren (dd118r) <dd118r@us.att.com>
2018-07-06 13:50:16 -0400

    Iterate on shared libs, global vars
    This PS aims to iterate on and improve the shared libs, as well
    as add in some global vars.
    
    Change-Id: I7094b163fc240799de1c39caa00773dac0d85d6a

--- src/att/comdev/cicd/config/conf.groovy
+++ src/att/comdev/cicd/config/conf.groovy
@@ -2,11 +2,17 @@ package att.comdev.cicd.config
 
 class conf {
 
+    // URLs
+    public static final String ARTF_WEB_URL = "artifacts-aic.atlantafoundry.com/artifactory"
+
+    // Static Images
     public static final String PEGLEG_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/pegleg:ef47933903047339bd63fcfa265dfe4296e8a322"
     public static final String PROMENADE_IMAGE = "artifacts-aic.atlantafoundry.com/att-comdev/promenade@sha256:e8a6d6e671fa330e63a91b978757c3cde241aad81f2166aebc9a0880702c0f7c"
-    public static final String EXCLUDE_NODES = "master jenkins-node-launch clcp-seaworthy-genesis 5ec-seaworthy-genesis"
 
     // Artifactory
     public static final String ARTF_SERVER_ID = "artifactory"
+
+    // Other
+    public static final String EXCLUDE_NODES = "master jenkins-node-launch clcp-seaworthy-genesis 5ec-seaworthy-genesis"
 
 }
