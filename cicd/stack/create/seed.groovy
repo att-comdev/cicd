2d8959e5da3c103cd1233a92e7f233d32648206e
sb464f <sb464f@att.com>
2018-07-12 17:28:01 +0000

    Jenkins Job to create a stack
    Change-Id: Ie3787c27ac21a809d46b7f58073e4e3dd771dcf3

---
+++ cicd/stack/create/seed.groovy
@@ -0,0 +1,29 @@
+parent_path = "cicd/stack"
+child_path = "cicd/stack/create"
+job_path = "${base_path}/StackCreate"
+folder("${parent_path}")
+folder("${child_path}")
+
+pipelineJob(job_path) {
+    description("This job executes heta stack create for Image stack")
+    label('master')
+    parameters {
+        stringParam {
+            name ('CLOUD_IMAGE')
+            defaultValue('https://artifacts-aic.atlantafoundry.com/artifactory/ubuntu-images/bionic/current/bionic-server-cloudimg-amd64.img')
+            description('Image reference to be used to cretae image stack')
+        }
+
+        stringParam {
+            name ('STACK_NAME')
+            defaultValue('ubuntu-genesis-1604')
+            description('Stack name to be used to cretae image stack')
+        }
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace("${child_path}/Jenkinsfile"))
+            sandbox()
+        }
+    }
+}
