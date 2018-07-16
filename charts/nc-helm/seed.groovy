796263f3cc6784a724d40058e035a942382d7ef5
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-07-16 17:09:28 -0500

    Add chart pipeline
    This patchset install OSH-infra with Kubeadm for basic helm chart
    testing
    
    Change-Id: Ie5b1f8284ae60c5f8ccbd755efb6ada85531fa22

---
+++ charts/nc-helm/seed.groovy
@@ -0,0 +1,88 @@
+import groovy.json.JsonSlurper
+
+folder("charts/nc-helm")
+folder("charts/openstack")
+folder("charts/airship")
+
+def imagesJson = '''{ "github":[{
+                               "directory": "charts/nc-helm",
+                               "gerrit_project": "nc-helm",
+                               "trigger": "mtn5-gerrit",
+                               "project_name":"nc_helm"
+                            },{
+                               "directory": "charts/openstack",
+                               "gerrit_project": "openstack/openstack-helm",
+                               "trigger": "OS-CommunityGerrit",
+                               "project_name":"openstack-helm"
+                            },
+                            {
+                               "directory": "charts/airship",
+                               "gerrit_project": "openstack/airship-deckhand",
+                               "trigger": "ATT-airship-CI",
+                               "project_name":"airship-deckhand"
+                            }
+                            ]}'''
+
+jsonSlurper = new JsonSlurper()
+object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.github) {
+  folder("${entry.directory}")
+  pipelineJob("${entry.directory}/${entry.project_name}") {
+        parameters {
+            stringParam {
+                defaultValue("${entry.gerrit_project}")
+                description('Name of repo in openstack to build')
+                name ('GIT_REPO')
+            }
+            stringParam {
+                defaultValue("1.0.0")
+                description('Put RC version here')
+                name('VERSION')
+            }
+        }
+        configure {
+            node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                durationName 'hour'
+                count '10'
+            }
+        }
+        triggers {
+            gerritTrigger {
+                silentMode(true)
+                serverName("${entry.trigger}")
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("${entry.gerrit_project}")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+                triggerOnEvents {
+                    patchsetCreated {
+                       excludeDrafts(false)
+                       excludeTrivialRebase(false)
+                       excludeNoCodeChange(false)
+                    }
+                    changeMerged()
+                    commentAddedContains {
+                        commentAddedCommentContains('recheck')
+                    }
+                }
+           }
+
+           definition {
+               cps {
+                   script(readFileFromWorkspace("charts/nc-helm/Jenkinsfile"))
+                   sandbox()
+               }
+           }
+        }
+    }
+}
