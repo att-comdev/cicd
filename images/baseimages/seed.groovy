0461632916d0bfb039a5f3501fed913174ebc231
Jerome Brette <jbrette@gmail.com>
2018-07-17 21:46:24 -0500

    Add Dockerfile for base images
    The Dockerfile allow to control exectly what software
    is added to the images of the containers.
    
    The following are rebuild from scratch in the order:
    - docker-brew-ubuntu-core/xenial
    - buildpack-deps/xenial/curl
    - buildpack-deps/xenial/scm
    - buildpack-deps/xenial
    - python/3.5/xenial
    - python/3.6/xenial
    
    Change-Id: Ifbf9281c7380618e24f255dd3149ea73f11b3f0d

---
+++ images/baseimages/seed.groovy
@@ -0,0 +1,214 @@
+folder("images/baseimages")
+
+pipelineJob("images/baseimages/buildall") {
+    parameters {
+        stringParam {
+            defaultValue("0.9.0")
+            description('Put RC version here')
+            name('VERSION')
+        }
+    }
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '10'
+        }
+    }
+    triggers {
+
+       definition {
+           cps {
+               // TBD.
+               // Need to invoke the other job in a specific order
+               sandbox()
+           }
+       }
+    }
+}
+
+pipelineJob("images/baseimages/master/docker-brew-ubuntu-core-xenial") {
+    parameters {
+        stringParam {
+            defaultValue("ncbaseimages/ubuntu:xenial")
+            description('Name of the Image to publish')
+            name('TARGET_IMAGE')
+        }
+        stringParam {
+            defaultValue("scratch")
+            description('Name of the Image to publish')
+            name('BASE_IMAGE')
+        }
+    }
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '10'
+        }
+    }
+    triggers {
+
+       definition {
+           cps {
+               script(readFileFromWorkspace("images/baseimages/JenkinsfileMaster"))
+               sandbox()
+           }
+       }
+    }
+}
+
+pipelineJob("images/baseimages/master/buildpack-deps-xenial-curl") {
+    parameters {
+        stringParam {
+            defaultValue("ncbaseimages/buildpack-deps:xenial-curl")
+            description('Name of the Image to publish')
+            name('TARGET_IMAGE')
+        }
+        stringParam {
+            defaultValue("ncbaseimages/ubuntu:xenial")
+            description('Name of the Image to publish')
+            name('BASE_IMAGE')
+        }
+    }
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '10'
+        }
+    }
+    triggers {
+
+       definition {
+           cps {
+               script(readFileFromWorkspace("images/baseimages/JenkinsfileMaster"))
+               sandbox()
+           }
+       }
+    }
+}
+
+pipelineJob("images/baseimages/master/buildpack-deps-xenial-scm") {
+    parameters {
+        stringParam {
+            defaultValue("ncbaseimages/buildpack-deps:xenial-scm")
+            description('Name of the Image to publish')
+            name('TARGET_IMAGE')
+        }
+        stringParam {
+            defaultValue("ncbaseimages/buildpack-deps:xenial-curl")
+            description('Name of the Image to publish')
+            name('BASE_IMAGE')
+        }
+    }
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '10'
+        }
+    }
+    triggers {
+
+       definition {
+           cps {
+               script(readFileFromWorkspace("images/baseimages/JenkinsfileMaster"))
+               sandbox()
+           }
+       }
+    }
+}
+
+pipelineJob("images/baseimages/master/buildpack-deps-xenial") {
+    parameters {
+        stringParam {
+            defaultValue("ncbaseimages/buildpack-deps:xenial")
+            description('Name of the Image to publish')
+            name('TARGET_IMAGE')
+        }
+        stringParam {
+            defaultValue("ncbaseimages/buildpack-deps:xenial-scm")
+            description('Name of the Image to publish')
+            name('BASE_IMAGE')
+        }
+    }
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '10'
+        }
+    }
+    triggers {
+
+       definition {
+           cps {
+               script(readFileFromWorkspace("images/baseimages/JenkinsfileMaster"))
+               sandbox()
+           }
+       }
+    }
+}
+
+pipelineJob("images/baseimages/master/python-3.5-xenial") {
+    parameters {
+        stringParam {
+            defaultValue("ncbaseimages/ubuntu:xenial")
+            description('Name of the Image to publish')
+            name('TARGET_IMAGE')
+        }
+        stringParam {
+            defaultValue("ncbaseimages/buildpack-deps:xenial")
+            description('Name of the Image to publish')
+            name('BASE_IMAGE')
+        }
+    }
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '10'
+        }
+    }
+    triggers {
+
+       definition {
+           cps {
+               script(readFileFromWorkspace("images/baseimages/JenkinsfileMaster"))
+               sandbox()
+           }
+       }
+    }
+}
+
+pipelineJob("images/baseimages/master/python-3.6-xenial") {
+    parameters {
+        stringParam {
+            defaultValue("ncbaseimages/python:3.6")
+            description('Name of the Image to publish')
+            name('TARGET_IMAGE')
+        }
+        stringParam {
+            defaultValue("ncbaseimages/buildpack-deps:xenial")
+            description('Name of the Image to publish')
+            name('BASE_IMAGE')
+        }
+    }
+    configure {
+        node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+            durationName 'hour'
+            count '10'
+        }
+    }
+    triggers {
+
+       definition {
+           cps {
+               script(readFileFromWorkspace("images/baseimages/JenkinsfileMaster"))
+               sandbox()
+           }
+       }
+    }
+}
+
+
+
+
+
+
+
