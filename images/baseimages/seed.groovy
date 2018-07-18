464cae8144c322190ba1f53c0e105f8952fec317
Jerome Brette <jbrette@gmail.com>
2018-07-17 21:46:24 -0500

    Add Dockerfile for base images
    The Dockerfile allow to control exectly what software
    is added to the images of the containers.
    
    The following are rebuild from scratch in the order:
    - docker-brew-ubuntu-core/xenial
    - buildpack-deps/xenial/curl
    - buildpack-deps/xenial/scm
    - buildpack-deps/xenial
    - python/3.5/xenial
    - python/3.6/xenial
    
    Change-Id: Ifbf9281c7380618e24f255dd3149ea73f11b3f0d

---
+++ images/baseimages/seed.groovy
@@ -0,0 +1,52 @@
+import groovy.json.JsonSlurper
+
+folder("images/baseimages/master")
+folder("images/baseimages/patchset")
+
+pipelineJob("baseimages") {
+        parameters {
+            stringParam {
+                defaultValue("0.9.0")
+                description('Put RC version here')
+                name('VERSION')
+            }
+        }
+        configure {
+            node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                durationName 'hour'
+                count '10'
+            }
+        }
+        triggers {
+            // gerritTrigger {
+            //     silentMode(true)
+            //     serverName('ATT-airship-CI')
+            //     gerritProjects {
+            //         gerritProject {
+            //             compareType('PLAIN')
+            //             pattern("att-comdev/baseimage")
+            //             branches {
+            //                 branch {
+            //                     compareType("ANT")
+            //                     pattern("**")
+            //                 }
+            //             }
+            //             disableStrictForbiddenFileVerification(false)
+            //         }
+            //     }
+            //     triggerOnEvents {
+            //         changeMerged()
+            //         commentAddedContains {
+            //             commentAddedCommentContains('recheck')
+            //         }
+            //     }
+            // }
+
+           definition {
+               cps {
+                   script(readFileFromWorkspace("images/baseimages/JenkinsfileMaster"))
+                   sandbox()
+               }
+           }
+        }
+    }
