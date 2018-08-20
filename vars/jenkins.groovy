38238084bf4e10b0f59a7b2ff770454dd46e9fed
SKELS, KASPARS (ks3019@att.com) <ks3019@att.com>
2018-08-20 16:24:41 -0500

    Add jenkins lib to support custom ssh key
    Change-Id: I4fb8ccf67f92021e16cc2e1a89c8f609df2d8b4c

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,7 +1,7 @@
 //This groovy file is used for Jenkins node methods.
 
-def node_create(String name, String host) {
-    config = node_config(name, host)
+def node_create(String name, String host, String key = 'jenkins-slave-ssh') {
+    config = node_config(name, host, key)
     withCredentials([usernamePassword(credentialsId: 'jenkins-token',
                                       usernameVariable: 'JENKINS_USER',
                                       passwordVariable: 'JENKINS_TOKEN')]) {
@@ -26,7 +26,7 @@ def node_delete(String name) {
 }
 
 //jenkins-slave-ssh is already in use for the foundry.  We need to standardize to something not in use.
-def node_config(String name, String host) {
+def node_config(String name, String host, String key) {
     config = """<slave>
         <name>${name}</name>
         <description></description>
@@ -37,7 +37,7 @@ def node_config(String name, String host) {
         <launcher class=\"hudson.plugins.sshslaves.SSHLauncher\" plugin=\"ssh-slaves@1.5\">
         <host>${host}</host>
         <port>22</port>
-        <credentialsId>jenkins-slave-ssh</credentialsId>
+        <credentialsId>${key}</credentialsId>
         <maxNumRetries>0</maxNumRetries>
         <retryWaitTime>0</retryWaitTime>
         </launcher>
