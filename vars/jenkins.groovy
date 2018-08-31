7de8a9a01847c394e11a80a1d52d0ef92cd8782e
Stacey Fletcher <staceylynnfletcher@gmail.com>
2018-08-31 12:16:09 -0500

    Parameterize executors
    This patchset allows the number of executors on a Jenkins slave
    to be parameterized.
    
    Change-Id: Ia1e6e5b8d5c0c3db4278c8bde18921fb65a9ce49

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,7 +1,7 @@
 //This groovy file is used for Jenkins node methods.
 
-def node_create(String name, String host, String key = 'jenkins-slave-ssh') {
-    config = node_config(name, host, key)
+def node_create(String name, String host, String key = 'jenkins-slave-ssh', String numOfExecutors) {
+    config = node_config(name, host, key, numOfExecutors)
     withCredentials([usernamePassword(credentialsId: 'jenkins-token',
                                       usernameVariable: 'JENKINS_USER',
                                       passwordVariable: 'JENKINS_TOKEN')]) {
@@ -26,12 +26,12 @@ def node_delete(String name) {
 }
 
 //jenkins-slave-ssh is already in use for the foundry.  We need to standardize to something not in use.
-def node_config(String name, String host, String key) {
+def node_config(String name, String host, String key, String numOfExecutors) {
     config = """<slave>
         <name>${name}</name>
         <description></description>
         <remoteFS>/home/ubuntu/jenkins</remoteFS>
-        <numExecutors>2</numExecutors>
+        <numExecutors>${numOfExecutors}</numExecutors>
         <mode>EXCLUSIVE</mode>
         <retentionStrategy class=\"hudson.slaves.RetentionStrategy\$Always\"/>
         <launcher class=\"hudson.plugins.sshslaves.SSHLauncher\" plugin=\"ssh-slaves@1.5\">
