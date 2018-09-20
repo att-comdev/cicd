8e8cd36851664da3a14993fa5372880d4f9ff613
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2018-09-20 01:44:46 -0400

    Support multi pipeline seed updates
    Enhance superseed to update multiple seed.groovy
    Add logic for lookup of seed.groovy in higher level dirs
    support triggers for nc-cicd
    remove trim(true) as its not needed
    add missed proxy
    
    Change-Id: I2e5fb48f1553ec39c532af7f9fbbaaf395b1f449

--- cicd/SuperSeed/seed.groovy
+++ cicd/SuperSeed/seed.groovy
@@ -9,29 +9,43 @@ freeStyleJob("${job_path}") {
             name ('SEED_PATH')
             defaultValue('')
             description('Seed path. Example: cicd/SuperSeed/seed.groovy')
-            trim(true)
         }
         stringParam {
             name ('GERRIT_REFSPEC')
             defaultValue('origin/master')
             description('Gerrit refspec')
-            trim(true)
+        }
+        stringParam {
+            name ('GERRIT_HOST')
+            defaultValue('review.gerrithub.io')
+            description('Gerrit host')
         }
         stringParam {
             name ('GERRIT_PROJECT')
             defaultValue('att-comdev/cicd')
-            description('Project on Gerrithub')
-            trim(true)
+            description('Project on Gerrit')
         }
     }
 
     triggers {
         gerritTrigger {
-            serverName('Gerrithub-jenkins')
             gerritProjects {
                 gerritProject {
                     compareType('PLAIN')
                     pattern("att-comdev/cicd")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            gerritProjects {
+                gerritProject {
+                    compareType('REG_EXP')
+                    pattern("^nc-cicd$")
                     branches {
                         branch {
                             compareType('ANT')
@@ -61,7 +75,7 @@ freeStyleJob("${job_path}") {
         }
         shell(readFileFromWorkspace("${job_path}/superseed.sh"))
         dsl {
-            external('${BUILD_NUMBER}/seed.groovy')
+            external('${BUILD_NUMBER}/**/*seed.groovy')
             //ignoreExisting(true)
             //removeAction('DISABLE')
         }
