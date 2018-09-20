261870b509ec50b1524f7b508f68a9cb239164a3
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2018-09-20 01:44:46 -0400

    Support multi pipeline seed updates
    Enhance superseed to update multiple seed.groovy
    Add logic for lookup of seed.groovy in higher level dirs
    remove trim(true) as its not needed
    add missed proxy
    
    Change-Id: I2e5fb48f1553ec39c532af7f9fbbaaf395b1f449

--- cicd/SuperSeed/seed.groovy
+++ cicd/SuperSeed/seed.groovy
@@ -9,19 +9,16 @@ freeStyleJob("${job_path}") {
             name ('SEED_PATH')
             defaultValue('')
             description('Seed path. Example: cicd/SuperSeed/seed.groovy')
-            trim(true)
         }
         stringParam {
             name ('GERRIT_REFSPEC')
             defaultValue('origin/master')
             description('Gerrit refspec')
-            trim(true)
         }
         stringParam {
             name ('GERRIT_PROJECT')
             defaultValue('att-comdev/cicd')
             description('Project on Gerrithub')
-            trim(true)
         }
     }
 
