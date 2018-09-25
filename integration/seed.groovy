58be56c1917f1eeacbfe50b389e98e92a34d314e
KAVVA <jk330k@att.com>
2018-09-25 16:43:36 -0500

    For all the Ci/CD operations the Jenkins job logs will be cleanedup in 180 days.
    Change-Id: I1172bb019fa1268f8e12bc32682e45031fa690fc

--- integration/seed.groovy
+++ integration/seed.groovy
@@ -12,6 +12,9 @@ def object = jsonSlurper.parseText(imagesJson)
 
 for (entry in object.genesis) {
     pipelineJob("${base_path}/${entry}") {
+        options {
+            buildDiscarder(logRotator(daysToKeepStr: '180'))
+        }
         parameters {
             booleanParam {
                 defaultValue(false)
