bb39461a7ec84180314ea7ea6446451cc3d91ab9
KAVVA <jk330k@att.com>
2018-09-25 16:43:36 -0500

    Jenkins job logs cleanup
    For all the Ci/CD operations the Jenkins building and deployment
    job logs will be cleaned up in 180 days.
    fix superseed error for NodeCleanup job
    get EXCLUDE_NODE_LIST from conf.EXCLUDE_NODES
    remove trim(true)
    
    Change-Id: I1172bb019fa1268f8e12bc32682e45031fa690fc

--- cicd/NodeCleanup/seed.groovy
+++ cicd/NodeCleanup/seed.groovy
@@ -3,6 +3,9 @@ job_path = "${base_path}/NodeCleanup"
 folder("${base_path}")
 
 pipelineJob(job_path) {
+    logRotator{
+        daysToKeep(180)
+    }
     description("This job deletes the jenkins node and its underlying stack")
     logRotator {
         daysToKeep(180)
--- images/airship/shipyard/airflow/seed.groovy
+++ images/airship/shipyard/airflow/seed.groovy
@@ -1,6 +1,9 @@
 JOB_FOLDER="images/airship/shipyard/airflow"
 folder(JOB_FOLDER)
 pipelineJob("${JOB_FOLDER}/airflow") {
+    logRotator{
+         daysToKeep(180)
+    }
     configure {
                 node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
                     durationName 'hour'
