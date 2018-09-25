81683eefb6725312600dcaf17d5b8bf94e5fe5cf
KAVVA <jk330k@att.com>
2018-09-25 16:43:36 -0500

    For all the Ci/CD operations the Jenkins job logs will be cleanedup in 180 days.
    Change-Id: I1172bb019fa1268f8e12bc32682e45031fa690fc

--- cicd/NodeCleanup/seed.groovy
+++ cicd/NodeCleanup/seed.groovy
@@ -3,6 +3,10 @@ job_path = "${base_path}/NodeCleanup"
 folder("${base_path}")
 
 pipelineJob(job_path) {
+    options {
+        buildDiscarder(logRotator(daysToKeepStr: '180'))
+    }
+
     description("This job deletes the jenkins node and its underlying stack")
     parameters {
         stringParam {
