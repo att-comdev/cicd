4b315e2d2ecc63eab47b1ab5e68eb6c950922a2b
KAVVA <jk330k@att.com>
2018-09-26 09:48:33 -0500

    [US406651] Jenkins job log rotation
    Change-Id: Idfe0e9ed2c6ca8862718902e78da80d3b469e2ce

--- packages/ubuntu/seed.groovy
+++ packages/ubuntu/seed.groovy
@@ -3,8 +3,7 @@ JOB_NAME="BuildPackages"
 
 folder(JOB_FOLDER)
 
-pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {   
-
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
     displayName('Build Packages')
     description('\nThis job is supposed to build (backport/re-build) custom '+
                 'Ubuntu packages and upload them to Artifactory.\nList of '+
