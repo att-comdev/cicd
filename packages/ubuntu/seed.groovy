7f4c74fdb58267c5a7d9b2a87aaffa29cf847adb
KAVVA <jk330k@att.com>
2018-09-27 06:51:03 -0500

    Jenkins job log rotatation
    Change-Id: If9102986bb332b103529e4ae7fbc8daeabac9c09

--- packages/ubuntu/seed.groovy
+++ packages/ubuntu/seed.groovy
@@ -4,14 +4,12 @@ JOB_NAME="BuildPackages"
 folder(JOB_FOLDER)
 
 pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+    logRotator(daysToKeep(180)
+    }
     displayName('Build Packages')
     description('\nThis job is supposed to build (backport/re-build) custom '+
                 'Ubuntu packages and upload them to Artifactory.\nList of '+
-                'packages is in Jenkinsfile, submit a change to amend it.')
-
-    options {
-        buildDiscarder(logRotator(daysToKeepStr: '180'))
-    }
+                'packages is in Jenkinsfile, submit a change to amend it.')    
 
     parameters {
         choiceParam('UPLOAD_PACKAGES', ['false', 'true'], 'Upload packages to repository')
