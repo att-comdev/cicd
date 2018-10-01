f0e42c548a99397d6cfb325f18a8b160fa38456f
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2018-10-01 10:38:59 -0400

    fix superseed error and add log rotator for NodeCleanup job
    Add log rotator to delete records > 180 days
    get EXCLUDE_NODE_LIST from conf.EXCLUDE_NODES
    remove trim(true)
    
    Change-Id: I2067cf064611d3ac69648dbe9c2351ab045166c1

--- cicd/NodeCleanup/seed.groovy
+++ cicd/NodeCleanup/seed.groovy
@@ -3,13 +3,18 @@ job_path = "${base_path}/NodeCleanup"
 folder("${base_path}")
 
 pipelineJob(job_path) {
+    options {
+        buildDiscarder logRotator(artifactDaysToKeepStr: '',
+                                  artifactNumToKeepStr: '',
+                                  daysToKeepStr: '180',
+                                  numToKeepStr: '')
+    }
     description("This job deletes the jenkins node and its underlying stack")
     parameters {
         stringParam {
             name ('DELETE_NODENAME')
             defaultValue('')
             description('Node to be deleted')
-            trim(true)
         }
     }
     definition {
