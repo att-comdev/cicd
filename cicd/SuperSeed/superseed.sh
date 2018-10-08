18c15196b85b6100bdb6ad0176d0ca17ce891beb
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2018-10-08 09:50:20 -0400

    Skip linter check on files with shared lib imports
    declarative-linter don't work with shared library(JENKINS-42730)
    This ps avoids failing superseed job by skipping linter check
    on Jenkinsfile/seed.groovy having global shared library class imports
    
    Change-Id: Id2b3d871695d6540fdfa6b12ed5f1cf85ab95949

--- cicd/SuperSeed/superseed.sh
+++ cicd/SuperSeed/superseed.sh
@@ -138,7 +138,13 @@ lint_jenkins_files(){
 
     echo "NOTICE: Jenkins linter does not check for all errors and can't be 100% trusted"
     for file in ${MODIFIED_FILES}; do
-        echo "INFO: linting jenkins file ""${file}""..."
+        # JENKINS-42730: declarative-linter don't work with shared library
+        # Iterate all files and skip linter checks if file contains
+        # class imports from global share libraries
+        egrep "import.*att.*" ${file} && \
+            echo "INFO:[JENKINS-42730] Skipping linter for file ""${file}""..." && \
+            continue
+        echo "INFO: linting file ""${file}""..."
         opts="-s ${JENKINS_CLI_URL} -auth ${JENKINS_USER}:${JENKINS_TOKEN}"
         cat "${file}" | java -jar ${JENKINS_CLI} ${opts} declarative-linter
     done
