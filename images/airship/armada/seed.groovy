d60f9258422945709dfdac1a507c2942d29febf2
narendra.choudhary <narendra.choudhary@ericsson.com>
2018-10-08 20:57:09 +0000

    Revert "Jenkins job logs cleanup"
    This reverts commit 9a466b188722a1e90ed51c777532bec4f4c3125e.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Ifa982f58c61e84b8e2e5041a76bd8763a13059ed

--- images/airship/armada/seed.groovy
+++ images/airship/armada/seed.groovy
@@ -1,5 +1,5 @@
 import groovy.json.JsonSlurper
-import att.comdev.cicd.config.conf
+import att.comdev.cicd.config.conf as cicdconf
 
 def imagesJson = '''{ "airship":[{
                         "repo":"airship",
@@ -17,7 +17,7 @@ for (entry in object.airship) {
     for (image in entry.images) {
       pipelineJob("images/${entry.repo}/${image}/${image}") {
             logRotator{
-                daysToKeep(conf.LOGROTATE_DAYS)
+                daysToKeep(cicdconf.LOGROTATE_DAYS)
             }
             configure {
                 node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
