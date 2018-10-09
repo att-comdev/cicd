d508a096b6709644d1b8262b786fd4f2311b839c
Jagan Kavva <jk330k@att.com>
2018-10-09 10:49:06 -0500

    Jenkins job log rotation
    For all the Ci/CD operations the Jenkins building and deployment
    job logs will be cleaned up in 90 days.
    
    Change-Id: I027eb8b50836821fc180c29e4bebadc26ea6846d

--- images/coredns/seed.groovy
+++ images/coredns/seed.groovy
@@ -3,6 +3,9 @@ JOB_NAME='coredns'
 
 folder(JOB_FOLDER)
 pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+    logRotator{
+        daysToKeep(90)
+    }
     parameters {
         stringParam {
             name ('GO_VERSION')
