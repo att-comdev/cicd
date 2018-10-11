2bc0d85373d1073a66dc11302bc4ddcf6482e768
SKELS, KASPARS (ks3019) <ks3019@att.com>
2018-10-11 15:40:25 -0500

    All helper library for ssh commands
    Change-Id: I34731214cb65762e7a59de56cb0f6383181f1cc3

---
+++ vars/ssh.groovy
@@ -0,0 +1,66 @@
+
+// wrapper for https://jenkins.io/doc/pipeline/steps/ssh-steps/
+// requires 'SSH Steps Plugin' to be installed in Jenkins
+
+/**
+ * Helper function to form remote object for the plugin
+ * https://github.com/jenkinsci/ssh-steps-plugin#remote
+ *
+ * @param creds Jenkins credentials ID setup for SSH
+ * @param ip The IP address or Hostname of the node
+ */
+def getRemote (String creds, String ip) {
+    withCredentials([sshUserPrivateKey(credentialsId: creds,
+        keyFileVariable: 'SSH_KEY',
+        usernameVariable: 'SSH_USER')]) {
+
+        def remote = [:]
+        remote.name = 'remote'
+        remote.host = ip
+        remote.allowAnyHosts = true
+
+        remote.user = SSH_USER
+
+        def key = readFile SSH_KEY
+        remote.identity = key
+
+        return remote
+    }
+}
+
+def cmd (String creds, String ip, String cmd) {
+    def remote = getRemote(creds, ip)
+    sshCommand remote: remote, command: cmd
+}
+
+
+/**
+ * Function used to wait and retry execution of a command.
+ * Good for waiting on the node to boot/restart and become available
+ */
+def wait (String creds, String ip, String cmd) {
+    retry (12) {
+        try {
+            cmd (creds, ip, cmd)
+        } catch (err) {
+            sleep 60
+            error(err)
+        }
+    }
+}
+
+def get (String creds, String ip, String src, String dst) {
+    def remote = getRemote(creds, ip)
+    sshGet remote: remote, from: src, into: dst, override: true
+}
+
+def put (String creds, String ip, String src, String dst) {
+    def remote = getRemote(creds, ip)
+    sshPut remote: remote, from: src, into: dst
+}
+
+def script (String creds, String ip, String script) {
+    def remote = getRemote(creds, ip)
+    sshScript remote: remote, script: script
+}
+
