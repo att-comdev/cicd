dc320f8baffe0c38d2df8edf8922ab4141be76af
SKELS, KASPARS (ks3019) <ks3019@att.com>
2018-10-11 15:40:25 -0500

    All helper library for ssh commands
    Change-Id: I34731214cb65762e7a59de56cb0f6383181f1cc3

---
+++ vars/ssh.groovy
@@ -0,0 +1,76 @@
+
+def cmd (String creds, String ip, String cmd) {
+    withCredentials([sshUserPrivateKey(credentialsId: creds,
+        keyFileVariable: 'SSH_KEY',
+        usernameVariable: 'SSH_USER')]) {
+
+        def remote = [:]
+        remote.name = 'genesis'
+        remote.host = ip
+        remote.user = SSH_USER
+        remote.identityFile = SSH_KEY
+        remote.allowAnyHosts = true
+
+        sshCommand remote: remote, command: cmd
+    }
+}
+
+def wait (String creds, String ip ) {
+    retry (12) {
+        try {
+            cmd (creds, ip, 'hostname')
+        } catch (err) {
+            sleep 60
+            error(err)
+        }
+    }
+}
+
+def put (String creds, String src, String dst) {
+    withCredentials([sshUserPrivateKey(credentialsId: creds,
+        keyFileVariable: 'SSH_KEY',
+        usernameVariable: 'SSH_USER')]) {
+
+        def remote = [:]
+        remote.name = 'genesis'
+        remote.host = ip
+        remote.user = SSH_USER
+        remote.identityFile = SSH_KEY
+        remote.allowAnyHosts = true
+
+        sshPut remote: remote, from: src, into: dst
+    }
+}
+
+def get (String creds, String src, String dst) {
+    withCredentials([sshUserPrivateKey(credentialsId: creds,
+        keyFileVariable: 'SSH_KEY',
+        usernameVariable: 'SSH_USER')]) {
+
+        def remote = [:]
+        remote.name = 'genesis'
+        remote.host = ip
+        remote.user = SSH_USER
+        remote.identityFile = SSH_KEY
+        remote.allowAnyHosts = true
+
+        sshGet remote: remote, from: src, into: dst, override: true
+    }
+}
+
+def script (String creds, String script) {
+    withCredentials([sshUserPrivateKey(credentialsId: creds,
+        keyFileVariable: 'SSH_KEY',
+        usernameVariable: 'SSH_USER')]) {
+
+        def remote = [:]
+        remote.name = 'genesis'
+        remote.host = ip
+        remote.user = SSH_USER
+        remote.identityFile = SSH_KEY
+        remote.allowAnyHosts = true
+
+        sshScript remote: remote, script: script
+    }
+}
+
