8c31714b952fdace72e9fafba265df5f2100a5dd
Kaspars Skels <kaspars.skels@gmail.com>
2018-10-15 23:26:12 -0500

    Add log publish and replace vm.groovy with vm3.groovy
     - Adds updating of Gerrit URL and log publishing
     - Replaces legacy vm.groovy that is not used anymore
    
    Change-Id: I304b1d2dbbb320322fb66f2af519927186089f57

--- vars/vm.groovy
+++ vars/vm.groovy
@@ -1,4 +1,7 @@
 
+import att.comdev.cicd.config.conf
+
+
 // Required credentials names
 //  - jenkins-openstack
 //  - jenkins-token
@@ -10,178 +13,93 @@
 //  - JENKINS_CLI
 
 
-def openstack_cmd(String cmd, String mount = "") {
-    keystone_image = "kolla/ubuntu-source-keystone:3.0.3"
+/**
+ * Create single node using a heat template
+ * Usage:
+ *       vm([:]) - All defaults
+ *       vm(doNotDeleteNode:true)
+ *       vm(initScript:'loci-bootstrap.sh', buildType:'loci')
+ *       vm(flavor:'m1.xlarge',
+ *           image:'cicd-ubuntu-18.04-server-cloudimg-amd64',
+ *           initScript:'loci-bootstrap.sh')
+ *
+**/
+def call(Map map,
+         Closure body) {
 
-    docker_env = " -e OS_AUTH_URL=http://keystone/v3" +
-                 " -e OS_PROJECT_DOMAIN_NAME=default" +
-                 " -e OS_USER_DOMAIN_NAME=default" +
-                 " -e OS_PROJECT_NAME=service" +
-                 " -e OS_REGION_NAME=RegionOne" +
-                 " -e OS_USERNAME=\$OS_USERNAME" +
-                 " -e OS_PASSWORD=\$OS_PASSWORD" +
-                 " -e OS_IDENTITY_API_VERSION=3"
+    // Startup script to run after VM instance creation
+    //  bootstrap.sh - default
+    //  loci-bootstrap.sh - for loci builds
+    def initScript = map.initScript ?: 'bootstrap.sh'
 
-    docker_opts = "--rm --net=host"
+    // image used for creating instance
+    def image = map.image ?: 'cicd-ubuntu-16.04-server-cloudimg-amd64'
 
-    if (mount) {
-        docker_opts += " -v ${mount}:/target"
+    // flavor type used for creating instance
+    def flavor = map.flavor ?: 'm1.medium'
+
+    // postfix string for instance nodename
+    def nodePostfix = map.nodePostfix ?: ''
+
+    // build template used for heat stack creation
+    //  basic - default
+    //  loci - for loci builds
+    def buildType = map.buildType ?: 'basic'
+
+    // Flag to control node cleanup after job execution
+    // Useful for retaining env for debugging failures
+    // NodeCleanup job be used to destroy the node later
+    //  false - default, deletes node after job
+    //  true - do not delete node
+    def doNotDeleteNode = map.doNotDeleteNode ?: false
+
+    // Flag to control Jenkins console log publishing to Artifactory.
+    //
+    // This will also set custom URL to be returned when voting in Gerrit
+    // https://jenkins.io/doc/pipeline/steps/gerrit-trigger/
+    //
+    // Useful for providing Jenkins console log when acting as 3rd party gate,
+    // especially when Jenkins itself is not accessible
+    def artifactoryLogs = map.artifactoryLogs ?: false
+
+    // resolve args to heat parameters
+    def parameters = " --parameter image=${image}" +
+                     " --parameter flavor=${flavor}"
+
+    // node used for launching VMs
+    def launch_node = 'jenkins-node-launch'
+
+    def name = "${JOB_BASE_NAME}-${BUILD_NUMBER}"
+
+    def stack_template="heat/stack/ubuntu.${buildType}.stack.template.yaml"
+
+    // optionally uer may supply additional identified for the VM
+    // this makes it easier to find it in OpenStack
+    if (nodePostfix) {
+      name += "-${nodePostfix}"
     }
 
-    return "docker run ${docker_opts} ${docker_env} ${keystone_image} ${cmd}"
-}
-
-
-def stack_create(String name, String tmpl) {
-
-
-    withCredentials([usernamePassword(credentialsId: 'jenkins-openstack-18',
-                                          usernameVariable: 'OS_USERNAME',
-                                          passwordVariable: 'OS_PASSWORD')]) {
-
-        cmd = openstack_cmd("openstack stack create -t /target/\$(basename ${tmpl}) ${name}", "\$(dirname ${tmpl})")
-        code = sh (script: cmd, returnStatus: true)
-        if (!code) {
-            // todo: improve timeouts to more user friendly look
-            timeout = 300
-            for (i = 0; i < timeout; i=i+10) {
-                sleep 30
-                cmd = openstack_cmd("openstack stack show -f value -c stack_status ${name}")
-                ret = sh (script: cmd, returnStdout: true).trim()
-                if (ret == "CREATE_COMPLETE") {
-                    print "Stack ${name} created!"
-                    return
-                } else if (ret != "CREATE_IN_PROGRESS") {
-                    print "Failed to create stack ${name}"
-                    sh "exit 1"
-                }
-            }
-        }
-        print "Failed to create stack ${name}"
-        sh "exit 1"
-    }
-}
-
-
-def stack_delete(String name) {
-    withCredentials([usernamePassword(credentialsId: 'jenkins-openstack-18',
-                                          usernameVariable: 'OS_USERNAME',
-                                          passwordVariable: 'OS_PASSWORD')]) {
-
-        cmd = openstack_cmd("openstack stack delete --yes $name")
-        code = sh (script: cmd, returnStatus: true)
-        if (!code) {
-            timeout = 30
-            for (i = 0; i < timeout; i=i+5) {
-                sleep 10
-                cmd = openstack_cmd("openstack stack list")
-                ret = sh (script: cmd, returnStdout: true)
-                if (!ret.contains(name)) {
-                    print "Stack ${name} deleted!"
-                    return
-                }
-            }
-            print "Failed to delete stack ${name}"
-            sh "exit 1" //
-        } else {
-            print "Likely stack ${name} did not exist! It's OK."
-        }
-    }
-}
-
-
-def stack_ip_get(String name) {
-    withCredentials([usernamePassword(credentialsId: 'jenkins-openstack-18',
-                                      usernameVariable: 'OS_USERNAME',
-                                      passwordVariable: 'OS_PASSWORD')]) {
-        cmd = openstack_cmd("openstack stack output show -f value -c output_value ${name} floating_ip")
-        return sh(returnStdout: true, script: cmd).trim()
-    }
-}
-
-
-def jenkins_node_config(String name, String host) {
-    config = """<slave>
-        <name>${name}</name>
-        <description></description>
-        <remoteFS>/home/ubuntu/jenkins</remoteFS>
-        <numExecutors>4</numExecutors>
-        <mode>EXCLUSIVE</mode>
-        <retentionStrategy class=\"hudson.slaves.RetentionStrategy\$Always\"/>
-        <launcher class=\"hudson.plugins.sshslaves.SSHLauncher\" plugin=\"ssh-slaves@1.5\">
-        <host>${host}</host>
-        <port>22</port>
-        <credentialsId>jenkins</credentialsId>
-        </launcher>
-        <label>${name}</label>
-        <nodeProperties/>
-        <userId>ubuntu</userId>
-        </slave>"""
-    return config
-}
-
-
-def jenkins_node_create(String name, String host) {
-    config = jenkins_node_config(name, host)
-    withCredentials([usernamePassword(credentialsId: 'jenkins-token',
-                                      usernameVariable: 'JENKINS_USER',
-                                      passwordVariable: 'JENKINS_TOKEN')]) {
-
-        opts = "-s \$JENKINS_URL -auth \$JENKINS_USER:\$JENKINS_TOKEN"
-        cmd = "echo '${config}' | java -jar \$JENKINS_CLI ${opts} create-node ${name}"
-        sh (script: cmd, returnStatus: true)
-    }
-}
-
-
-def jenkins_node_delete(String name) {
-    withCredentials([usernamePassword(credentialsId: 'jenkins-token',
-                                      usernameVariable: 'JENKINS_USER',
-                                      passwordVariable: 'JENKINS_TOKEN')]) {
-
-        opts = "-s \$JENKINS_URL -auth \$JENKINS_USER:\$JENKINS_TOKEN"
-        cmd = "java -jar \$JENKINS_CLI $opts delete-node $name"
-        code = sh (script: cmd , returnStatus: true)
-        // todo: handle exit code properly
-    }
-}
-
-
-def jenkins_vm_launch(String name, String tmpl, String host = "") {
-
-    stack_create(name, tmpl)
-
-    // use floating ip if not specified
-    if (!host) {
-        host = stack_ip_get(name)
-    }
-    jenkins_node_create (name, host)
-}
-
-
-def jenkins_vm_destroy(String name) {
-    jenkins_node_delete(name)
-    stack_delete(name)
-}
-
-
-
-// single node/vm job template
-//  - create vm based on gven template
-//  - clean-up after exceptions/failures
-//  - timeout if node is not getting ready
-
-def call(name, tmpl, Closure body) {
-
-    // node used for launching vms
-    def launch_node = 'local-vm-launch'
+    def ip = ""
 
     try {
         stage ('Node Launch') {
             node(launch_node) {
-                jenkins_vm_launch(name, "${HOME}/${tmpl}")
+                tmpl = libraryResource "${stack_template}"
+                writeFile file: 'template.yaml', text: tmpl
 
-                timeout (14) {
+                data = libraryResource "heat/stack/${initScript}"
+                writeFile file: initScript, text: data
+
+                heat.stack_create(name, "${WORKSPACE}/template.yaml", parameters)
+                ip = heat.stack_output(name, 'floating_ip')
+            }
+
+            node('master') {
+                jenkins.node_create (name, ip)
+            }
+
+            node(launch_node) {
+                 timeout (14) {
                     node(name) {
                         sh 'hostname'
                     }
@@ -195,15 +113,50 @@ def call(name, tmpl, Closure body) {
         }
 
     } catch (error) {
-        notify.msg("Pipeline failed: ${error}")
+        // notify.msg("Pipeline failed: ${error}")
         error(error)
 
     } finally {
-        stage ('Node Destroy') {
-            node(launch_node) {
-                jenkins_vm_destroy(name)
+        if (!doNotDeleteNode) {
+            stage ('Node Destroy') {
+                node('master') {
+                    jenkins.node_delete(name)
+                }
+                node(launch_node) {
+                   heat.stack_delete(name)
+                }
             }
         }
+
+        // publish Jenkins console output
+        // note: keep this as very last step to capture most logs
+        if (artifactoryLogs) {
+            node('master'){
+                try{
+                    def logBase = "logs/${JOB_NAME}/${BUILD_ID}"
+
+                    sh "curl -s -o console.log ${BUILD_URL}/consoleText"
+                    artifactory.upload ('console.log', "${logBase}/console.log")
+
+                    setGerritReview customUrl: "${conf.ARTF_WEB_URL}/${logBase}"
+
+                } catch (error){
+                    // gracefully handle failures to publish
+                    print "Failed to publish logs to Artifactory: ${err}"
+                }
+            }
+        }
+
     }
+  return ip
 }
 
+/**
+ *  This method allow for conventional usage as vm()
+ *       vm() - All defaults
+ *
+**/
+def call(Closure body) {
+   map = [:]
+    call(map, body)
+}
