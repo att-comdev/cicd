c995a063757aa24ce672daa83af0a1dbe5107e12
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2018-10-17 04:52:34 -0400

    Pipeline for att-comdev/charts
    This ps creates a new pipeline to build and deploy charts for
    att-comdev/charts
    reused deprecated Jenkinsfile in charts/att-comdev folder
    
    Change-Id: I3b3a24d1d1702322e2fbc12e28e046080bd2cd83

---
+++ charts/att-comdev/seed.groovy
@@ -0,0 +1,77 @@
+JOB_FOLDER="charts/att-comdev"
+folder(JOB_FOLDER)
+
+def projects = ['jenkins':'jenkins/.*',
+                'artifactory-ha':'artifactory-ha/.*',
+                'distribution':'distribution/.*',
+                'mission-control':'mission-control/.*',
+                'xray':'xray/.*']
+
+projects.each { project_name, file_path ->
+    JOB_BASE_NAME=project_name
+    pipelineJob("${JOB_FOLDER}/${JOB_BASE_NAME}") {
+        parameters {
+            stringParam {
+                name ('GERRIT_PROJECT')
+                defaultValue("att-comdev/charts")
+                description('Gerrit refspec or branch')
+            }
+            stringParam {
+                name ('GERRIT_REFSPEC')
+                defaultValue('master')
+                description('Gerrit refspec or branch')
+            }
+            stringParam {
+                name ('GERRIT_CHANGE_NUMBER')
+                defaultValue('0')
+                description('patchset number')
+            }
+            stringParam {
+                name ('GERRIT_EVENT_TYPE')
+                defaultValue('patchset-created')
+                description('patchset-created or change-merged')
+            }
+        }
+        triggers {
+            gerritTrigger {
+                serverName('Gerrithub-jenkins')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("att-comdev/charts")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        filePaths {
+                            filePath {
+                                compareType('REG_EXP')
+                                pattern("${file_path}")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+                triggerOnEvents {
+                    changeMerged()
+                    patchsetCreated {
+                        excludeDrafts(true)
+                        excludeTrivialRebase(false)
+                        excludeNoCodeChange(false)
+                    }
+                    commentAddedContains {
+                        commentAddedCommentContains('^recheck\$')
+                    }
+                }
+            }
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                    sandbox(false)
+                }
+            }
+        }
+    }
+}
