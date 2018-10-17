c70390be81fe17de9b0605e134916129fa21355c
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2018-10-17 04:52:34 -0400

    Pipeline for att-comdev/charts
    This ps creates a new pipeline to build and deploy charts for
    att-comdev/charts
    reused deprecated Jenkinsfile in charts/att-comdev folder
    
    Change-Id: I3b3a24d1d1702322e2fbc12e28e046080bd2cd83

---
+++ charts/att-comdev/seed.groovy
@@ -0,0 +1,125 @@
+JOB_FOLDER="charts/att-comdev"
+folder(JOB_FOLDER)
+
+def projects = ['jenkins',
+                'artifactory-ha',
+                'distribution',
+                'mission-control',
+                'xray']
+
+projects.each { project_name ->
+    JOB_BASE_NAME=project_name
+    pipelineJob("${JOB_FOLDER}/${JOB_BASE_NAME}") {
+        description("This job builds the helm charts for ${JOB_BASE_NAME}")
+        logRotator {
+            daysToKeep(30)
+        }
+        parameters {
+            stringParam {
+                name ('GERRIT_PROJECT')
+                defaultValue("att-comdev/charts")
+                description('Gerrit refspec or branch')
+            }
+            stringParam {
+                name ('GERRIT_REFSPEC')
+                defaultValue('master')
+                description('Gerrit refspec or branch')
+            }
+            stringParam {
+                name ('GERRIT_CHANGE_NUMBER')
+                defaultValue('0')
+                description('patchset number')
+            }
+            stringParam {
+                name ('GERRIT_EVENT_TYPE')
+                defaultValue('patchset-created')
+                description('patchset-created or change-merged')
+            }
+        }
+        triggers {
+            gerritTrigger {
+                serverName('Gerrithub-jenkins')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("att-comdev/charts")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        filePaths {
+                            filePath {
+                                compareType("ANT")
+                                pattern("${project_name}/*")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+                triggerOnEvents {
+                    changeMerged()
+                    patchsetCreated {
+                        excludeDrafts(true)
+                        excludeTrivialRebase(false)
+                        excludeNoCodeChange(false)
+                    }
+                    commentAddedContains {
+                        commentAddedCommentContains('recheck')
+                    }
+                }
+            }
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                    sandbox(false)
+                }
+            }
+        }
+    }
+}
+
+// This pipeline builds the charts in att-comdev/charts
+// for any changes in helm-toolkit to ensure charts are not broken
+JOB_BASE_NAME="openstack-helm-infra"
+pipelineJob("${JOB_FOLDER}/${JOB_BASE_NAME}") {
+    description('This job builds the helm charts for all components in att-comdev/charts \n' +
+                'for helm-toolkit changes in openstack/openstack-helm-infra')
+    logRotator {
+        daysToKeep(30)
+    }
+    triggers {
+        gerritTrigger {
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/openstack-helm-infra")
+                    branches {
+                        branch {
+                            compareType("ANT")
+                            pattern("**")
+                        }
+                    }
+                    filePaths {
+                        filePath {
+                            compareType("ANT")
+                            pattern("helm-toolkit/*")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                changeMerged()
+            }
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                sandbox(false)
+            }
+        }
+    }
+}
