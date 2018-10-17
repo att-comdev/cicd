44054e509e845ea90a6d1b269d4b50b93f9118f1
Shturm, Svetlana (ss233c) <sshturm@mirantis.com>
2018-10-17 15:26:56 -0500

    Adds new shipyard library
    This change adds new shipyard library that contains common
    operations that may be used in different pipelines.
    Also getter for keystone token was shcnaged to support
    authorisation for both cases:
     - from Jenkins credentials,
     - from username and password.
    
    Change-Id: I177e116071fd600823923da17a70bee146468f1c

--- vars/keystone.groovy
+++ vars/keystone.groovy
@@ -6,30 +6,47 @@ import groovy.json.JsonOutput
  *
  * @param keystoneCredId The ID of the credential (user+pass) established within Jenkins to authenticate against a site's Keystone
  * @param keystoneUrl The IAM URL of the site you are authenticating against.
+ * @param withCreds Boolean. Flag for using jenkins configuration to get keystone credentials.
+ *                           In case of disable flag will use keystoneCredId as a password and username as user for keystone request.
+ *                           Default value is True.
  * @return res The response supplied by the Keystone upon successful authentication
  */
-def retrieveToken(keystoneCredId, keystoneUrl) {
-    withCredentials([[$class: "UsernamePasswordMultiBinding",
-                      credentialsId: keystoneCredId,
-                      usernameVariable: "user",
-                      passwordVariable: "pass"]]) {
-
-        def req = ["auth": [
-                   "identity": [
-                     "methods": ["password"],
-                     "password": [
-                       "user": ["name": user,
-                                "domain": ["id": "default"],
-                                "password": pass]]]]]
-
-        def jreq = new JsonOutput().toJson(req)
-
-        def res = httpRequest(url: keystoneUrl + "/v3/auth/tokens",
-                              contentType: "APPLICATION_JSON",
-                              httpMode: "POST",
-                              quiet: true,
-                              requestBody: jreq)
-
-        return res
+def retrieveToken(keystoneCredId, keystoneUrl, withCreds=true, username='shipyard') {
+    def password = keystoneCredId
+    if (withCreds) {
+        withCredentials([[$class: "UsernamePasswordMultiBinding",
+                          credentialsId: keystoneCredId,
+                          usernameVariable: "user",
+                          passwordVariable: "pass"]]) {
+            username = user
+            password = pass
+        }
     }
+
+    def req = ["auth": [
+               "identity": [
+                 "methods": ["password"],
+                 "password": [
+                   "user": ["name": username,
+                            "domain": ["id": "default"],
+                            "password": password]]]]]
+
+    def jreq = new JsonOutput().toJson(req)
+
+    def res
+    retry (3) {
+        try {
+
+            res = httpRequest(url: keystoneUrl + "/v3/auth/tokens",
+                                  contentType: "APPLICATION_JSON",
+                                  httpMode: "POST",
+                                  quiet: true,
+                                  requestBody: jreq)
+
+        } catch (err) {
+            sleep 120
+            error(err)
+        }
+    }
+    return res
 }
