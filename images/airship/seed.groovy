fb423fbcfb0a578d163ac314936f08f16896d183
Jagan Kavva <jk330k@att.com>
2018-10-18 12:42:44 -0500

    Add log rotator logic to airship pipelines.
    The Jenkins job logs will be cleaned up every 90 days.
    Also made changes to check for sandbox() and change it to false.
    Change-Id: Ie4ee30952cb7e496f7e659f99b51e0521619cb40

--- images/airship/seed.groovy
+++ images/airship/seed.groovy
@@ -56,6 +56,9 @@ object = jsonSlurper.parseText(imagesJson)
 for (entry in object.github) {
   folder("${entry.directory}")
     pipelineJob("${entry.directory}/${entry.image}") {
+        logRotator{
+            daysToKeep(90)
+        }
         parameters {
             stringParam {
                 defaultValue("${entry.repo}")
@@ -107,7 +110,7 @@ for (entry in object.github) {
            definition {
                cps {
                    script(readFileFromWorkspace("images/airship/JenkinsfileMaster"))
-                   sandbox()
+                   sandbox(false)
                }
            }
         }
@@ -135,6 +138,9 @@ object = jsonSlurper.parseText(imagesJson)
 for (entry in object.github) {
   folder("${entry.directory}")
     pipelineJob("${entry.directory}/${entry.image}") {
+        logRotator{
+            daysToKeep(90)
+        }
         parameters {
             stringParam {
                 defaultValue("${entry.repo}")
@@ -185,7 +191,7 @@ for (entry in object.github) {
            definition {
                cps {
                  script(readFileFromWorkspace("images/airship/${entry.jenkinsfile_loc}"))
-                   sandbox()
+                   sandbox(false)
                }
            }
         }
