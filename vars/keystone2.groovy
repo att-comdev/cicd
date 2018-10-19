8e0cbc5c9bff96ba623f1b1312b3c248205abda9
Kaspars Skels <kaspars.skels@gmail.com>
2018-10-18 19:09:27 -0500

    Udpate keystone library to key/value args
    Change-Id: I84e3917e151b162b339355667866a7b8a6dcb6ee

---
+++ vars/keystone2.groovy
@@ -0,0 +1,43 @@
+import groovy.json.JsonOutput
+
+
+def token(Map map) {
+
+    def retryCount = map.retryCount ?: 3
+    def retryTimeout = map.retryTimeout ?: 120
+
+    withCredentials([[$class: "UsernamePasswordMultiBinding",
+                      credentialsId: map.keystoneCreds,
+                      usernameVariable: "USER",
+                      passwordVariable: "PASS"]]) {
+        map.keystoneUser = USER
+        map.keystonePassword = PASS
+    }
+
+    def req = ["auth": [
+               "identity": [
+                 "methods": ["password"],
+                 "password": [
+                   "user": ["name": map.keystoneUser,
+                            "domain": ["id": "default"],
+                            "password": map.keystonePassword ]]]]]
+
+    def jreq = new JsonOutput().toJson(req)
+
+    retry (map.retryCount) {
+        try {
+            def res = httpRequest(url: map.keystoneUrl + "/v3/auth/tokens",
+                                  contentType: "APPLICATION_JSON",
+                                  httpMode: "POST",
+                                  quiet: true,
+                                  requestBody: jreq)
+
+            return res.getHeaders()["X-Subject-Token"][0]
+
+        } catch (err) {
+            sleep map.retryTimeout
+            error(err)
+        }
+    }
+}
+
