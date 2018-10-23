b9cb6d6cf21febb349997ade2ce6087e4f6f9c8d
Shturm, Svetlana (ss233c) <sshturm@mirantis.com>
2018-10-23 17:48:31 -0500

    Add version library
    New version library may retrieve:
     - shipyard creds,
     - IPMI creds,
     - genesis hostname,
     - core DNS,
     - IPMI IPs,
     - genesis OAM IP,
     - ceph disks.
    
    Also added YAML generation site's data.
    
    Change-Id: I34b81b9904b4746a23f0fdfef1552787ce04d389

---
+++ vars/version.groovy
@@ -0,0 +1,102 @@
+def getShipyardCreds(siteName, sitePath=".") {
+    def shipyard = readYaml file: "${sitePath}/site/${siteName}/secrets/passphrases/ucp_shipyard_keystone_password.yaml"
+    return shipyard.data
+}
+
+
+def getIPMICreds(siteName, sitePath=".") {
+    def ipmi = readYaml file: "${sitePath}/site/${siteName}/secrets/passphrases/ipmi_admin_password.yaml"
+    return ipmi.data
+}
+
+def getGenesisHostname(siteName, sitePath=".") {
+    def data = readYaml file: "${sitePath}/site/${siteName}/networks/common-addresses.yaml"
+    return data.data.genesis.hostname
+}
+
+def getCoreDNS(siteName, sitePath=".") {
+    def data = readYaml file: "${sitePath}/site/${siteName}/networks/common-addresses.yaml"
+    return data.data.dns.service_ip
+}
+
+def getIPMIIPs(siteName, sitePath=".") {
+    def ips = []
+    patterns = ["rack*.yaml", "nodes.yaml"]
+    patterns.each {
+        files = findFiles(glob: "${sitePath}/site/${siteName}/baremetal/${it}")
+        files.each {
+                print "Reading file -> ${it}"
+                data = readYaml file: it.path
+                if ( !(data instanceof List)) {
+                  data = [data]
+                }
+                data.each {
+                    name = it.metadata.name
+                    it.data.addressing.each {
+                        res = ["name": name,
+                               "ips": ["network": it.network,
+                                       "address": it.address]
+                        ]
+                        if (it.network.contains("oob")) {
+                           ips = ips << res
+                        }
+                    }
+                }
+        }
+    }
+    return ips
+}
+
+def getGenesisOamIp(siteName, sitePath=".") {
+      def ip
+      patterns = ["rack*.yaml", "networks.yaml"]
+      patterns.each {
+          files = findFiles(glob: "${sitePath}/site/${siteName}/networks/physical/${it}")
+          files.each {
+              def data = readYaml file: it.path
+              if ( !(data instanceof List)) {
+                data = [data]
+              }
+              data.each {
+                  if (it.metadata.name.contains("oam")) {
+                      it.data.ranges.each {
+                          if (it.type == "static") {
+                              ip = ip ?: it.start
+                          }
+                      }
+                  }
+              }
+          }
+      }
+      return ip
+}
+
+def getCephDisks(siteName, sitePath=".") {
+    def disks = []
+    def data = readYaml file: "${sitePath}/site/${siteName}/software/charts/ucp/ceph/ceph-osd.yaml"
+    data.data.values.conf.storage.osd.each {
+        disks = disks << it.data.location
+    }
+    return disks
+}
+
+
+def generateYaml(siteName, sitePath=".") {
+    genesis_hostname = getGenesisHostname(siteName, sitePath)
+    genesis_oam_ip = getGenesisOamIp(siteName, sitePath)
+    genesis_node = ["name": genesis_hostname,
+                    "ips": [["netowk": "oob", "address": "GENESIS_IPMI"]],
+                    "metadata": ["tags": "genesis",
+                                 "genesis_oam_ip": genesis_oam_ip]
+    ]
+    node = [genesis_node]
+    ipmis = getIPMIIPs(siteName, sitePath)
+    node = node << ipmis
+    core_dns = getCoreDNS(siteName, sitePath)
+    disks = getCephDisks(siteName, sitePath)
+    res = ["data": ["node": node,
+                    "dns_service_ip": core_dns,
+                    "ceph": disks]
+    ]
+    return res
+}
