039bf6cb7d757219fe3518164d8f9f4f320c3a80
Shturm, Svetlana (ss233c) <sshturm@mirantis.com>
2018-10-23 13:16:20 -0500

    Add retrieve of service endpoint into keystone lib
    Changes:
     - new function for service endpoint in keystone2 library,
     - url for requests in shipyard2 changed according to shipyard endpoint url format.
    
    Change-Id: I9275dc1f109bf2956f7835ae1f1c925cae2736d6

--- vars/keystone2.groovy
+++ vars/keystone2.groovy
@@ -1,4 +1,5 @@
 import groovy.json.JsonOutput
+import groovy.json.JsonSlurperClassic
 
 
 /**
@@ -63,3 +64,134 @@ def token(Map map) {
     }
 }
 
+
+/**
+ * Retrieve service id from Keystone for given service type
+ * Usage:
+ *     keystone.getServiceId(keystoneUrl: 'http://keystone',
+ *                           token: 'token',
+                             'serviceType': 'shipyard')
+ *
+ * @param keystoneUrl
+ * @param token
+ * @param serviceType Default values is 'shipyard'
+ * @param retryCount
+ * @param retryTimeout
+ */
+def getServiceId(Map map) {
+
+    if (!map.containsKey('keystoneUrl')) {
+        error("Must provide Keystone URL 'keystoneUrl'")
+
+    } else if (!map.containsKey('token')) {
+        error("Must provide Keystone token 'token'")
+    }
+
+    // optional with defaults
+    def retryCount = map.retryCount ?: 3.toInteger()
+    def retryTimeout = map.retryTimeout ?: 120.toInteger()
+    def serviceType = map.serviceType ?: "shipyard"
+    retry (retryCount) {
+        try {
+
+            def res = httpRequest (url: map.keystoneUrl + "/v3/services?type=${serviceType}",
+                                   httpMode: "GET",
+                                   contentType: "APPLICATION_JSON",
+                                   customHeaders: [[name: "X-Auth-Token", value: map.token]],
+                                   quiet: true)
+            services = new JsonSlurperClassic().parseText(res.content)
+            service_id = services.services[0]["id"]
+            return service_id
+
+        } catch (err) {
+            print "Failed to get shipyard service id: ${err}"
+            sleep retryTimeout
+            error(err)
+        }
+    }
+}
+
+
+/**
+ * Retrieve service endpoint from Keystone for given serviceId and serviceInterface.
+ * Usage:
+ *     keystone.getServiceEndpoint(keystoneUrl: 'http://keystone',
+ *                                 token: 'token',
+                                   serviceId': 'serviceId',
+                                   serviceInterface: 'public')
+ *
+ * @param keystoneUrl
+ * @param token
+ * @param serviceId
+ * @param serviceInterface Default values is 'public'
+ * @param retryCount
+ * @param retryTimeout
+ */
+def _getServiceEndpoint(Map map) {
+
+    if (!map.containsKey('keystoneUrl')) {
+        error("Must provide Keystone URL 'keystoneUrl'")
+
+    } else if (!map.containsKey('token')) {
+        error("Must provide Keystone token 'token'")
+    } else if (!map.containsKey('serviceId')) {
+        error("Must provide Keystone service id 'serviceId'")
+    }
+
+    // optional with defaults
+    def retryCount = map.retryCount ?: 3.toInteger()
+    def retryTimeout = map.retryTimeout ?: 120.toInteger()
+    def serviceInterface = map.serviceInterface ?: "public"
+    retry (retryCount) {
+        try {
+
+            def res = httpRequest (url: map.keystoneUrl + "/v3/endpoints?service_id=${map.serviceId}&interface=${serviceInterface}",
+                                   httpMode: "GET",
+                                   contentType: "APPLICATION_JSON",
+                                   customHeaders: [[name: "X-Auth-Token", value: map.token]],
+                                   quiet: true)
+            endpoints = new JsonSlurperClassic().parseText(res.content)
+            return endpoints.endpoints[0]["url"]
+
+        } catch (err) {
+            print "Failed to get endpoint for service ${serviceId}: ${err}"
+            sleep retryTimeout
+            error(err)
+        }
+    }
+}
+
+/**
+ * Retrieve service endpoint from Keystone for given sevice type and interface.
+ * Usage:
+ *     keystone.getServiceEndpoint(keystoneUrl: 'http://keystone',
+ *                                 token: 'token',
+                                   serviceType': 'shipyard',
+                                   serviceInterface: 'public')
+ *
+ * @param keystoneUrl
+ * @param token
+ * @param serviceType Default '"shipyard'
+ * @param serviceInterface Default values is 'public'
+ * @param retryCount
+ * @param retryTimeout
+ */
+def getServiceEndpoint(Map map) {
+    if (!map.containsKey('keystoneUrl')) {
+        error("Must provide Keystone URL 'keystoneUrl'")
+
+    } else if (!map.containsKey('token')) {
+        error("Must provide Keystone token 'token'")
+    }
+    def serviceType = map.serviceType ?: "shipyard"
+    def serviceInterface = map.serviceInterface ?: "public"
+
+    service_id = getServiceId(keystoneUrl: map.keystoneUrl,
+                              token: map.token,
+                              serviceType: serviceType)
+    endpoint = _getServiceEndpoint(keystoneUrl: map.keystoneUrl,
+                              token: map.token,
+                              serviceId: service_id,
+                              serviceInterface: serviceInterface)
+    return endpoint
+}
