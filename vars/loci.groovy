969e07e2db773cfd1a783b6c43ac88711105da2c
Andrii Ostapenko <aostapenko@mirantis.com>
2018-11-01 12:47:53 -0500

    Mount ssh-agent socket to loci container during docker run
    That is the fourth change from chain, that intends to create an ability
    for loci to use sources that require authentication while creating
    docker images. With this change internal dependencies may be specified
    in upper-constraints.txt.
    
    With this change ssh-agent socket, available on host, is reused in
    loci container during docker run. Also SSH_DATA internal variable is
    introduced as a source data to compile loci docker ssh config and what
    credentials to use in ssh-agent.
    
    Change-Id: I9a6524cedd0ab7a0a7117f227c8055ca4d95f1ba

--- vars/loci.groovy
+++ vars/loci.groovy
@@ -13,6 +13,9 @@ def runDind(String artifactoryURL, String artifactoryCred, String containerName)
                " -e HTTP_PROXY=${HTTP_PROXY} -e HTTPS_PROXY=${HTTP_PROXY} "
     def mounts = '-v /var/lib/docker' +
                  ' -v $(pwd):/opt/loci'
+    if ("${SSH_AUTH_SOCK}") {
+        mounts += " -v ${SSH_AUTH_SOCK}:${CONT_SSH_AUTH_SOCK}"
+    }
 
     // cmd for running Docker in Docker
     dind = "sudo docker exec ${containerName}"
@@ -131,14 +134,15 @@ def mergeArgs(mapList) {
  *
  * @param paramMap Map Map that contains docker run parameters
  * @param unset Boolean Build empty parameters if true
+ * @param separator String used between key and value
  * @return args String containing ready to use parameter for docker run
  */
-def buildParameters(paramMap, dockerArg='--build-arg', unset=false) {
+def buildParameters(paramMap, dockerArg='--build-arg', unset=false, separator='=') {
     args = ''
     if(unset) {
         paramMap.each { entry -> args += " ${dockerArg} $entry.key" }
     } else {
-        paramMap.each { entry -> args += " ${dockerArg} $entry.key='$entry.value'" }
+        paramMap.each { entry -> args += " ${dockerArg} $entry.key${separator}'$entry.value'" }
     }
     return args
 }
