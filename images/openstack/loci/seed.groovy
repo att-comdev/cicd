d3d7f34dbf6ae8aecf6e57d823bc4b569a5babb2
Andrii Ostapenko <aostapenko@mirantis.com>
2018-11-02 13:01:23 -0500

    Allow to pass requirement loci image into loci build jobs
    Change-Id: I8bb81b962a17d9d18d1d06778905b2dda953aba4

--- images/openstack/loci/seed.groovy
+++ images/openstack/loci/seed.groovy
@@ -106,6 +106,12 @@ MOS_PROJECTS.each { project, ref ->
                             'Currently master is supported.')
                 name ('PROJECT_REF')
             }
+            stringParam {
+                defaultValue('')
+                description('Url to requirements loci image.\n\n' +
+                            'If empty, default one is used.')
+                name ('REQUIREMENTS_LOCI')
+            }
         }
         triggers {
             gerritTrigger {
