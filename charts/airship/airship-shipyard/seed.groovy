f514ef9bf0eb467dcc135026a8dc201f2987944c
Dimitrios Markou <dm844v@att.com>
2018-11-08 09:50:59 -0600

    [WIP] Shipyard Chart pipeline
    This patchset is the initial commit for the airship-shipyard pipeline
    to package, test, and publish the shipyard chart.
    
    Change-Id: I4cfff8f2f870f31bb3cb2c37999ef4bd06869d52
    Signed-off-by: Dimitrios Markou <dm844v@att.com>

---
+++ charts/airship/airship-shipyard/seed.groovy
@@ -0,0 +1,72 @@
+import groovy.json.JsonSlurper
+
+def chartsJson = '''{ "airship":[{
+                        "repo_name":"airship-shipyard",
+                        "git_repo":"openstack/airship-shipyard",
+                        "ref":"master"
+                    }]}'''
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(chartsJson)
+
+for (entry in object.airship) {
+    pipelineJob("charts/airship/${entry.repo_name}") {
+        disabled(false)
+        logRotator{
+            daysToKeep(90)
+        }
+        parameters {
+            stringParam('SHIPYARD_PROJECT',"${entry.git_repo}")
+            stringParam('INFRA_PROJECT', "openstack/openstack-helm-infra")
+            stringParam('OPENSTACK_HELM_INFRA_COMMIT', "master")
+        }
+        triggers {
+            gerritTrigger {
+                silentMode(true)
+                serverName('ATT-airship-CI')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("${entry.git_repo}")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("openstack/openstack-helm-infra")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+                triggerOnEvents {
+                    patchsetCreated {
+                        excludeDrafts(false)
+                        excludeTrivialRebase(false)
+                        excludeNoCodeChange(false)
+                    }
+                    changeMerged()
+                    commentAddedContains {
+                        commentAddedCommentContains('recheck')
+                    }
+                }
+            }
+            definition {
+                cps {
+                    script(readFileFromWorkspace('charts/airship/airship-shipyard/Jenkinsfile'))
+                    sandbox(false)
+                }
+            }
+        }
+    }
+}
+
