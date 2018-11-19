24f072761a39efca11982e1b4b0e803aca802ce9
sb464f <sb464f@att.com>
2018-11-19 21:04:05 +0000

    Add imaeg pipeline for minimirror
    Change-Id: Id293c2d011930286c7334230e8954d7566bff72f

---
+++ images/att-comdev/minimirror/seed.groovy
@@ -0,0 +1,57 @@
+JOB_FOLDER="images/att-comdev/minimirror"
+folder("images/att-comdev")
+folder("images/att-comdev/minimirror")
+pipelineJob("${JOB_FOLDER}/minimirror") {
+    logRotator{
+        daysToKeep(90)
+    }
+    configure {
+                node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                    durationName 'hour'
+                    count '3'
+                }
+    }
+    triggers {
+        gerritTrigger {
+            silentMode(false)
+            serverName('OS-CommunityGerrit')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("openstack/airship-utils")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**/master")
+                        }
+                    }
+                    filePaths {
+                        filePath {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(true)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(true)
+                }
+                changeMerged()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+            sandbox(false)
+        }
+    }
+}
+
