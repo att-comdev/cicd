2b25f2dcfd717bbf0a5d9e4166bb012fe844c219
Andrii Ostapenko <aostapenko@mirantis.com>
2018-11-28 13:52:08 -0600

    [test] switch to multijob for loci
    Change-Id: I5a7f109d98ddfdb718a74f1d3f6e10e3dcc50cba

---
+++ images/openstack/loci_multijob/seed.groovy
@@ -0,0 +1,27 @@
+JOB_BASE = 'images/openstack/loci_multijob'
+ref = "master"
+multiJob("complex") {
+    parameters {
+        stringParam {
+            defaultValue(ref)
+            description('Default branch for manual build.\n\n' +
+                        'Currently master is supported.')
+            name ('PROJECTS_BRANCH')
+        }
+        stringParam {
+            defaultValue(ref)
+            description('Default reference for manual build.\n\n' +
+                        'Branch or gerrit refspec is supported.')
+            name ('REQUIREMENTS_REF')
+        }
+    }
+    steps {
+        phase("Build requirements image") {
+            phaseJob("images/openstack/loci/mos/mos-requirements") {
+                parameters {
+                    stringParam('PROJECT_REF', "${REQUIREMENTS_REF}")
+                }
+            }
+        }
+    }
+}
