5deaa3f31e07ceabe6a659df7f9713f77e7f1fa3
Tim March <timothy.p.march@accenture.com>
2018-12-06 12:13:40 -0600

    [WIP] Update Calls for Pegleg
    Create duplicate pegleg2.groovy with updated calls
    
    Remove -p flag
    
    Move -v flag
    
    Fix method signature
    
    Update call again
    
    Update call again
    
    Change-Id: I1ac8c04940d6e3c47d6405c12a601d01a7ef7060

--- vars/pegleg2.groovy
+++ vars/pegleg2.groovy
@@ -1,5 +1,23 @@
 import att.comdev.cicd.config.conf
 
+
+/**
+ * Execution of Pegleg "lint" against a Pegleg
+ * container. NOTE: Currently setup to ignore
+ * P001 & P003. See: https://github.com/openstack/airship-pegleg/blob/master/src/bin/pegleg/pegleg/engine/errorcodes.py
+ * for more information on returned error codes.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def lint(siteRepo, globalRepo, username, sshKey, siteName) {
+    sh "sudo docker run --rm -i --net=none --workdir='/workspace' -v \$(pwd):/workspace \
+        $conf.PEGLEG_IMAGE pegleg -v -v site -r ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} lint ${siteName} -x P001 -x P003"
+}
+
 /**
  * Execution of Pegleg "lint" within a Pegleg
  * container. NOTE: Currently setup to ignore
@@ -13,6 +31,59 @@ import att.comdev.cicd.config.conf
  */
 def lintWithinContainer(siteRepo, globalRepo, secretsRepo, siteName) {
     sh "pegleg -v site -r ${siteRepo} -e global=${globalRepo} -e secrets=${secretsRepo} lint ${siteName} -w P001 -w P003"
+}
+
+/**
+ * Execution of Pegleg "lint" within a Pegleg
+ * container. NOTE: Currently setup to ignore
+ * P001 & P003. See: https://github.com/openstack/airship-pegleg/blob/master/src/bin/pegleg/pegleg/engine/errorcodes.py
+ * for more information on returned error codes.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param gerritRefspec The refspec of the manifests repo
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def lintWithinContainer(siteRepo, globalRepo, secretsRepo, username, sshKey, siteName, type) {
+  if(type == "directory"){
+      lintWithinContainer(siteRepo, globalRepo, secretsRepo, siteName)
+  } else {
+      sh "pegleg -v site -r ${siteRepo} -e global=${globalRepo} -e secrets=${secretsRepo} -u ${username} -k ${sshKey} lint ${siteName} -x P001 -x P003"
+  }
+}
+
+
+/**
+ * Execution of Pegleg "lint" within a Pegleg
+ * container. NOTE: Currently setup to ignore
+ * P001 & P003. See: https://github.com/openstack/airship-pegleg/blob/master/src/bin/pegleg/pegleg/engine/errorcodes.py
+ * for more information on returned error codes.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ * @param type Globalrepo and secretRepo can either be a local directory or a git repository url
+ */
+def lintWithinContainer(siteRepo, username, sshKey, siteName, type) {
+    sh "pegleg -v site -r ${siteRepo} -u ${username} -k ${sshKey} lint ${siteName} -x P001 -x P003"
+}
+
+/**
+ * Execution of Pegleg "render" against a Pegleg
+ * container. Redirects the output to a file, so
+ * it doesn't get written to stdout.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def render(siteRepo, globalRepo, username, sshKey, siteName) {
+    sh "sudo docker run --rm -i --net=none --workdir='/workspace' -v \$(pwd):/workspace \
+        $conf.PEGLEG_IMAGE pegleg site -p ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} render ${siteName} -o ${siteName}.yaml"
 }
 
 /**
@@ -30,6 +101,56 @@ def renderWithinContainer(siteRepo, globalRepo, secretsRepo, siteName) {
 }
 
 /**
+ * Execution of Pegleg "render" within a Pegleg
+ * container. Redirects the output to a file, so
+ * it doesn't get written to stdout.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ * @param "directory" for local directory repositories, "url" to override yaml site-definition
+ */
+def renderWithinContainer(siteRepo, globalRepo, secretsRepo, username, sshKey, siteName, type) {
+  if (type == "directory"){
+    renderWithinContainer(siteRepo, globalRepo, secretsRepo, siteName) 
+  } else {
+    sh "pegleg site -r ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} render ${siteName} -o ${siteName}.yaml"
+  }
+}
+
+/**
+ * Execution of Pegleg "render" within a Pegleg
+ * container. Redirects the output to a file, so
+ * it doesn't get written to stdout.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ * @param "directory" for local directory repositories, "url" to override yaml site-definition
+ */
+def renderWithinContainer(siteRepo, username, sshKey, siteName, type) {
+    sh "pegleg site -r ${siteRepo} -u ${username} -k ${sshKey} render ${siteName} -o ${siteName}.yaml"
+}
+
+/**
+ * Execution of Pegleg "collect" against a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def collect(siteRepo, globalRepo, username, sshKey, siteName) {
+    sh "docker run --rm -i --net=none --workdir=/workspace -v \$(pwd):/workspace \
+        $conf.PEGLEG_IMAGE pegleg site -p ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} collect ${siteName} -s ${siteName}"
+}
+
+/**
  * Execution of Pegleg "collect" within a Pegleg
  * container.
  *
@@ -40,4 +161,178 @@ def renderWithinContainer(siteRepo, globalRepo, secretsRepo, siteName) {
  */
 def collectWithinContainer(siteRepo, globalRepo, secretsRepo, siteName) {
     sh "pegleg site -r ${siteRepo} -e global=${globalRepo} -e secrets=${secretsRepo} collect ${siteName} -s ${siteName}"
+}
+
+/**
+ * Execution of Pegleg "collect" within a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ * @param "directory" for local directory repositories, "url" to override yaml site-definition
+ */
+def collectWithinContainer(siteRepo, globalRepo, secretsRepo, username, sshKey, siteName, type) {
+  if (type == "directory"){
+    collectWithinContainer(siteRepo, globalRepo, secretsRepo, siteName) 
+  } else {
+    sh "pegleg -v site -r ${siteRepo} -e global=${globalRepo} -e secrets=${secretsRepo} -u ${username} -k ${sshKey} collect ${siteName} -s ${siteName}"
+  }
+}
+
+/**
+ * Execution of Pegleg "collect" within a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ * @param "directory" for local directory repositories, "url" to override yaml site-definition
+ */
+def collectWithinContainer(siteRepo, username, sshKey, siteName, type) {
+    sh "pegleg -v site -r ${siteRepo} -u ${username} -k ${sshKey} collect ${siteName} -s ${siteName}"
+}
+
+/**
+ * Execution of Pegleg "encrypt" against a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def encrypt(siteRepo, globalRepo, username, sshKey, author, siteName) {
+    sh "docker run --rm -i --net=none --workdir=/workspace -v \$(pwd):/workspace \
+        $conf.PEGLEG_IMAGE pegleg.sh -v site -r ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} secrets encrypt -a ${author} ${siteName}"
+}
+
+/**
+ * Execution of Pegleg "encrypt" within a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def encryptWithinContainer(siteRepo, globalRepo, username, sshKey, author, siteName) {
+    sh "pegleg.sh -v site -r ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} secrets encrypt -a ${author} ${siteName}"
+}
+
+/**
+ * Execution of Pegleg "encrypt" against a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def encrypt(siteRepo, username, sshKey, author, siteName, peglegPassphrase, peglegSalt) {
+    sh "docker run --rm -i --net=none --workdir=/workspace -v \$(pwd):/workspace \
+        $conf.PEGLEG_IMAGE pegleg.sh -v site -r ${siteRepo} -u ${username} -k ${sshKey} secrets encrypt -a ${author} ${siteName} PEGLEG_PASSPHRASE=${peglegPassphrase} PEGLEG_SALT=${peglegSalt}"
+}
+
+/**
+ * Execution of Pegleg "encrypt" within a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def encryptWithinContainer(siteRepo, username, sshKey, author, siteName, passphrase, salt) {
+    sh "pegleg.sh -v site -r ${siteRepo} -u ${username} -k ${sshKey} secrets encrypt -a ${author} ${siteName} PEGLEG_PASSPHRASE=${peglegPassphrase} PEGLEG_SALT=${peglegSalt}"
+}
+
+/**
+ * Execution of Pegleg "generate" against a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def generate(siteRepo, globalRepo, username, sshKey, author, siteName) {
+    sh "docker run --rm -i --net=none --workdir=/workspace -v \$(pwd):/workspace \
+        $conf.PEGLEG_IMAGE pegleg.sh -v site -r ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} secrets generate passphrases ${siteName} -a ${author} -s /workspace"
+}
+
+/**
+ * Execution of Pegleg "generate" within a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def generateWithinContainer(siteRepo, globalRepo, username, sshKey, author, siteName) {
+    sh "pegleg.sh -v site -r ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} secrets generate passphrases ${siteName} -a ${author} -s /workspace"
+}
+
+/**
+ * Execution of Pegleg "generate-pki" against a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def generatePki(siteRepo, globalRepo, username, sshKey, author, siteName) {
+    sh "docker run --rm -i --net=none --workdir=/workspace -v \$(pwd):/workspace \
+        $conf.PEGLEG_IMAGE pegleg.sh -v site -r ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} secrets generate-pki -a ${author} ${siteName}"
+}
+
+/**
+ * Execution of Pegleg "generate-pki" within a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param globalRepo The global repository + commit/branch/tag to checkout.
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def generatePkiWithinContainer(siteRepo, globalRepo, username, sshKey, author, siteName) {
+    sh "pegleg.sh -v site -r ${siteRepo} -e global=${globalRepo} -u ${username} -k ${sshKey} secrets generate-pki -a ${author} ${siteName}"
+}
+
+/**
+ * Execution of Pegleg "generate-pki" against a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def generatePki(siteRepo, username, sshKey, author, siteName) {
+    sh "docker run --rm -i --net=none --workdir=/workspace -v \$(pwd):/workspace \
+        $conf.PEGLEG_IMAGE pegleg.sh -v site -r ${siteRepo} -u ${username} -k ${sshKey} secrets generate-pki -a ${author} ${siteName}"
+}
+
+/**
+ * Execution of Pegleg "generate-pki" within a Pegleg
+ * container.
+ *
+ * @param siteRepo The folder containing your site-level documents (must be at your PWD)
+ * @param username The username for the service account.
+ * @param sshKey The SSH key for the service account.
+ * @param siteName The name of the site you're looking to render. Must match what's in your site repository's site-definition.yaml
+ */
+def generatePkiWithinContainer(siteRepo, username, sshKey, author, siteName) {
+    sh "pegleg.sh -v site -r ${siteRepo} -u ${username} -k ${sshKey} secrets generate-pki -a ${author} ${siteName}"
 }
