fc4674a6ac3c5fcc298c8bf02c20f3e0a9616a4b
Tim March <timothy.p.march@accenture.com>
2018-12-13 15:34:02 -0600

    [WIP] NCMANFST-16
    Fix formatting
    
    Fix seed.groovy and Jenkinsfile
    
    Add default value
    
    Change Jenkinsfile location
    
    Change-Id: I41edfc2063c22b19c85a7c9bb0cd9db04c1729b6

---
+++ utils/seed.groovy
@@ -0,0 +1,77 @@
+import groovy.json.JsonSlurper
+
+//This will change once all images/airship/xxx are deprecated
+def imagesJson = '''{ "github":[{
+                                "repo":"https://git.openstack.org/openstack/airship-pegleg",
+                                "directory":"utils",
+                                "image":"pegleg",
+                                "name":"pegleg"
+                            }]}'''
+
+jsonSlurper = new JsonSlurper()
+object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.github) {
+    folder("${entry.directory}")
+    pipelineJob("${entry.directory}/${entry.image} - Reset Passphrase") {
+        logRotator {
+            daysToKeep(90)
+        }
+        parameters {
+            stringParam {
+                defaultValue("PASSPHRASE")
+                description('New passphrase')
+                name('PASSPHRASE')
+            }
+            stringParam {
+                defaultValue("mtn29")
+                description('Name of site')
+                name('PROJECT_SITE')
+            }
+        }
+        configure {
+            node ->
+                node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl' {
+                    durationName 'hour'
+                    count '10'
+                }
+        }
+        triggers {
+            gerritTrigger {
+                silentMode(true)
+                serverName('ATT-airship-CI')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("openstack/airship-${entry.name}")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+                triggerOnEvents {
+                    patchsetCreated {
+                        excludeDrafts(false)
+                        excludeTrivialRebase(false)
+                        excludeNoCodeChange(false)
+                    }
+                    changeMerged()
+                    commentAddedContains {
+                        commentAddedCommentContains('recheck')
+                    }
+                }
+            }
+
+            definition {
+                cps {
+                    script(readFileFromWorkspace("utils/Jenkinsfile"))
+                    sandbox(false)
+                }
+            }
+        }
+    }
+}
