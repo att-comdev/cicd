dd144cf96c9816a0f9b82d30b8540b592d3a6626
Kaspars Skels <kaspars.skels@gmail.com>
2019-01-04 13:56:54 -0600

    Add new stack mgmt library
    Change-Id: Id1c493e6a6a2dc0547454ab64cda42af34e4abf7

---
+++ vars/stack.groovy
@@ -0,0 +1,23 @@
+
+import groovy.json.JsonOutput
+
+
+def create(String url, String creds, String userData, String heatTemplate) {
+
+    token = keystone2.token(keystoneUrl: url, keystoneCreds: creds)
+    endpoint = keystone2.getServiceEndpoint(keystoneUrl: url, token: token, serviceType: 'orchestration')
+
+    ud = readFile file: userData
+    tmpl = readFile file: heatTemplate
+
+    spec = ['files': ['bootstrap.sh': ud],
+            'stack_name': 'ks3019-test-stack',
+            'template': tmpl]
+
+    output = new JsonOutput().toJson(spec)
+
+    print output
+
+
+}
+
