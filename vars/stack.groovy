965eb8098d782acf2258bc4653439b7ee84bbbe7
Kaspars Skels <kaspars.skels@gmail.com>
2019-01-04 13:56:54 -0600

    Add new stack mgmt library
    Change-Id: Id1c493e6a6a2dc0547454ab64cda42af34e4abf7

---
+++ vars/stack.groovy
@@ -0,0 +1,16 @@
+
+def create(String url, String creds, String userData, String heatTemplate) {
+
+    keystone2.token(keystoneUrl: url, keystoneCreds: creds)
+
+    // ud = readFile file: userData,
+    // tmpl = readFile file: heatTemplate
+
+    // print ud
+    // print tmpl
+
+    // read user data
+    // set parameters
+    // read template
+}
+
