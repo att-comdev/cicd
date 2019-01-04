38ed1857e8193704c195b9c94bc6709d0e8e56cb
Tim March <timothy.p.march@accenture.com>
2019-01-04 16:31:15 -0600

    [WIP] NCMANFST-13
    Update seed.groovy
    
    Remove call to global repo
    
    Allow for passphrase with encrypt command
    
    Set passwordParam to String param. Will fix it later.
    
    Update seed.groovy to allow for choices of sites, update gerrit.groovy
    with new method
    
    Fix choices param in seed.groovy
    
    Revert changes to remove parameters. Change site to string param
    
    Change-Id: Iab021690e25269484d901c710ec6d5f197bbec47

---
+++ utils/seed.groovy
@@ -0,0 +1,47 @@
+import groovy.json.JsonSlurper
+
+//This will change once all images/airship/xxx are deprecated
+def imagesJson = '''{ "github":[{
+                                "repo":"https://git.openstack.org/openstack/airship-pegleg",
+                                "directory":"utils",
+                                "image":"pegleg",
+                                "name":"pegleg"
+                            }]}'''
+
+jsonSlurper = new JsonSlurper()
+object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.github) {
+    folder("${entry.directory}")
+    pipelineJob("${entry.directory}/${entry.image} - Regenerate Certificate") {
+        logRotator {
+            daysToKeep(90)
+        }
+        parameters {
+            stringParam {
+                defaultValue("PASSPHRASE")
+                description('New passphrase')
+                name('PASSPHRASE')
+            }
+            stringParam {
+                defaultValue("SITE")
+                description('Name of site')
+                name('PROJECT_SITE')
+            }
+        }
+        configure {
+            node ->
+                node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl' {
+                    durationName 'hour'
+                    count '10'
+                }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("utils/Jenkinsfile"))
+                sandbox(false)
+            }
+        }
+    }
+}
