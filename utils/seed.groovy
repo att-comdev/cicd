b04ca16c5cd9707a43c9b08e65694e85ba3fe5ce
Tim March <timothy.p.march@accenture.com>
2019-01-21 09:29:38 -0600

    [WIP] NCMANFST-14
    Generate Genesis Scripts
    
    Use parameters and remove pegleg2.groovy change
    
    Remove unused def
    
    Change-Id: I86f47fb9af5889459a233195cde21f3598b9ccdd

---
+++ utils/seed.groovy
@@ -0,0 +1,48 @@
+import groovy.json.JsonSlurper
+
+//This will change once all images/airship/xxx are deprecated
+def imagesJson = '''{ "github":[{
+                                "repo":"https://git.openstack.org/openstack/airship-pegleg",
+                                "directory":"utils",
+                                "image":"pegleg",
+                                "name":"pegleg"
+                            }]}'''
+
+jsonSlurper = new JsonSlurper()
+object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.github) {
+    folder("${entry.directory}")
+    pipelineJob("${entry.directory}/${entry.image} - Regenerate Certificate") {
+        logRotator {
+            daysToKeep(90)
+        }
+        parameters {
+            stringParam {
+                defaultValue("DESTINATION_DIRECTORY")
+                description('Destination Directory')
+                name('DESTINATION_DIRECTORY')
+            }
+            stringParam {
+                defaultValue("SITE")
+                description('Name of site')
+                name('PROJECT_SITE')
+            }
+            choiceParam ('PROJECT_SITE', "${sites}", 'Name of site')
+        }
+        configure {
+            node ->
+                node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl' {
+                    durationName 'hour'
+                    count '10'
+                }
+        }
+
+        definition {
+            cps {
+                script(readFileFromWorkspace("utils/Jenkinsfile"))
+                sandbox(false)
+            }
+        }
+    }
+}
