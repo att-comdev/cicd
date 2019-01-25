6daab97216a98d4f1a5b06c05a5b0f3c8c3445db
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2019-01-25 10:52:38 -0500

    pipeline to build ceph-daemon image
    This ps adds a new pipeline to build the ceph-daemon image.
    The pipeline uses a pod template to run the job within the container
    
    Change-Id: I330965b0c660d67395b1662d5acb57d1caec3ccb

---
+++ images/openstack/helm-images/seed.groovy
@@ -0,0 +1,87 @@
+JOB_FOLDER="images/openstack/helm-images"
+folder(JOB_FOLDER)
+
+def projects = ['ceph-daemon']
+
+projects.each { project_name ->
+    JOB_BASE_NAME=project_name
+    pipelineJob("${JOB_FOLDER}/${JOB_BASE_NAME}") {
+        description("This job builds the image for ${JOB_BASE_NAME}")
+        logRotator {
+            daysToKeep(90)
+        }
+        parameters {
+            stringParam {
+                name ('GERRIT_PROJECT')
+                defaultValue("openstack/openstack-helm-images")
+                description('Gerrit refspec or branch')
+            }
+            stringParam {
+                name ('GERRIT_REFSPEC')
+                defaultValue('master')
+                description('Gerrit refspec or branch')
+            }
+            stringParam {
+                name ('GERRIT_PATCHSET_REVISION')
+                defaultValue('0')
+                description('patchset revision')
+            }
+            stringParam {
+                name ('GERRIT_EVENT_TYPE')
+                defaultValue('patchset-created')
+                description('patchset-created or change-merged')
+            }
+            stringParam {
+                name ('GERRIT_CHANGE_URL')
+                defaultValue('manual')
+                description('Change URL
+            }
+        }
+        triggers {
+            gerritTrigger {
+                silentMode(true)
+                serverName('ATT-airship-CI')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern('openstack/openstack-helm-images')
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        filePaths {
+                            filePath {
+                                compareType('PLAIN')
+                                pattern('Makefile')
+                            }
+                            filePath {
+                                compareType("ANT")
+                                pattern("${project_name}/**")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                }
+                triggerOnEvents {
+                    changeMerged()
+                    patchsetCreated {
+                        excludeDrafts(true)
+                        excludeTrivialRebase(false)
+                        excludeNoCodeChange(false)
+                    }
+                    commentAddedContains {
+                        commentAddedCommentContains('recheck')
+                    }
+                }
+            }
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                    sandbox(false)
+                }
+            }
+        }
+    }
+}
