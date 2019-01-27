7536f1788e45f653b993831baab66d2fc819e1ee
Andrii Ostapenko <aostapenko@mirantis.com>
2019-01-27 17:44:14 -0600

    temp test change for generic review pipeline
    Change-Id: I936ad0950508c0e9f9f8b02d3cc697e0695d55f6

--- images/openstack/loci/mos-review/seed.groovy
+++ images/openstack/loci/mos-review/seed.groovy
@@ -157,6 +157,7 @@ PROJECT_MAP.each { projectName, buildTypes ->
                 "UPDATE_TOPIC":        UPDATE_TOPIC,
                 "PROJECT_NAME":        projectName,
                 "BUILD_TYPE":          buildType,
+                "LOCI_BUILD_SLAVE_LABEL": LOCI_BUILD_SLAVE_LABEL,
             )
         }
     }
@@ -174,6 +175,9 @@ pipelineJob("${JOB_BASE}/TestDeploymentPipeline") {
     options {
         disableResume()
     }
+    environmentVariables(
+        "LOCI_BUILD_SLAVE_LABEL": LOCI_BUILD_SLAVE_LABEL,
+    )
     parameters {
         stringParam {
             defaultValue('')
