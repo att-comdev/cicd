050b765fdc38f4d1977f6954d4de11c0fea77115
Dejaeger, Darren (dd118r) <dd118r@att.com>
2019-03-22 17:44:49 -0400

    [multiple-creds] change shared libs to allow multiple cred attempts
    This PS allows multiple creds to be passed to the functions that
    require them, in an attempt to more easily facilitate a change in
    creds during the middle of a workflow.
    
    Change-Id: I992c4acab316ed514b497d9493d69e27f070c21e

--- vars/keystone.groovy
+++ vars/keystone.groovy
@@ -4,49 +4,53 @@ import groovy.json.JsonOutput
  * Retrieve a token from a site's Keystone that can potentially be used to talk to and perform actions with the
  * other k8s cluster components.
  *
- * @param keystoneCredId The ID of the credential (user+pass) established within Jenkins to authenticate against a site's Keystone
+ * @param keystoneCreds The ID of the credential (user+pass) established within Jenkins to authenticate against a site's Keystone
  * @param keystoneUrl The IAM URL of the site you are authenticating against.
  * @param withCreds Boolean. Flag for using jenkins configuration to get keystone credentials.
  *                           In case of disable flag will use keystoneCredId as a password and username as user for keystone request.
  *                           Default value is True.
  * @return res The response supplied by the Keystone upon successful authentication
  */
-def retrieveToken(keystoneCredId, keystoneUrl, withCreds=true, username='shipyard') {
-    def password = keystoneCredId
-    if (withCreds) {
-        withCredentials([[$class: "UsernamePasswordMultiBinding",
-                          credentialsId: keystoneCredId,
-                          usernameVariable: "user",
-                          passwordVariable: "pass"]]) {
-            username = user
-            password = pass
+def retrieveToken(keystoneCreds, keystoneUrl, withCreds=true, username='shipyard') {
+    keystoneCreds.each {
+        def password = it
+        if (withCreds) {
+            withCredentials([[$class: "UsernamePasswordMultiBinding",
+                              credentialsId: it,
+                              usernameVariable: "user",
+                              passwordVariable: "pass"]]) {
+                username = user
+                password = pass
+            }
         }
-    }
 
-    def req = ["auth": [
-               "identity": [
-                 "methods": ["password"],
-                 "password": [
-                   "user": ["name": username,
-                            "domain": ["id": "default"],
-                            "password": password]]]]]
+        def req = ["auth": [
+                   "identity": [
+                     "methods": ["password"],
+                     "password": [
+                       "user": ["name": username,
+                                "domain": ["id": "default"],
+                                "password": password]]]]]
 
-    def jreq = new JsonOutput().toJson(req)
+        def jreq = new JsonOutput().toJson(req)
 
-    def res
-    retry (3) {
-        try {
-
-            res = httpRequest(url: keystoneUrl + "/v3/auth/tokens",
+        def res
+        retry (3) {
+            try {
+                res = httpRequest(url: keystoneUrl + "/v3/auth/tokens",
                                   contentType: "APPLICATION_JSON",
                                   httpMode: "POST",
                                   quiet: true,
                                   requestBody: jreq)
-
-        } catch (err) {
-            sleep 120
-            error(err)
+            } catch(err) {
+                if(res && res.status != 401) {
+                    sleep 120
+                } else {
+                    sleep 5
+                }
+                error(err)
+            }
         }
+        return res
     }
-    return res
 }
