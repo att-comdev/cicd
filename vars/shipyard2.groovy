153edf53cb993ffe61bc3cc1ab28d78c3668c14f
Shturm, Svetlana (ss233c) <ss233c@att.com>
2019-03-29 16:30:32 -0500

    [shipyard2] Add debug kubectl commands into wait action
    Nodes and pods info can be helpfull for developers to understand
    why shipyard action is failed. It will give them some initial info
    for debug before they will connect to genesis node and check other
    services.
    
    Change-Id: Ia5d4bce1624c0c427124ff3d2567403d0a522e7d

--- vars/shipyard2.groovy
+++ vars/shipyard2.groovy
@@ -348,14 +348,16 @@ def _printActionSteps(action) {
  * @param keystoneUrl The IAM URL of the site you are authenticating against.
  * @param withCreds Boolean. Flag for using jenkins configuration to get keystone credentials.
  * @param parameters Optional map of parameters needed to create action
+ * @param genesisCreds Jenkins creds name for debug kubectl command execution during action check
+ * @param genesisIp IP address of genesis node for debug kubectl command execution
  */
-def waitAction(action, uuid, shipyardUrl, keystoneCredId, keystoneUrl, withCreds=true, parameters = null) {
+def waitAction(action, uuid, shipyardUrl, keystoneCredId, keystoneUrl, withCreds=true, parameters = null, genesisCreds=null, genesisIp=null) {
 
     def actionId
     stage('Action create') {
-        def req = keystone.retrieveToken(keystoneCredId, keystoneUrl, withCreds, parameters)
+        def req = keystone.retrieveToken(keystoneCredId, keystoneUrl, withCreds)
         def token = req.getHeaders()["X-Subject-Token"][0]
-        def res = createAction(uuid, token, shipyardUrl, action)
+        def res = createAction(uuid, token, shipyardUrl, action, parameters)
         def cont = new JsonSlurperClassic().parseText(res.content)
         actionId = cont.id
     }
@@ -381,6 +383,7 @@ def waitAction(action, uuid, shipyardUrl, keystoneCredId, keystoneUrl, withCreds
                 error("Step ${failedSteps} failed")
             }
         }
+        def debug = true
         runningSteps.each() {
             if ( !(it in stages) & !(it.toString() in skipSteps)) {
                 stages += it
@@ -390,6 +393,17 @@ def waitAction(action, uuid, shipyardUrl, keystoneCredId, keystoneUrl, withCreds
                 //stage creation. All other stages will be in running state until job is finished.
                 // Add sleep to fix this issue with hanging stages.
                 sleep 5
+            }
+            // Execute kubectl debug command only once for all running steps for each action check.
+            // For drydock_build step check nodes state. For all other check pods state.
+            if (genesisCreds && genesisIp && debug) {
+                if (it.toString() == 'drydock_build') {
+                    ssh.cmd (genesisCreds, genesisIp, 'sudo kubectl get nodes')
+                    debug = false
+                } else {
+                    ssh.cmd (genesisCreds, genesisIp, 'sudo kubectl get pods --all-namespaces | grep -vE "Completed|Running"')
+                    debug = false
+                }
             }
         }
     }
