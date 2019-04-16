a8c20072a3d673e45a5f91db8e5c566233470470
Thippaiah, Balachandra (bt061u) <bt061u@att.com>
2019-04-16 13:33:09 -0400

    Add on demand job to build image used in code-review pipeline
    This ps creates a new pipeline to build image with preinstalled
    packages to be run on demand.
    This would allow us to containerize our code-review pipeline
    resulting in faster execution
    
    Change-Id: I908bb931b4014d0dec8c409b285282fdebb83142

---
+++ images/code-review-image-build/seed.groovy
@@ -0,0 +1,26 @@
+JOB_FOLDER="images"
+folder(JOB_FOLDER)
+
+JOB_BASE_NAME="code-review-image-build"
+pipelineJob("${JOB_FOLDER}/${JOB_BASE_NAME}") {
+    description("This job builds the image used in code-review pipeline")
+    logRotator {
+        daysToKeep(90)
+    }
+    parameters {
+        stringParam {
+            name ('BASE_IMAGE')
+            defaultValue('')
+            description('Base image defaults to com.att.nccicd.config.conf.POD_IMAGE_1604')
+            trim(true)
+        }
+    }
+    triggers {
+        definition {
+            cps {
+                script(readFileFromWorkspace("${JOB_FOLDER}/${JOB_BASE_NAME}/Jenkinsfile"))
+                sandbox(false)
+            }
+        }
+    }
+}
