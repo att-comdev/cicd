bf36596cc55ee9b86da6420f6b4a8f296270d938
Matt McEuen <MM9745@att.com>
2019-04-22 09:19:37 -0500

    [fix] Adjust Airship image build jobs for opendev
    The Airship project (along with the other OpenStack Foundation
    projects) moved to new git hosting infrastructure with a new
    domain name - review.opendev.org.  This change adjusts the
    cicd project to build images from these new locations.  As part
    of this change, the 'airship-' prefixes have been removed from
    project names, and the projects instead live under an /airship
    namespace (instead of the previous /openstack).
    
    - Move the pipeline structure to de-nest the jobs a bit
    - Disable the promenade multinode gate as it isn't related
      to image build and also fails always.
    
    Change-Id: I429b5016ee68645047b376187e5f715cb0d7b4fe

--- images/airship/seed.groovy
+++ images/airship/seed.groovy
@@ -2,58 +2,56 @@ import groovy.json.JsonSlurper
 //This will change once all images/airship/xxx are deprecated
 folder("images")
 folder("images/airship")
-folder("images/airship/update")
-folder("images/airship/update/airship-shipyard")
-folder("images/airship/update/airship-maas")
-folder("images/airship/patchset")
+basefolder="images/airship"
+
 def imagesJson = '''{ "github":[{
-                               "repo":"https://git.openstack.org/openstack/airship-shipyard",
-                               "directory":"images/airship/update/airship-shipyard/shipyard",
+                               "repo":"https://review.opendev.org/airship/shipyard",
+                               "directory":"${basefolder}",
                                "image":"shipyard",
                                "name":"shipyard"
                             },{
-                               "repo":"https://git.openstack.org/openstack/airship-shipyard",
-                                "directory":"images/airship/update/airship-shipyard/airflow",
+                               "repo":"https://review.opendev.org/airship/shipyard",
+                                "directory":"${basefolder}",
                                 "image":"airflow",
                                 "name":"shipyard"
                             },{
-                               "repo":"https://git.openstack.org/openstack/airship-drydock",
-                                "directory":"images/airship/update/airship-drydock",
+                               "repo":"https://review.opendev.org/airship/drydock",
+                                "directory":"${basefolder}",
                                 "image":"drydock",
                                 "name":"drydock"
                             },{
-                                "repo":"https://git.openstack.org/openstack/airship-maas",
-                                "directory":"images/airship/update/airship-maas/maas-region-controller",
+                                "repo":"https://review.opendev.org/airship/maas",
+                                "directory":"${basefolder}",
                                 "image":"maas-region-controller",
                                 "name":"maas"
                             },{
-                                "repo":"https://git.openstack.org/openstack/airship-maas",
-                                "directory":"images/airship/update/airship-maas/maas-rack-controller",
+                                "repo":"https://review.opendev.org/airship/maas",
+                                "directory":"${basefolder}",
                                 "image":"maas-rack-controller",
                                 "name":"maas"
                             },{
-                                "repo":"https://git.openstack.org/openstack/airship-maas",
-                                "directory":"images/airship/update/airship-maas/sstream-cache",
+                                "repo":"https://review.opendev.org/airship/maas",
+                                "directory":"${basefolder}",
                                 "image":"sstream-cache",
                                 "name":"maas"
                             },{
-                                "repo":"https://git.openstack.org/openstack/airship-deckhand",
-                                "directory":"images/airship/update/airship-deckhand",
+                                "repo":"https://review.opendev.org/airship/deckhand",
+                                "directory":"${basefolder}",
                                 "image":"deckhand",
                                 "name":"deckhand"
                             },{
-                                "repo":"https://git.openstack.org/openstack/airship-armada",
-                                "directory":"images/airship/update/airship-armada",
+                                "repo":"https://review.opendev.org/airship/armada",
+                                "directory":"${basefolder}",
                                 "image":"armada",
                                 "name":"armada"
                             },{
-                                "repo":"https://git.openstack.org/openstack/airship-pegleg",
-                                "directory":"images/airship/update/airship-pegleg",
+                                "repo":"https://review.opendev.org/airship/pegleg",
+                                "directory":"${basefolder}",
                                 "image":"pegleg",
                                 "name":"pegleg"
                             },{
-                                "repo":"https://git.openstack.org/openstack/airship-promenade",
-                                "directory":"images/airship/update/airship-promenade",
+                                "repo":"https://review.opendev.org/airship/promenade",
+                                "directory":"${basefolder}",
                                 "image":"promenade",
                                 "name":"promenade"
                             }]}'''
@@ -94,7 +92,7 @@ for (entry in object.github) {
                 gerritProjects {
                     gerritProject {
                         compareType('PLAIN')
-                        pattern("openstack/airship-${entry.name}")
+                        pattern("airship/${entry.name}")
                         branches {
                             branch {
                                 compareType("ANT")
@@ -127,78 +125,80 @@ for (entry in object.github) {
     }
 }
 
-imagesJson = '''{ "github":[{
-                                "repo":"https://git.openstack.org/openstack/airship-promenade",
-                                "directory":"images/airship/patchset/airship-promenade",
-                                "image":"multi-node-promenade",
-                                "name":"promenade",
-                                "jenkinsfile_loc":"JenkinsfilePromenade"
-                            }]}'''
-
-jsonSlurper = new JsonSlurper()
-object = jsonSlurper.parseText(imagesJson)
-
-for (entry in object.github) {
-  folder("${entry.directory}")
-    pipelineJob("${entry.directory}/${entry.image}") {
-        logRotator{
-            daysToKeep(90)
-        }
-        parameters {
-            stringParam {
-                defaultValue("${entry.repo}")
-                description('Name of repo in airship to build')
-                name ('GIT_REPO')
-                trim(true)
-            }
-            stringParam {
-                defaultValue("1.0.0")
-                description('Put RC version here')
-                name('VERSION')
-                trim(true)
-            }
-        }
-        configure {
-            node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
-                durationName 'hour'
-                count '10'
-            }
-        }
-        triggers {
-            gerritTrigger {
-                silentMode(true)
-                serverName('ATT-airship-CI')
-                gerritProjects {
-                    gerritProject {
-                        compareType('PLAIN')
-                        pattern("openstack/airship-${entry.name}")
-                        branches {
-                            branch {
-                                compareType("ANT")
-                                pattern("**")
-                            }
-                        }
-                        disableStrictForbiddenFileVerification(false)
-                    }
-                }
-                triggerOnEvents {
-                    patchsetCreated {
-                       excludeDrafts(false)
-                       excludeTrivialRebase(false)
-                       excludeNoCodeChange(false)
-                    }
-                    commentAddedContains {
-                        commentAddedCommentContains('recheck')
-                    }
-                }
-           }
-
-           definition {
-               cps {
-                 script(readFileFromWorkspace("images/airship/${entry.jenkinsfile_loc}"))
-                   sandbox(false)
-               }
-           }
-        }
-    }
-}
+//TODO(sh8121) this should be moved to something outside of images as it is not a build
+//             pipeline. also it currently fails always.
+//imagesJson = '''{ "github":[{
+//                                "repo":"https://review.opendev.org/airship/promenade",
+//                                "directory":"images/airship/patchset/promenade",
+//                                "image":"multi-node-promenade",
+//                                "name":"promenade",
+//                                "jenkinsfile_loc":"JenkinsfilePromenade"
+//                            }]}'''
+//
+//jsonSlurper = new JsonSlurper()
+//object = jsonSlurper.parseText(imagesJson)
+//
+//for (entry in object.github) {
+//  folder("${entry.directory}")
+//    pipelineJob("${entry.directory}/${entry.image}") {
+//        logRotator{
+//            daysToKeep(90)
+//        }
+//        parameters {
+//            stringParam {
+//                defaultValue("${entry.repo}")
+//                description('Name of repo in airship to build')
+//                name ('GIT_REPO')
+//                trim(true)
+//            }
+//            stringParam {
+//                defaultValue("1.0.0")
+//                description('Put RC version here')
+//                name('VERSION')
+//                trim(true)
+//            }
+//        }
+//        configure {
+//            node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+//                durationName 'hour'
+//                count '10'
+//            }
+//        }
+//        triggers {
+//            gerritTrigger {
+//                silentMode(true)
+//                serverName('ATT-airship-CI')
+//                gerritProjects {
+//                    gerritProject {
+//                        compareType('PLAIN')
+//                        pattern("airship/${entry.name}")
+//                        branches {
+//                            branch {
+//                                compareType("ANT")
+//                                pattern("**")
+//                            }
+//                        }
+//                        disableStrictForbiddenFileVerification(false)
+//                    }
+//                }
+//                triggerOnEvents {
+//                    patchsetCreated {
+//                       excludeDrafts(false)
+//                       excludeTrivialRebase(false)
+//                       excludeNoCodeChange(false)
+//                    }
+//                    commentAddedContains {
+//                        commentAddedCommentContains('recheck')
+//                    }
+//                }
+//           }
+//
+//           definition {
+//               cps {
+//                 script(readFileFromWorkspace("images/airship/${entry.jenkinsfile_loc}"))
+//                   sandbox(false)
+//               }
+//           }
+//        }
+//    }
+//}
