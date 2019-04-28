3aa52e6ee2f96c479b5daa81daf3b84390b39ed2
Sukant JR <suka.dx@gmail.com>
2019-04-27 23:25:49 -0500

    Jenkins image builder pipeline
    Change-Id: Idd7969bdb9edb6c793809c4e507d0a39a321dd7c

---
+++ images/jenkins/seed.groovy
@@ -0,0 +1,98 @@
+JOB_FOLDER="images/jenkins"
+folder(JOB_FOLDER)
+def projects = ['jenkins']
+projects.each { project_name ->
+    JOB_BASE_NAME=project_name
+    pipelineJob("${JOB_FOLDER/}") {
+        description("This job builds the image for ${JOB_BASE_NAME}")
+        logRotator {
+            daysToKeep(90)
+        }
+        parameters {
+            stringParam {
+                name ('GERRIT_REFSPEC')
+                defaultValue('master')
+                description('Gerrit refspec or branch')
+                trim(true)
+            }
+            stringParam {
+                name ('GERRIT_PATCHSET_REVISION')
+                defaultValue('0')
+                description('patchset revision')
+                trim(true)
+            }
+            stringParam {
+                name ('GERRIT_EVENT_TYPE')
+                defaultValue('patchset-created')
+                description('patchset-created or change-merged')
+                trim(true)
+            }
+            stringParam {
+                name ('DEFAULT_TAG')
+                defaultValue('lts')
+                description('Tag of the upstream image that needs to be built')
+                trim(true)
+            }
+        }
+        triggers {
+            gerritTrigger {
+                silentMode(true)
+                serverName('Gerrithub-jenkins')
+                gerritProjects {
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("att-comdev/charts")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        filePaths {
+                            filePath {
+                                compareType("ANT")
+                                pattern("${project_name}/**")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+                    gerritProject {
+                        compareType('PLAIN')
+                        pattern("att-comdev/dockerfiles")
+                        branches {
+                            branch {
+                                compareType("ANT")
+                                pattern("**")
+                            }
+                        }
+                        filePaths {
+                            filePath {
+                                compareType("ANT")
+                                pattern("${project_name}/**")
+                            }
+                        }
+                        disableStrictForbiddenFileVerification(false)
+                    }
+
+                }
+                triggerOnEvents {
+                    changeMerged()
+                    patchsetCreated {
+                        excludeDrafts(true)
+                        excludeTrivialRebase(false)
+                        excludeNoCodeChange(false)
+                    }
+                    commentAddedContains {
+                        commentAddedCommentContains('recheck')
+                    }
+                }
+            }
+            definition {
+                cps {
+                    script(readFileFromWorkspace("${JOB_FOLDER}/Jenkinsfile"))
+                    sandbox(false)
+                }
+            }
+        }
+    }
+}
