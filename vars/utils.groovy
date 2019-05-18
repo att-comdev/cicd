5d5d1eaf6b23432deb7a012d7e3343eb0660d968
Andrii Ostapenko <aostapenko@mirantis.com>
2019-05-17 22:03:02 -0500

    Add implementation of retry that respects aborts
    Change-Id: Ib2d5c4908262c624f06707f549a9b2517991e6c9

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,21 +1,50 @@
-/* Handy method to use instead of retry before workflow-basic-steps 2.7
-   Currently native retry does not throw up FlowInterruptedException
-   that makes job abortion messy.
-   Usage:
-       retrier(3) {
-           ...
-       }
+ABORT_MESSAGE = "Job was aborted."
+ABORT_ON = ["Aborted by", "Calling Pipeline was cancelled", ABORT_MESSAGE]
+
+/* Method to run downstream jobs to be used in combination with retrier. */
+def runBuild(name, parameters, retries=1) {
+    retrier (retries) {
+        job = build(
+            job: name,
+            wait: true,
+            propagate: false,
+            parameters: parameters
+        )
+        if (job.result == 'SUCCESS') { return job }
+        else if (job.result == 'ABORTED') { throw new Exception("'${name}': ${ABORT_MESSAGE}")  }
+        else { throw new Exception("'${name}': Job failed.") }
+    }
+}
+
+
+/* Method that allows to retry enclosed body that respects job abort,
+   including upstream and downstream ones
+      Usage:
+         retrier(3) {
+             ...
+         }
 */
 def retrier(int retries, Closure body) {
+    def lastError
     for(int i=0; i<retries; i++) {
+        lastError = null
         try {
-            body.call()
+            result = body.call()
             break
-        } catch (hudson.AbortException | org.jenkinsci.plugins.workflow.steps.FlowInterruptedException fie) {
-            throw fie
-        } catch (Exception e) {
-            echo "${e}"
+        } catch (err) {
+            echo "${err}"
+            sleep 1
+            lastLog = currentBuild.rawBuild.getLog(20).join()
+            if (lastLog.find(ABORT_ON.join("|"))) {
+                currentBuild.result = 'ABORTED'
+                throw err
+            }
+            lastError = err
             continue
         }
     }
+    if (lastError) {
+        throw lastError
+    }
+    return result
 }
