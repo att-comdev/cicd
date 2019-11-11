531ca93d6cf47bd05850f7cb08dc2538bb3c6031
jh629g <jh629g@att.com>
2019-11-11 11:44:37 -0600

    Update ranger image build job
    Updating ranger build job to support 1.9
    and 2.0 images building to seperate folders
    in artifactory and ensuring that respective
    builds build from their respective branches
    
    Also removed reference to quay repository
    as public posting of ranger images will
    be changed to occur as a Gate in upstream
    ranger patchsets.
    
    Change-Id: I8907ab9bfa4636cfea52dbba29ff5b949476be3a

--- images/openstack/ranger/seed.groovy
+++ images/openstack/ranger/seed.groovy
@@ -1,5 +1,4 @@
 import groovy.json.JsonSlurper
-
 def imagesJson = '''{ "os":[{
                         "repo":"x/ranger",
                         "images":[
@@ -9,18 +8,24 @@ def imagesJson = '''{ "os":[{
                         },{
                         "repo":"x/ranger-agent",
                         "images":["ranger-agent"]}]}'''
-
 def jsonSlurper = new JsonSlurper()
 def object = jsonSlurper.parseText(imagesJson)
 folder("images")
 folder("images/openstack")
 folder("images/openstack/ranger")
-
 for (entry in object.os) {
     for (image in entry.images) {
       pipelineJob("images/openstack/ranger/${image}") {
             logRotator{
               daysToKeep(90)
+            }
+            parameters {
+                stringParam("REPO_NAME", entry.repo, "Upstream repository")
+                stringParam("GERRIT_EVENT_TYPE", "Manual", "This will be used in naming the job when it runs.")
+                stringParam("GERRIT_PATCHSET_REVISION", "", "Patchset or latest master Commit ID")
+                stringParam("GERRIT_CHANGE_ID", "", "Change ID of patchset or latest merge")
+                stringParam("GERRIT_CHANGE_URL", "review.opendev.org/x/ranger", "Link to the patchset or to the upstream repo this job is run for")
+                stringParam("GERRIT_REFSPEC", "", "Only necessary when building from patchset.")
             }
             triggers {
                 gerritTrigger {
@@ -47,7 +52,6 @@ for (entry in object.os) {
                         changeMerged()
                     }
                 }
-
                 definition {
                     cps {
                       script(readFileFromWorkspace("images/openstack/ranger/Jenkinsfile"))
