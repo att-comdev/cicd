580ebd6ee945eeb9144ab702d3b08b71cabb4e76
Andrii Ostapenko <aostapenko@mirantis.com>
2019-12-23 16:27:17 -0600

    Refactor vm and heat templates to allow use vms via jumphost
    Change-Id: Ie55ef259158c709c9e595ea34597c5a98225cd8d

---
+++ cicd/JumpHost/Jenkinsfile.groovy
@@ -0,0 +1,45 @@
+CONFIG_HEADER = """
+global
+       daemon
+       maxconn 10000
+
+defaults
+       timeout connect 500s
+       timeout client 5000s
+       timeout server 1h
+"""
+
+def getConfigSection(Integer octet) {
+    def frontend = "frontend-${octet}"
+    def backend = "backend-${octet}"
+    def server = "server-${octet}"
+    def ip = "10.0.0.${octet}"
+    def port = 10000 + octet
+    return """
+frontend ${frontend}
+       bind *:${port}
+       default_backend ${backend}
+       timeout client 1h
+
+backend ${backend}
+       mode tcp
+       server ${server} ${ip}:22
+"""
+}
+
+
+def compileConfig() {
+    def config = CONFIG_HEADER
+    2..254.each {
+        config += getConfigSection(it)
+    }
+    writeFile file: "myhaproxy.cfg", text: config
+}
+
+
+vm (doNotDeleteNode: true, publicNet: "routable") {
+    sh "sudo bash -c 'apt-get update && apt-get install haproxy -y'"
+    compileConfig()
+    sh "sudo cp myhaproxy.cfg /etc/haproxy/haproxy.cfg"
+    sh "sudo service haproxy restart"
+}
