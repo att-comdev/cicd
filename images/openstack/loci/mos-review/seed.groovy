61679609a980016193c20c8d918eae7c26f228f1
Andrii Ostapenko <andrii.ostapenko@att.com>
2020-06-26 12:02:09 -0500

    Run tox for mos on VMs
    Due to unstable pod related infrastructure and poor resource isolation.
    Some functional tests are resource dependent and consistently fail
    on timeout.
    
    Signed-off-by: Andrii Ostapenko <andrii.ostapenko@att.com>
    Change-Id: I45c0b1394f1ac24627873422b705f88f0213800f

--- images/openstack/loci/mos-review/seed.groovy
+++ images/openstack/loci/mos-review/seed.groovy
@@ -620,3 +620,38 @@ pipelineJob("${JOB_BASE}/UpliftPipeline") {
         }
     }
 }
+
+
+pipelineJob("${JOB_BASE}/code-review") {
+    description("This job is for python applications to run pep8, bandit, unit tests, and code coverage")
+    logRotator{
+        daysToKeep(90)
+    }
+    parameters {
+        stringParam {
+            defaultValue("")
+            description('Internal project name for manual build.')
+            name ('PROJECT_NAME')
+            trim(true)
+        }
+        stringParam {
+            defaultValue("")
+            description('Reference for manual build of internal project.\n\n' +
+                        'Branch or gerrit refspec is supported.')
+            name ('PROJECT_REF')
+            trim(true)
+        }
+        stringParam {
+            defaultValue("")
+            description('Branch for manual build of internal project.\n\n')
+            name ('PROJECT_BRANCH')
+            trim(true)
+        }
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace("${JOB_BASE}/JenkinsfileTox"))
+            sandbox()
+        }
+    }
+}
