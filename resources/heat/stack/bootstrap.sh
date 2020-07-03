78c9dd98ef46b772b926c6fcf50cb8e2d651977d
Andrii Ostapenko <andrii.ostapenko@att.com>
2020-07-02 22:28:37 -0500

    Fix bootstrap.sh for vm and remove related w/a in OSH AIO
    Signed-off-by: Andrii Ostapenko <andrii.ostapenko@att.com>
    Change-Id: I36579664854fe7e8b8e975a1362f3b7048f6bdde

--- resources/heat/stack/bootstrap.sh
+++ resources/heat/stack/bootstrap.sh
@@ -6,5 +6,5 @@ apt-get install -y default-jre-headless
 echo "====== Updating /etc/hosts ======"
 HOSTNAME=$(hostname)
 cat << EOF | sudo tee -a /etc/hosts
-127.0.0.1 $HOSTNAME
+127.0.1.1 $HOSTNAME
 EOF
