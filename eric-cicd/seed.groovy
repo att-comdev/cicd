ab0b37101fb7398a33562b35d1b053954a7fd4c6
Shabrinath Motamary <motamary346@gmail.com>
2020-09-08 16:16:47 -0500

    CAPD Jenkins pipeline
    Change-Id: I764240425a9f38c2810748d71859949c2bbeac59

---
+++ eric-cicd/seed.groovy
@@ -0,0 +1,49 @@
+
+JOB_FOLDER="Deploy Work Load Cluster_CAPD"
+pipelineJob("${JOB_FOLDER}") {
+    properties {
+        disableConcurrentBuilds()
+    }
+    logRotator{
+        daysToKeep(90)
+    }
+    triggers {
+        gerritTrigger {
+            silentMode(true)
+            serverName('airship-ci')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("airship/airshipctl")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(false)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(true)
+                    excludePrivateState(false)
+                    excludeWipState(false)
+                }
+                changeMerged()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/Jenkins_CAPD"))
+            sandbox(false)
+        }
+    }
+}
+
