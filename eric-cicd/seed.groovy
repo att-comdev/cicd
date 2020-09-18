b9298da07e552a1b908ce02055a2feb0812f69ad
sureshl <suresh.lakshmipathy@ericsson.com>
2020-09-18 11:31:51 -0500

    Folder restructure
    Change-Id: I4d89e7fd77e8bd388c8510987faa42b536f499d5

--- eric-cicd/seed.groovy
+++ eric-cicd/seed.groovy
@@ -1,6 +1,7 @@
 
-JOB_FOLDER="Deploy Work Load Cluster_CAPD"
-pipelineJob("${JOB_FOLDER}") {
+JOB_FOLDER="CAPD"
+JOB_NAME="DeployOnCAPD"
+pipelineJob("${JOB_NAME}") {
     properties {
         disableConcurrentBuilds()
     }
@@ -41,9 +42,57 @@ pipelineJob("${JOB_FOLDER}") {
     }
     definition {
         cps {
-          script(readFileFromWorkspace("${JOB_FOLDER}/Jenkins_CAPD"))
+          script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capd"))
             sandbox(false)
         }
     }
 }
 
+JOB_NAME="DeployOnCAPZ"
+JOB_FOLDER="CAPZ"
+pipelineJob("${JOB_NAME}") {
+    properties {
+        disableConcurrentBuilds()
+    }
+    logRotator{
+        daysToKeep(90)
+    }
+    triggers {
+        gerritTrigger {
+            silentMode(true)
+            serverName('airship-ci')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("airship/airshipctl")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(false)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(true)
+                    excludePrivateState(false)
+                    excludeWipState(false)
+                }
+                changeMerged()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capz"))
+            sandbox(false)
+        }
+    }
+}
