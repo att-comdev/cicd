aebb97c6cb713fe0ec135aeb3b4e095afeba84eb
sureshl <suresh.lakshmipathy@ericsson.com>
2020-09-18 11:31:51 -0500

    Folder ref updated
    Change-Id: I3338e0394ee0eccf1ffbed31909fddd87145aade

---
+++ eric-cicd/seed.groovy
@@ -0,0 +1,105 @@
+JOB_FOLDER="eric-cicd/CAPD"
+JOB_NAME="DeployOnCAPD"
+pipelineJob("${JOB_NAME}") {
+    properties {
+        disableConcurrentBuilds()
+    }
+    logRotator{
+        daysToKeep(90)
+    }
+    parameters {
+        stringParam {
+            name ('targetVM')
+            defaultValue('10.1.1.31')
+            description('Target VM to deploy')
+            trim(true)
+        }
+    }
+    triggers {
+        gerritTrigger {
+            silentMode(true)
+            serverName('airship-ci')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("airship/airshipctl")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(false)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(true)
+                    excludePrivateState(false)
+                    excludeWipState(false)
+                }
+                changeMerged()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capd"))
+            sandbox(false)
+        }
+    }
+}
+
+JOB_NAME="DeployOnCAPZ"
+JOB_FOLDER="eric-cicd/CAPZ"
+pipelineJob("${JOB_NAME}") {
+    properties {
+        disableConcurrentBuilds()
+    }
+    logRotator{
+        daysToKeep(90)
+    }
+    triggers {
+        gerritTrigger {
+            silentMode(true)
+            serverName('airship-ci')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("airship/airshipctl")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(false)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(true)
+                    excludePrivateState(false)
+                    excludeWipState(false)
+                }
+                changeMerged()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capz"))
+            sandbox(false)
+        }
+    }
+}
