3cbd95eb5a56a3e88b74ae040f5e235158eb2e9e
sureshl <suresh.lakshmipathy@ericsson.com>
2020-09-18 11:31:51 -0500

    Adding capz pipeline script and update to seed.groovy
    Change-Id: Ibb9b4af0841979c6044153a48a0037915d4a3800

--- eric-cicd/seed.groovy
+++ eric-cicd/seed.groovy
@@ -1,5 +1,5 @@
 
-JOB_FOLDER="Deploy Work Load Cluster_CAPD"
+JOB_FOLDER="Deploy_CAPD"
 pipelineJob("${JOB_FOLDER}") {
     properties {
         disableConcurrentBuilds()
@@ -47,3 +47,50 @@ pipelineJob("${JOB_FOLDER}") {
     }
 }
 
+JOB_FOLDER="DeployOnCAPZ"
+pipelineJob("${JOB_FOLDER}") {
+    properties {
+        disableConcurrentBuilds()
+    }
+    logRotator{
+        daysToKeep(90)
+    }
+    triggers {
+        gerritTrigger {
+            silentMode(true)
+            serverName('airship-ci')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("airship/airshipctl")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(false)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(true)
+                    excludePrivateState(false)
+                    excludeWipState(false)
+                }
+                changeMerged()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capz"))
+            sandbox(false)
+        }
+    }
+}
