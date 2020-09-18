b747c84e00f903bedd62377f0ae4a9bd6b1f03d4
sureshl <suresh.lakshmipathy@ericsson.com>
2020-09-18 11:31:51 -0500

    Folder ref updated
    Change-Id: I3338e0394ee0eccf1ffbed31909fddd87145aade

--- eric-cicd/seed.groovy
+++ eric-cicd/seed.groovy
@@ -1,6 +1,63 @@
+JOB_FOLDER="eric-cicd/CAPD"
+JOB_NAME="DeployOnCAPD"
+pipelineJob("${JOB_NAME}") {
+    properties {
+        disableConcurrentBuilds()
+    }
+    logRotator{
+        daysToKeep(90)
+    }
+    parameters {
+        stringParam {
+            name ('targetVM')
+            defaultValue('10.1.1.31')
+            description('Target VM to deploy')
+            trim(true)
+        }
+    }
+    triggers {
+        gerritTrigger {
+            silentMode(true)
+            serverName('airship-ci')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("airship/airshipctl")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    disableStrictForbiddenFileVerification(false)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(false)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(true)
+                    excludePrivateState(false)
+                    excludeWipState(false)
+                }
+                changeMerged()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capd"))
+            sandbox(false)
+        }
+    }
+}
 
-JOB_FOLDER="Deploy Work Load Cluster_CAPD"
-pipelineJob("${JOB_FOLDER}") {
+JOB_NAME="DeployOnCAPZ"
+JOB_FOLDER="eric-cicd/CAPZ"
+pipelineJob("${JOB_NAME}") {
     properties {
         disableConcurrentBuilds()
     }
@@ -41,9 +98,8 @@ pipelineJob("${JOB_FOLDER}") {
     }
     definition {
         cps {
-          script(readFileFromWorkspace("${JOB_FOLDER}/Jenkins_CAPD"))
+          script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capz"))
             sandbox(false)
         }
     }
 }
-
