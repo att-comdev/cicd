f499eb2df0eb30a8b9649d79a98c76dda8da5824
esurlak <suresh.lakshmipathy@ericsson.com>
2020-09-30 08:45:57 -0500

    VPP CNF
    Change-Id: I6c0bf0e58a70b084ced98ebfe9fe9e1210beca0a

--- eric-cicd/seed.groovy
+++ eric-cicd/seed.groovy
@@ -64,41 +64,26 @@ pipelineJob("${JOB_NAME}") {
     logRotator{
         daysToKeep(90)
     }
-    triggers {
-        gerritTrigger {
-            silentMode(true)
-            serverName('airship-ci')
-            gerritProjects {
-                gerritProject {
-                    compareType('PLAIN')
-                    pattern("airship/airshipctl")
-                    branches {
-                        branch {
-                            compareType('ANT')
-                            pattern("**")
-                        }
-                    }
-                    disableStrictForbiddenFileVerification(false)
-                }
-            }
-            triggerOnEvents {
-                patchsetCreated {
-                    excludeDrafts(false)
-                    excludeTrivialRebase(false)
-                    excludeNoCodeChange(true)
-                    excludePrivateState(false)
-                    excludeWipState(false)
-                }
-                changeMerged()
-                commentAddedContains {
-                   commentAddedCommentContains('recheck')
-                }
-            }
-        }
-    }
     definition {
         cps {
           script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capz"))
+            sandbox(false)
+        }
+    }
+}
+
+JOB_NAME="DeployVppIpFwdOnAZ"
+JOB_FOLDER="eric-cicd/CAPZ"
+pipelineJob("${JOB_NAME}") {
+    properties {
+        disableConcurrentBuilds()
+    }
+    logRotator{
+        daysToKeep(90)
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/jenkins_capz_vpp_cnf"))
             sandbox(false)
         }
     }
