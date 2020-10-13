de0207b1c360267a460c28477a893853b58774db
Andrii Ostapenko <andrii.ostapenko@att.com>
2020-10-13 10:06:03 -0500

    Don't use floating ip in vm
    Signed-off-by: Andrii Ostapenko <andrii.ostapenko@att.com>
    Change-Id: I2118d58e44377ccba43a6e2bc6e3d160dcc18bc2

---
+++ resources/heat/stack/bootstrap-jumphost.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "====== Installing JAVA packages ======"
+apt-get update
+apt-get install -y default-jre-headless
+
+echo "====== Updating /etc/hosts ======"
+HOSTNAME=$(hostname)
+cat << EOF | sudo tee -a /etc/hosts
+127.0.1.1 $HOSTNAME
+EOF
+
+echo "====== Setup insterfaces ======"
+netfile=$(find /etc/network/interfaces.d -name "*.cfg")
+for interface in $(ls -1 /sys/class/net | grep ens); do
+  if [ $interface != "ens3" ];then
+    sudo bash -c "echo 'auto $interface' >> ${netfile}"
+    sudo bash -c "echo 'iface $interface inet dhcp' >> ${netfile}"
+    sudo ifup $interface
+  fi
+done
