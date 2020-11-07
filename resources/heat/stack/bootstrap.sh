d44b764ffb4e5fc9a9731e466fd0e4106c574915
Andrii Ostapenko <andrii.ostapenko@att.com>
2020-11-07 14:14:07 -0600

    Setup all interfaces in bootstrap for vm
    Signed-off-by: Andrii Ostapenko <andrii.ostapenko@att.com>
    Change-Id: Ie5255e63f1bf0b6851e776a0e9879eb4fd2231da

--- resources/heat/stack/bootstrap.sh
+++ resources/heat/stack/bootstrap.sh
@@ -8,3 +8,14 @@ HOSTNAME=$(hostname)
 cat << EOF | sudo tee -a /etc/hosts
 127.0.1.1 $HOSTNAME
 EOF
+
+echo "====== Setup insterfaces ======"
+netfile=$(find /etc/network/interfaces.d -name "*.cfg")
+for interface in $(ls -1 /sys/class/net | grep ens); do
+  if [ $interface != "ens3" ];then
+    sudo bash -c "echo 'auto $interface' >> ${netfile}"
+    sudo bash -c "echo 'iface $interface inet dhcp' >> ${netfile}"
+    sudo ifdown $interface
+    sudo ifup $interface
+  fi
+done
--- resources/heat/stack/loci-bootstrap.sh
+++ resources/heat/stack/loci-bootstrap.sh
@@ -4,6 +4,17 @@ set -e
 
 echo "127.0.1.1   $(hostname)" >> /etc/hosts
 
+echo "====== Setup insterfaces ======"
+netfile=$(find /etc/network/interfaces.d -name "*.cfg")
+for interface in $(ls -1 /sys/class/net | grep ens); do
+  if [ $interface != "ens3" ];then
+    sudo bash -c "echo 'auto $interface' >> ${netfile}"
+    sudo bash -c "echo 'iface $interface inet dhcp' >> ${netfile}"
+    sudo ifdown $interface
+    sudo ifup $interface
+  fi
+done
+
 # required for docker within docker
 mkdir -p /etc/docker
 cat << EOF > /etc/docker/daemon.json
