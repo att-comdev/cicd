3972445281c60613ce8d569ab7c6c71c0a27eda7
sureshl <suresh.lakshmipathy@ericsson.com>
2021-03-16 12:05:35 -0500

    seed groovy script for nc
    Change-Id: I133a81bfaf1fcb51ced96c27dac2ed914dc3d8b6

---
+++ nc/seed.groovy
@@ -0,0 +1,62 @@
+JOB_FOLDER="nc/Airshipctl"
+JOB_NAME="Airshipctl"
+pipelineJob("${JOB_NAME}") {
+    parameters {
+        stringParam {
+            name ('GERRIT_REFSPEC')
+            defaultValue('master')
+            description('The gerrit refspec')
+	    trim(true)
+        }
+        stringParam {
+            name ('NODE_LABEL')
+            defaultValue('airship1')
+            description('The node label of the slave')
+	    trim(true)
+        }
+    }
+    triggers {
+        cron("H */4 * * *")
+        gerritTrigger {
+            silentMode(false)
+            serverName('gerrit-service')
+            gerritProjects {
+                gerritProject {
+                    compareType('PLAIN')
+                    pattern("airship/airshipctl")
+                    branches {
+                        branch {
+                            compareType('ANT')
+                            pattern("**")
+                        }
+                    }
+                    forbiddenFilePaths {{
+                        compareType('ANT')
+                        pattern("docs/**")
+                    }}
+                    disableStrictForbiddenFileVerification(true)
+                }
+            }
+            triggerOnEvents {
+                patchsetCreated {
+                    excludeDrafts(false)
+                    excludeTrivialRebase(false)
+                    excludeNoCodeChange(false)
+                    excludePrivateState(false)
+                    excludeWipState(true)
+                }
+                topicChanged()
+		draftPublished()
+                commentAddedContains {
+                   commentAddedCommentContains('recheck')
+                }
+            }
+        }
+    }
+    definition {
+        cps {
+          script(readFileFromWorkspace("${JOB_FOLDER}/airshipctl.groovy"))
+            sandbox(true)
+        }
+    }
+}
