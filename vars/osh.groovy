dc911f739e686c0cc1384a54d82e6b74e5589436
Singh, Shashipratap (sx083a) <sx083a@att.com>
2021-07-21 15:44:24 +0000

    Adding dockerlogin function for artifactory access
    Change-Id: I4751321ce1211e076628d6655142088c48dad35b

--- vars/osh.groovy
+++ vars/osh.groovy
@@ -24,6 +24,18 @@ def installDockerCE() {
  *
  * @param creds Artifactory credentials ID
  */
+def dockerAuth(String docker_repo = "${ARTF_SECURE_DOCKER_URL}",String creds = 'jenkins-artifactory') {
+    withCredentials([usernamePassword(credentialsId: creds,
+                     usernameVariable: 'ARTIFACTORY_USER',
+                     passwordVariable: 'ARTIFACTORY_PASSWORD')]) {
+        opts = '-u $ARTIFACTORY_USER -p $ARTIFACTORY_PASSWORD'
+        sh "sudo docker login ${opts} ${docker_repo}"
+
+        // Allow kubectl to pull images, requires auth config to be on / directory
+        sh 'sudo cp -R ~/.docker /'
+    }
+}
+
 def dockerAuth(String creds = 'jenkins-artifactory') {
     withCredentials([usernamePassword(credentialsId: creds,
                      usernameVariable: 'ARTIFACTORY_USER',
