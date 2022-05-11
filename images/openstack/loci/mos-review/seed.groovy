eb4dcb71604134bb02ee1b78fd217e3224c77840
Kabanov, Dmitrii <dmitrii.kabanov@att.com>
2022-05-11 23:23:37 +0000

    [loci] Add Victoria release support
    Change-Id: Ib1f5ac4d29a7d18674a7b5d7134c3bf243db337f

--- images/openstack/loci/mos-review/seed.groovy
+++ images/openstack/loci/mos-review/seed.groovy
@@ -54,6 +54,19 @@ PROJECT_MAP = [
         'mos-placement':       [],
         'mos-ironic':          [],
     ],
+    "victoria": [
+        "${REQ_PROJECT_NAME}": [],
+        'mos-keystone':        [],
+        'mos-heat':            [],
+        'mos-glance':          [],
+        'mos-cinder':          [],
+        'mos-horizon':         [],
+        'mos-neutron':         ['neutron', 'neutron-sriov'],
+        'mos-nova':            ['nova', 'nova-1804'],
+        'mos-barbican':        [],
+        'mos-placement':       [],
+        'mos-ironic':          [],
+    ],
 ]
 
 MERGED_MAP = [:]
@@ -92,6 +105,16 @@ DEPENDENCY_PROJECT_LIST = [
         'openstack/tap-as-a-service-dashboard',
     ],
     "ussuri": [
+        'mos-keystoneclient',
+        'mos-neutronclient',
+        'mos-novaclient',
+        'mos-glanceclient',
+        'mos-glance-store',
+        'mos-neutron-lib',
+        'openstack/tap-as-a-service',
+        'openstack/tap-as-a-service-dashboard',
+    ],
+    "victoria": [
         'mos-keystoneclient',
         'mos-neutronclient',
         'mos-novaclient',
