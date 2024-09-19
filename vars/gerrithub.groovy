b7979f7b9236e12139610cd887f4dee122309ad2
Grutsenko, Vladislav (vg273g) <vg273g@att.com>
2024-09-18 18:44:50 -0700

    Refactor gerrit checkout functions
    Change-Id: Iaa2e96710d1799cd5ef27a8efbbabdf64cbf84e3

--- vars/gerrithub.groovy
+++ vars/gerrithub.groovy
@@ -1,20 +1,24 @@
 /**
  * This is a wrapper library for vars/gerrit.groovy for use with gerrithub
+ * Deprecated: please use 'gerrit.cloneUpstream' instead
  * TODO: remove it completely
 */
 
+@Deprecated
 def clone(String project, String refspec){
 // Usage example: gerrithub.clone("att-comdev/cicd", "origin/master")
 // clone refspec: gerrithub.clone("att-comdev/cicd", "${env.GERRIT_REFSPEC}")
   gerrit.clone("https://review.gerrithub.io/" + project, refspec)
 }
 
+@Deprecated
 def cloneToBranch(String project, String refspec, String targetDirectory){
 //This method is used so that we can checkout the patchset to a local
 //branch and then rebase it locally with the current master before we build and test
   gerrit.cloneToBranch("https://review.gerrithub.io/" + project, refspec, targetDirectory)
 }
 
+@Deprecated
 def cloneProject(String project, String branch, String refspec, String targetDirectory){
 //This method is used so that we can checkout different project
 //from any patchset in different pipelines
